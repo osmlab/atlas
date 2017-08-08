ba819728f0ec633140134857520dff1b3d8bec80
matthieun <matthieu.nahoum@gmail.com>
2017-08-08 12:53:43 -0700

    Updated travis secrets

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    openssl aes-256-cbc -K $encrypted_188d9cfa26f4_key -iv $encrypted_188d9cfa26f4_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
+    openssl aes-256-cbc -K $encrypted_7b323cc104d6_key -iv $encrypted_7b323cc104d6_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
 fi
