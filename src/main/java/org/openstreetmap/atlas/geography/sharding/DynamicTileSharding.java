baaa765085b86620fbfbd84efc129f4a0abba3fc
Yali Mu <yali.mu114@gmail.com>
2017-10-02 17:40:29 -0700

    Fix NPE when processing partial of the world tiles
    When building sharding tree from maps data, we need to put the tiles of entire world. This is a fix to make sure partial of the world maps is able to build a sharding tree for some regions.

--- src/main/java/org/openstreetmap/atlas/geography/sharding/DynamicTileSharding.java
+++ src/main/java/org/openstreetmap/atlas/geography/sharding/DynamicTileSharding.java
@@ -413,7 +413,7 @@ public class DynamicTileSharding extends Command implements Sharding
             int count = 0;
             for (final SlippyTile miniTile : tile.split(finalZoom))
             {
-                count += counts.get(miniTile);
+                count += counts.getOrDefault(miniTile, 0);
             }
             if (count <= MINIMUM_TO_SPLIT)
             {
