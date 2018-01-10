386cdb2954c49ece6c7750ddf56694b82179567a
mgostinstev <mikegost@hotmail.com>
2018-01-10 13:07:26 -0800

    Removing superfluous Atlas Point (#48)
    * Removing superfluous Atlas Point and tests
    
    * Fixing javadoc warning
    
    * Using text atlas files instead of .osm files to properly test Atlas creation
    
    * undoing text atlas conversion
    
    * Explaining PBF presence as test data

---
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/pbf/OsmPbfNodeToAtlasItemTestRule.java
@@ -0,0 +1,79 @@
+package org.openstreetmap.atlas.geography.atlas.pbf;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+
+/**
+ * {@link OsmPbfNodeToAtlasItemTest} test cases. This class relies on "loadFromJosmOsmResource"
+ * methods, instead of text atlases, because it relies on loading from PBF and testing the
+ * conversion from OSM PBF to Atlas Item. Loading in any other format, would skip this step.
+ *
+ * @author mgostintsev
+ */
+public class OsmPbfNodeToAtlasItemTestRule extends CoreTestRule
+{
+    @TestAtlas(loadFromJosmOsmResource = "noRelationNoTagsNoIntersectionAtlas.osm")
+    private Atlas atlasWithoutRelationNoTagsNoIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "partOfRelationNoTagsAtIntersectionAtlas.osm")
+    private Atlas atlasPartOfRelationNoTagsAtIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "partOfRelationWithTagsNoIntersectionAtlas.osm")
+    private Atlas atlasPartOfRelationWithTagsNoIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "partOfRelationWithTagsAtIntersectionAtlas.osm")
+    private Atlas atlasPartOfRelationWithTagsAtIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "noRelationWithTagsNoIntersectionAtlas.osm")
+    private Atlas atlasWithoutRelationWithTagsNoIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "noRelationNoTagsAtIntersectionAtlas.osm")
+    private Atlas atlasWithoutRelationNoTagsAtIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "noRelationWithTagsAtIntersectionAtlas.osm")
+    private Atlas atlasWithoutRelationWithTagsAtIntersection;
+
+    @TestAtlas(loadFromJosmOsmResource = "partOfRelationNoTagsNoIntersectionAtlas.osm")
+    private Atlas atlasPartOfRelationNoTagsNoIntersection;
+
+    public Atlas getNoRelationNoTagsAtIntersectionAtlas()
+    {
+        return this.atlasWithoutRelationNoTagsAtIntersection;
+    }
+
+    public Atlas getNoRelationNoTagsNoIntersectionAtlas()
+    {
+        return this.atlasWithoutRelationNoTagsNoIntersection;
+    }
+
+    public Atlas getNoRelationWithTagsAtIntersectionAtlas()
+    {
+        return this.atlasWithoutRelationWithTagsAtIntersection;
+    }
+
+    public Atlas getNoRelationWithTagsNoIntersectionAtlas()
+    {
+        return this.atlasWithoutRelationWithTagsNoIntersection;
+    }
+
+    public Atlas getPartOfRelationNoTagsAtIntersectionAtlas()
+    {
+        return this.atlasPartOfRelationNoTagsAtIntersection;
+    }
+
+    public Atlas getPartOfRelationNoTagsNoIntersectionAtlas()
+    {
+        return this.atlasPartOfRelationNoTagsNoIntersection;
+    }
+
+    public Atlas getPartOfRelationWithTagsAtIntersectionAtlas()
+    {
+        return this.atlasPartOfRelationWithTagsAtIntersection;
+    }
+
+    public Atlas getPartOfRelationWithTagsNoIntersectionAtlas()
+    {
+        return this.atlasPartOfRelationWithTagsNoIntersection;
+    }
+}
