a46e836869b5aba709328e8b5b0ac84dc33700ed
Michael Gostintsev <mikegost@hotmail.com>
2018-01-19 14:23:55 -0800

    Supporting loading from OSM xml without JOSM parsing

--- src/test/java/org/openstreetmap/atlas/utilities/testing/TestAtlasTestRule.java
+++ src/test/java/org/openstreetmap/atlas/utilities/testing/TestAtlasTestRule.java
@@ -3,6 +3,8 @@ package org.openstreetmap.atlas.utilities.testing;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 
 /**
+ * {@link TestAtlasTest} test data
+ *
  * @author matthieun
  */
 public class TestAtlasTestRule extends CoreTestRule
@@ -10,8 +12,16 @@ public class TestAtlasTestRule extends CoreTestRule
     @TestAtlas(loadFromJosmOsmResource = "josmOsmFile.osm")
     private Atlas atlasFromJosmOsmResource;
 
+    @TestAtlas(loadFromOsmResource = "osmFile.osm")
+    private Atlas atlasFromOsmResource;
+
     public Atlas getAtlasFromJosmOsmResource()
     {
         return this.atlasFromJosmOsmResource;
+    }
+
+    public Atlas getAtlasFromOsmResource()
+    {
+        return this.atlasFromOsmResource;
     }
 }
