8f2efa3aa0011ed2a63031b333ea68e51ef68624
Mert Emin Kalender <merteminkalender@gmail.com>
2018-01-23 19:25:46 -0800

    Remove fail-fast condition from areasIntersecting (#66)

--- src/test/java/org/openstreetmap/atlas/geography/atlas/items/AtlasItemIntersectionTestRule.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/items/AtlasItemIntersectionTestRule.java
@@ -3,6 +3,8 @@ package org.openstreetmap.atlas.geography.atlas.items;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Area;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
 
 /**
  * {@link AtlasItemIntersectionTest} test data.
@@ -11,11 +13,24 @@ import org.openstreetmap.atlas.utilities.testing.TestAtlas;
  */
 public class AtlasItemIntersectionTestRule extends CoreTestRule
 {
+    private static final String LOCATION_1 = "47.625534, -122.210083";
+    private static final String LOCATION_2 = "47.625576, -122.208305";
+    private static final String LOCATION_3 = "47.626934, -122.208412";
+
     @TestAtlas(loadFromTextResource = "intersectionAtlas.atlas.txt")
     private Atlas intersectionAtlas;
+
+    @TestAtlas(areas = { @Area(coordinates = { @Loc(value = LOCATION_1), @Loc(value = LOCATION_2),
+            @Loc(value = LOCATION_3), @Loc(value = LOCATION_1) }) })
+    private Atlas noIntersectionAtlas;
 
     public Atlas getIntersectionAtlas()
     {
         return this.intersectionAtlas;
+    }
+
+    public Atlas getNoIntersectionAtlas()
+    {
+        return this.noIntersectionAtlas;
     }
 }
