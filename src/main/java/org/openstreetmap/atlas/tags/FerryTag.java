16adb8a956e6a4e66d98a039771c224a91657015
Isabelle Hillberg <ihillberg@apple.com>
2018-04-30 16:54:16 -0700

    Add FerryTag

---
+++ src/main/java/org/openstreetmap/atlas/tags/FerryTag.java
@@ -0,0 +1,37 @@
+package org.openstreetmap.atlas.tags;
+
+import java.util.EnumSet;
+
+import org.openstreetmap.atlas.tags.annotations.Tag;
+import org.openstreetmap.atlas.tags.annotations.TagKey;
+
+/**
+ * OSM Ferry Tag
+ *
+ * @author isabellehillberg
+ */
+@Tag(taginfo = "https://taginfo.openstreetmap.org/keys/ferry#values")
+public enum FerryTag
+{
+    YES,
+    NO,
+    MOTORWAY,
+    TRUNK,
+    PRIMARY,
+    SECONDARY,
+    TERTIARY,
+    UNCLASSIFIED,
+    RESIDENTIAL,
+    SERVICE;
+
+    private static final EnumSet<FerryTag> CAV_NAVIGABLE = EnumSet.of(MOTORWAY, TRUNK, PRIMARY,
+            SECONDARY, TERTIARY, UNCLASSIFIED, RESIDENTIAL, SERVICE);
+
+    @TagKey
+    public static final String KEY = "ferry";
+
+    public static boolean isCarNavigableHighway(final HighwayTag tag)
+    {
+        return CAV_NAVIGABLE.contains(tag);
+    }
+}
