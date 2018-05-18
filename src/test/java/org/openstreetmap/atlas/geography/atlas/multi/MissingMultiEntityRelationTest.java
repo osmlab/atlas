debc28793ed9685de88666d74f2b8bec87344313
lucaspcram <lucaspcram@gmail.com>
2018-05-18 07:01:32 -0700

    Travis should fail now. Added testcases for other MultiEntity bugs

--- src/test/java/org/openstreetmap/atlas/geography/atlas/multi/MissingMultiEntityRelationTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/multi/MissingMultiEntityRelationTest.java
@@ -7,10 +7,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openstreetmap.atlas.geography.Location;
 import org.openstreetmap.atlas.geography.PolyLine;
+import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.AtlasResourceLoader;
 import org.openstreetmap.atlas.geography.atlas.builder.RelationBean;
-import org.openstreetmap.atlas.geography.atlas.items.AtlasEntity;
 import org.openstreetmap.atlas.geography.atlas.items.ItemType;
 import org.openstreetmap.atlas.geography.atlas.packed.PackedAtlas;
 import org.openstreetmap.atlas.geography.atlas.packed.PackedAtlasBuilder;
@@ -30,7 +30,45 @@ public class MissingMultiEntityRelationTest
     @Test
     public void testAreasForMissingRelation()
     {
+        final List<Location> shapePoints = new ArrayList<>();
+        final PackedAtlasBuilder builderWithRelation = new PackedAtlasBuilder();
+        shapePoints.add(Location.forString("1,1"));
+        shapePoints.add(Location.forString("2,2"));
+        shapePoints.add(Location.forString("2,0"));
+        builderWithRelation.addArea(1L, new Polygon(shapePoints),
+                Maps.hashMap("someArea", "inTwoAtlases"));
+        final RelationBean bean = new RelationBean();
+        bean.addItem(1L,
+                "Area appears in multiple atlases, but parent relation is only in one atlas",
+                ItemType.AREA);
+        builderWithRelation.addRelation(2, 2, bean, Maps.hashMap());
 
+        shapePoints.clear();
+        final PackedAtlasBuilder builderNoRelation = new PackedAtlasBuilder();
+        shapePoints.add(Location.forString("1,1"));
+        shapePoints.add(Location.forString("2,2"));
+        shapePoints.add(Location.forString("2,0"));
+        builderNoRelation.addArea(1L, new Polygon(shapePoints),
+                Maps.hashMap("someArea", "inTwoAtlases"));
+
+        final PackedAtlas atlasWithRelation = (PackedAtlas) builderWithRelation.get();
+        final PackedAtlas atlasNoRelation = (PackedAtlas) builderNoRelation.get();
+
+        // 1 MiB resources
+        final ByteArrayResource resourceWithRelation = new ByteArrayResource(1024 * 1024 * 1);
+        final ByteArrayResource resourceNoRelation = new ByteArrayResource(1024 * 1024 * 1);
+
+        atlasWithRelation.save(resourceWithRelation);
+        atlasNoRelation.save(resourceNoRelation);
+
+        /*
+         * Add the Atlas that is missing the relation second so that it overwrites the first one.
+         * See the MultiAtlas.populateReferences() function to see this overwrite happen.
+         */
+        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceWithRelation,
+                resourceNoRelation);
+
+        Assert.assertTrue(!multiAtlas.area(1L).relations().isEmpty());
     }
 
     @Test
@@ -44,17 +82,17 @@ public class MissingMultiEntityRelationTest
                 Maps.hashMap("someNode", "inTwoAtlases"));
         shapePoints.add(Location.forString("1,1"));
         shapePoints.add(Location.forString("2,2"));
-        builderWithRelation.addEdge(3L, new PolyLine(shapePoints),
+        builderWithRelation.addEdge(1L, new PolyLine(shapePoints),
                 Maps.hashMap("someEdge", "inTwoAtlases"));
         final RelationBean bean = new RelationBean();
         bean.addItem(1L,
-                "Node that appears in multiple atlases, but parent relation is only in one of the atlases",
+                "Node appears in multiple atlases, but parent relation is only in one atlas",
                 ItemType.NODE);
         bean.addItem(2L,
-                "Node that appears in multiple atlases, but parent relation is only in one of the atlases",
+                "Node appears in multiple atlases, but parent relation is only in one atlas",
                 ItemType.NODE);
-        bean.addItem(3L,
-                "Edge that appears in multiple atlases, but parent relation is only in one of the atlases",
+        bean.addItem(1L,
+                "Edge appears in multiple atlases, but parent relation is only in one atlas",
                 ItemType.EDGE);
         builderWithRelation.addRelation(4, 4, bean, Maps.hashMap());
 
@@ -66,7 +104,7 @@ public class MissingMultiEntityRelationTest
                 Maps.hashMap("someNode", "inTwoAtlases"));
         shapePoints.add(Location.forString("1,1"));
         shapePoints.add(Location.forString("2,2"));
-        builderNoRelation.addEdge(3L, new PolyLine(shapePoints),
+        builderNoRelation.addEdge(1L, new PolyLine(shapePoints),
                 Maps.hashMap("someEdge", "inTwoAtlases"));
 
         final PackedAtlas atlasWithRelation = (PackedAtlas) builderWithRelation.get();
@@ -79,14 +117,13 @@ public class MissingMultiEntityRelationTest
         atlasWithRelation.save(resourceWithRelation);
         atlasNoRelation.save(resourceNoRelation);
 
-        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceNoRelation,
-                resourceWithRelation);
-
-        for (final AtlasEntity entity : multiAtlas.entities())
-        {
-            System.out.println(entity.toString());
-            System.out.println("MEMBER OF: " + entity.relations().toString());
-        }
+        /*
+         * Add the atlas that is missing the relation second, since the MultiAtlasBorderFixer bug is
+         * caused by a MultiMapWithSet.put() call that overwrites a set. What it should now be doing
+         * (since the bug is fixed) is performing a set union, not a set overwrite.
+         */
+        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceWithRelation,
+                resourceNoRelation);
 
         Assert.assertTrue(!multiAtlas.edge(1L).relations().isEmpty());
     }
@@ -94,7 +131,43 @@ public class MissingMultiEntityRelationTest
     @Test
     public void testLinesForMissingRelation()
     {
+        final List<Location> shapePoints = new ArrayList<>();
+        final PackedAtlasBuilder builderWithRelation = new PackedAtlasBuilder();
+        shapePoints.add(Location.forString("1,1"));
+        shapePoints.add(Location.forString("2,2"));
+        builderWithRelation.addLine(1L, new PolyLine(shapePoints),
+                Maps.hashMap("someLine", "inTwoAtlases"));
+        final RelationBean bean = new RelationBean();
+        bean.addItem(1L,
+                "Line appears in multiple atlases, but parent relation is only in one atlas",
+                ItemType.LINE);
+        builderWithRelation.addRelation(2, 2, bean, Maps.hashMap());
 
+        shapePoints.clear();
+        final PackedAtlasBuilder builderNoRelation = new PackedAtlasBuilder();
+        shapePoints.add(Location.forString("1,1"));
+        shapePoints.add(Location.forString("2,2"));
+        builderNoRelation.addLine(1L, new PolyLine(shapePoints),
+                Maps.hashMap("someLine", "inTwoAtlases"));
+
+        final PackedAtlas atlasWithRelation = (PackedAtlas) builderWithRelation.get();
+        final PackedAtlas atlasNoRelation = (PackedAtlas) builderNoRelation.get();
+
+        // 1 MiB resources
+        final ByteArrayResource resourceWithRelation = new ByteArrayResource(1024 * 1024 * 1);
+        final ByteArrayResource resourceNoRelation = new ByteArrayResource(1024 * 1024 * 1);
+
+        atlasWithRelation.save(resourceWithRelation);
+        atlasNoRelation.save(resourceNoRelation);
+
+        /*
+         * Add the Atlas that is missing the relation second so that it overwrites the first one.
+         * See the MultiAtlas.populateReferences() function to see this overwrite happen.
+         */
+        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceWithRelation,
+                resourceNoRelation);
+
+        Assert.assertTrue(!multiAtlas.line(1L).relations().isEmpty());
     }
 
     @Test
@@ -105,7 +178,7 @@ public class MissingMultiEntityRelationTest
                 Maps.hashMap("someNode", "inTwoAtlases"));
         final RelationBean bean = new RelationBean();
         bean.addItem(1L,
-                "Node that appears in multiple atlases, but parent relation is only in one of the atlases",
+                "Node appears in multiple atlases, but parent relation is only in one atlas",
                 ItemType.NODE);
         builderWithRelation.addRelation(2, 2, bean, Maps.hashMap());
 
@@ -123,6 +196,12 @@ public class MissingMultiEntityRelationTest
         atlasWithRelation.save(resourceWithRelation);
         atlasNoRelation.save(resourceNoRelation);
 
+        /*
+         * Here, we want to add the atlas without the relation first, since the MultiAtlas appends
+         * identical nodes into the MultiNode list. It then will always choose the first subNode to
+         * get the parent relations. See the MultiAtlas.populateReferences() and
+         * MultiNode.relations() to see what's happening.
+         */
         final Atlas multiAtlas = new AtlasResourceLoader().load(resourceNoRelation,
                 resourceWithRelation);
 
@@ -132,12 +211,78 @@ public class MissingMultiEntityRelationTest
     @Test
     public void testPointsForMissingRelation()
     {
+        final PackedAtlasBuilder builderWithRelation = new PackedAtlasBuilder();
+        builderWithRelation.addPoint(1L, Location.forString("1,1"),
+                Maps.hashMap("somePoint", "inTwoAtlases"));
+        final RelationBean bean = new RelationBean();
+        bean.addItem(1L,
+                "Point appears in multiple atlases, but parent relation is only in one atlas",
+                ItemType.POINT);
+        builderWithRelation.addRelation(2, 2, bean, Maps.hashMap());
 
+        final PackedAtlasBuilder builderNoRelation = new PackedAtlasBuilder();
+        builderNoRelation.addPoint(1L, Location.forString("1,1"),
+                Maps.hashMap("somePoiont", "inTwoAtlases"));
+
+        final PackedAtlas atlasWithRelation = (PackedAtlas) builderWithRelation.get();
+        final PackedAtlas atlasNoRelation = (PackedAtlas) builderNoRelation.get();
+
+        // 1 MiB resources
+        final ByteArrayResource resourceWithRelation = new ByteArrayResource(1024 * 1024 * 1);
+        final ByteArrayResource resourceNoRelation = new ByteArrayResource(1024 * 1024 * 1);
+
+        atlasWithRelation.save(resourceWithRelation);
+        atlasNoRelation.save(resourceNoRelation);
+
+        /*
+         * Add the Atlas that is missing the relation second so that it overwrites the first one.
+         * See the MultiAtlas.populateReferences() function to see this overwrite happen.
+         */
+        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceWithRelation,
+                resourceNoRelation);
+
+        Assert.assertTrue(!multiAtlas.point(1L).relations().isEmpty());
     }
 
     @Test
     public void testRelationsForMissingRelation()
     {
+        final PackedAtlasBuilder builderWithRelation = new PackedAtlasBuilder();
+        builderWithRelation.addPoint(1L, Location.forString("1,1"),
+                Maps.hashMap("somePoint", "inTwoAtlases"));
+        final RelationBean bean = new RelationBean();
+        bean.addItem(1L, "some member point", ItemType.POINT);
+        builderWithRelation.addRelation(1, 1, bean, Maps.hashMap());
+        final RelationBean bean2 = new RelationBean();
+        bean2.addItem(1L, "parent relation of subrelation", ItemType.RELATION);
+        builderWithRelation.addRelation(2, 2, bean2, Maps.hashMap());
 
+        final PackedAtlasBuilder builderNoRelation = new PackedAtlasBuilder();
+        builderNoRelation.addPoint(1L, Location.forString("1,1"),
+                Maps.hashMap("somePoint", "inTwoAtlases"));
+        final RelationBean bean3 = new RelationBean();
+        bean3.addItem(1L, "some member point", ItemType.POINT);
+        builderNoRelation.addRelation(1, 1, bean3, Maps.hashMap());
+
+        final PackedAtlas atlasWithRelation = (PackedAtlas) builderWithRelation.get();
+        final PackedAtlas atlasNoRelation = (PackedAtlas) builderNoRelation.get();
+
+        // 1 MiB resources
+        final ByteArrayResource resourceWithRelation = new ByteArrayResource(1024 * 1024 * 1);
+        final ByteArrayResource resourceNoRelation = new ByteArrayResource(1024 * 1024 * 1);
+
+        atlasWithRelation.save(resourceWithRelation);
+        atlasNoRelation.save(resourceNoRelation);
+
+        /*
+         * Here, we want to add the atlas without the relation first, since the MultiAtlas appends
+         * identical relations into the MultiRelation list. It then will always choose the first
+         * subRelation to get the parent relations. See the MultiAtlas.populateReferences() and
+         * MultiRelation.relations() to see what's happening.
+         */
+        final Atlas multiAtlas = new AtlasResourceLoader().load(resourceNoRelation,
+                resourceWithRelation);
+
+        Assert.assertTrue(!multiAtlas.relation(1L).relations().isEmpty());
     }
 }
