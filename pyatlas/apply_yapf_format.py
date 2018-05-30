f44aff46fd6bb4c284ac775ae43b10f49182022a
lucaspcram <lucaspcram@gmail.com>
2018-05-30 16:38:58 -0700

    Added print message to python formatter

--- pyatlas/apply_yapf_format.py
+++ pyatlas/apply_yapf_format.py
@@ -5,7 +5,8 @@ from yapf.yapflib.yapf_api import FormatFile
 def main(argv):
     srcdir = argv[1]
     for file in os.listdir(srcdir):
-        if file.endswith(".py"):
+        if file.endswith('.py'):
+            print "re-formatting " + str(file)
             FormatFile(os.path.join(srcdir, file), in_place=True)
 
 if __name__ == "__main__":
