3c620e09a15dce34373ce51817d181bf41bf6ea6
lucaspcram <lucaspcram@gmail.com>
2018-07-16 11:22:29 -0700

    Small change to get GitHub to see the commit

--- src/main/java/org/openstreetmap/atlas/geography/atlas/items/DiffViewFriendlyItem.java
+++ src/main/java/org/openstreetmap/atlas/geography/atlas/items/DiffViewFriendlyItem.java
@@ -4,7 +4,7 @@ package org.openstreetmap.atlas.geography.atlas.items;
  * Any feature that adheres to this interface promises to provide a method that returns a
  * representation of itself suitable for diff viewing. Diff view friendly strings should break
  * individual logical units onto separate lines. They should also avoid use of tab characters, since
- * the tab context in which the string will be displayed is generally unknown.
+ * the tab context in which the string will be displayed is generally unknown until runtime.
  *
  * @author lcram
  */
