a3fc6f70252dcee5373354e04f3e958129691a6f
lucaspcram <lucaspcram@gmail.com>
2018-08-03 09:09:43 -0700

    Added javadoc

--- src/main/java/org/openstreetmap/atlas/utilities/caching/Cache.java
+++ src/main/java/org/openstreetmap/atlas/utilities/caching/Cache.java
@@ -2,7 +2,23 @@ package org.openstreetmap.atlas.utilities.caching;
 
 import java.util.Optional;
 
+/**
+ * The basic interface with which all caches must comply.
+ * 
+ * @author lcram
+ * @param <K>
+ *            the type of the cache key
+ * @param <V>
+ *            the type of the cache value
+ */
 public interface Cache<K, V>
 {
+    /**
+     * Get a value from the cache, given some key.
+     * 
+     * @param key
+     *            the key
+     * @return the value wrapped in an {@link Optional}
+     */
     Optional<V> get(K key);
 }
