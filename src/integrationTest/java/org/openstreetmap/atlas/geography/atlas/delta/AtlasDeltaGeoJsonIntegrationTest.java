4a6549052b92f90547e186db82c0d7c4555a7ea0
Nicholas Hallahan <nick@theoutpost.io>
2018-08-14 14:07:26 -0700

    spotless apply

--- src/integrationTest/java/org/openstreetmap/atlas/geography/atlas/delta/AtlasDeltaGeoJsonIntegrationTest.java
+++ src/integrationTest/java/org/openstreetmap/atlas/geography/atlas/delta/AtlasDeltaGeoJsonIntegrationTest.java
@@ -1,9 +1,5 @@
 package org.openstreetmap.atlas.geography.atlas.delta;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,6 +9,10 @@ import org.openstreetmap.atlas.geography.atlas.builder.text.TextAtlasBuilder;
 import org.openstreetmap.atlas.streaming.compression.Decompressor;
 import org.openstreetmap.atlas.streaming.resource.InputStreamResource;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * @author hallahan
@@ -29,18 +29,19 @@ public class AtlasDeltaGeoJsonIntegrationTest
         before = new TextAtlasBuilder()
                 .read(new InputStreamResource(() -> AtlasDeltaIntegrationTest.class
                         .getResourceAsStream("DMA_9-168-233-base.txt.gz"))
-                        .withDecompressor(Decompressor.GZIP)
-                        .withName("DMA_9-168-233-base.txt.gz"));
+                                .withDecompressor(Decompressor.GZIP)
+                                .withName("DMA_9-168-233-base.txt.gz"));
         after = new TextAtlasBuilder()
                 .read(new InputStreamResource(() -> AtlasDeltaIntegrationTest.class
                         .getResourceAsStream("DMA_9-168-233-alter.txt.gz"))
-                        .withDecompressor(Decompressor.GZIP)
-                        .withName("DMA_9-168-233-alter.txt.gz"));
+                                .withDecompressor(Decompressor.GZIP)
+                                .withName("DMA_9-168-233-alter.txt.gz"));
         delta = new AtlasDelta(before, after, false).generate();
     }
 
     /**
-     * This is a basic test that should start failing if you change what the delta GeoJSON looks like.
+     * This is a basic test that should start failing if you change what the delta GeoJSON looks
+     * like.
      */
     @Test
     public void testGeoJson()
@@ -66,10 +67,10 @@ public class AtlasDeltaGeoJsonIntegrationTest
         final JsonObject geoJson = new JsonParser().parse(geoJsonStr).getAsJsonObject();
         final JsonArray features = geoJson.getAsJsonArray("features");
 
-        int i = 0;
-        for (; i < features.size(); ++i)
+        int idx = 0;
+        for (; idx < features.size(); ++idx)
         {
-            final JsonObject feature = features.get(i).getAsJsonObject();
+            final JsonObject feature = features.get(idx).getAsJsonObject();
             final JsonObject properties = feature.getAsJsonObject("properties");
 
             final JsonElement diffVal = properties.get("diff");
@@ -80,13 +81,14 @@ public class AtlasDeltaGeoJsonIntegrationTest
             Assert.assertNotNull(diffTypeVal);
 
             // a diff property should be before or after.
-            Assert.assertThat(diffVal.getAsString(), CoreMatchers.anyOf(CoreMatchers.is("BEFORE"), CoreMatchers.is("AFTER")));
+            Assert.assertThat(diffVal.getAsString(),
+                    CoreMatchers.anyOf(CoreMatchers.is("BEFORE"), CoreMatchers.is("AFTER")));
 
             // Make sure we have a reason and a type.
             Assert.assertTrue(diffReasonVal.getAsString().length() > 0);
             Assert.assertTrue(diffTypeVal.getAsString().length() > 0);
         }
 
-        Assert.assertEquals(47646, i);
+        Assert.assertEquals(47646, idx);
     }
 }
