49a49c54c893f2e9f235f4c7a5d214cf315d0621
Bentley Breithaupt <bentleysb@gmail.com>
2018-08-17 09:49:49 -0700

    get all entities at once

--- src/main/java/org/openstreetmap/atlas/geography/atlas/command/AtlasFindByIdSubCommand.java
+++ src/main/java/org/openstreetmap/atlas/geography/atlas/command/AtlasFindByIdSubCommand.java
@@ -7,6 +7,8 @@ import org.openstreetmap.atlas.utilities.runtime.Command;
 import org.openstreetmap.atlas.utilities.runtime.CommandMap;
 
 import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -20,6 +22,9 @@ public class AtlasFindByIdSubCommand extends AbstractAtlasSubCommand
             possibleMultipleOSMIdentifier -> Stream.of(possibleMultipleOSMIdentifier.split(","))
                     .collect(Collectors.toSet()), Command.Optionality.REQUIRED);
 
+    private static final Command.Switch<Path> JOINED_OUTPUT_PARAMETER = new Command.Switch<>("joinedOutput",
+            "The Atlas file to save the joined output to", Paths::get, Command.Optionality.OPTIONAL);
+
     private final Set<Long> ids = new HashSet<>();
 
     public AtlasFindByIdSubCommand()
@@ -28,47 +33,31 @@ public class AtlasFindByIdSubCommand extends AbstractAtlasSubCommand
                 "find which atlas files contain particular Atlas features using a given set of Atlas identifiers");
     }
 
-    @Override public Command.SwitchList switches()
+    @Override
+    public Command.SwitchList switches()
     {
-        return super.switches().with(FEATURE_ID_PARAMETER);
+        return super.switches().with(FEATURE_ID_PARAMETER,JOINED_OUTPUT_PARAMETER);
     }
 
-    @Override public void usage(final PrintStream writer)
+    @Override
+    public void usage(final PrintStream writer)
     {
         writer.printf(AtlasCommandConstants.INPUT_PARAMETER_DESCRIPTION);
         writer.printf(
                 "-id=1000000,2000000 : comma separated Atlas feature identifiers to search for\n");
     }
 
-    @Override protected void start(final CommandMap command)
+    @Override
+    protected void start(final CommandMap command)
     {
         this.ids.addAll((Set) command.get(FEATURE_ID_PARAMETER));
     }
 
-    @Override protected void handle(final Atlas atlas, final CommandMap command)
+    @Override
+    protected void handle(final Atlas atlas, final CommandMap command)
     {
-        atlas.nodes(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("NODE", atlas, item));
-        });
-
-        atlas.points(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("POINT", atlas, item));
-        });
-
-        atlas.edges(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("EDGE", atlas, item));
-        });
-
-        atlas.lines(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("LINE", atlas, item));
-        });
-
-        atlas.areas(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("AREA", atlas, item));
-        });
-
-        atlas.relations(idCheck()).forEach(item -> {
-            System.out.printf(formatAtlasObject("RELATION", atlas, item));
+        atlas.entities(idCheck()).forEach(item -> {
+            System.out.printf(formatAtlasObject(atlas, item));
         });
     }
 
@@ -76,11 +65,11 @@ public class AtlasFindByIdSubCommand extends AbstractAtlasSubCommand
         return object -> this.ids.contains(((Long) object.getIdentifier()).toString());
     }
 
-    private String formatAtlasObject(final String type, final Atlas atlas,
+    private String formatAtlasObject(final Atlas atlas,
             final AtlasEntity entity)
     {
         final String shardName = atlas.metaData().getShardName().orElse("UNKNOWN");
-        return String.format("[%s] [%d] [%d] --> [%s:%s] Tags: [%s]\n", type,
+        return String.format("[%s] [%d] [%d] --> [%s:%s] Tags: [%s]\n", entity.getType(),
                 entity.getOsmIdentifier(), entity.getIdentifier(), shardName, atlas.getName(),
                 entity.getTags());
     }
