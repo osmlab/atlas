9c1dde48581bab184e4038f15020361bb6394cd1
Bentley Breithaupt <bentleysb@gmail.com>
2018-09-04 16:28:10 -0700

    change to txt atlases

--- src/test/java/org/openstreetmap/atlas/geography/atlas/command/AtlasJoinerSubCommandTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/command/AtlasJoinerSubCommandTest.java
@@ -1,7 +1,13 @@
 package org.openstreetmap.atlas.geography.atlas.command;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openstreetmap.atlas.geography.atlas.builder.text.TextAtlasBuilder;
 import org.openstreetmap.atlas.streaming.resource.File;
 
 /**
@@ -11,8 +17,25 @@ import org.openstreetmap.atlas.streaming.resource.File;
  */
 public class AtlasJoinerSubCommandTest
 {
-    private final String shardPath = AtlasFindByAtlasIdentifierSubCommandTest.class
+    private static final String TXT_PATH = AtlasFindByAtlasIdentifierSubCommandTest.class
             .getResource("DNK_Copenhagen").getPath();
+
+    private static final File SHARD_PATH = File.temporaryFolder();
+
+    @BeforeClass
+    public static void createBinaryAtlases()
+    {
+        final List<String> shardList = Arrays.asList(TXT_PATH + "/DNK_1.txt",
+                TXT_PATH + "/DNK_2.txt", TXT_PATH + "/DNK_3.txt");
+        shardList.forEach(shard -> new TextAtlasBuilder().read(new File(shard))
+                .save(new File(SHARD_PATH.getPath() + shard.replace("txt", "atlas"))));
+    }
+
+    @AfterClass
+    public static void deleteBianaryAtlases()
+    {
+        SHARD_PATH.deleteRecursively();
+    }
 
     @Test
     public void testJoinedOutput()
@@ -20,7 +43,7 @@ public class AtlasJoinerSubCommandTest
         final File temp = File.temporary();
 
         // Run AtlasJoinerSubCommand
-        final String[] args = { "join", String.format("-input=%1$s", shardPath),
+        final String[] args = { "join", String.format("-input=%1$s", SHARD_PATH),
                 String.format("-output=%1$s", temp.getPath()) };
         new AtlasReader(args).runWithoutQuitting(args);
 
