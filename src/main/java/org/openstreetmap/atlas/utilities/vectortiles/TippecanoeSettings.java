d20ea6c7231599801a2ad5aaa5d7572ccbc1aad8
Nicholas Hallahan <nick@theoutpost.io>
2018-11-02 12:21:16 -0700

    min zoom to minimum zoom

--- src/main/java/org/openstreetmap/atlas/utilities/vectortiles/TippecanoeSettings.java
+++ src/main/java/org/openstreetmap/atlas/utilities/vectortiles/TippecanoeSettings.java
@@ -23,8 +23,8 @@ final class TippecanoeSettings
     public static final DefaultArtifactVersion MIN_VERSION = new DefaultArtifactVersion("1.32.1");
     public static final String GEOJSON = "EVERYTHING.geojson";
 
-    public static final String[] ARGS = new String[] { "-Z6", "-z14", "--generate-ids", "--read-parallel",
-            "--no-tile-size-limit", "--no-feature-limit" };
+    public static final String[] ARGS = new String[] { "-Z6", "-z14", "--generate-ids",
+            "--read-parallel", "--no-tile-size-limit", "--no-feature-limit" };
 
     public static final BiConsumer<AtlasEntity, JsonObject> JSON_MUTATOR = (atlasEntity, feature) ->
     {
@@ -34,16 +34,26 @@ final class TippecanoeSettings
         final String atlasType = atlasEntity.getType().name();
         tippecanoe.layer(atlasType);
 
-        atlasEntity.getTag("amenity").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.amenity(tag)));
-        atlasEntity.getTag("building").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.building(tag)));
-        atlasEntity.getTag("highway").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.highway(tag)));
-        atlasEntity.getTag("railway").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.railway(tag)));
-        atlasEntity.getTag("route").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.route(tag)));
-        atlasEntity.getTag("place").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.place(tag)));
-        atlasEntity.getTag("natural").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.natural(tag)));
-        atlasEntity.getTag("leisure").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.leisure(tag)));
-        atlasEntity.getTag("landuse").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.landuse(tag)));
-        atlasEntity.getTag("waterway").ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.waterway(tag)));
+        atlasEntity.getTag("amenity")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.amenity(tag)));
+        atlasEntity.getTag("building")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.building(tag)));
+        atlasEntity.getTag("highway")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.highway(tag)));
+        atlasEntity.getTag("railway")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.railway(tag)));
+        atlasEntity.getTag("route")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.route(tag)));
+        atlasEntity.getTag("place")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.place(tag)));
+        atlasEntity.getTag("natural")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.natural(tag)));
+        atlasEntity.getTag("leisure")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.leisure(tag)));
+        atlasEntity.getTag("landuse")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.landuse(tag)));
+        atlasEntity.getTag("waterway")
+                .ifPresent(tag -> tippecanoe.minimumZoom(MinimumZoom.waterway(tag)));
 
     };
 
