1f14f19adbbf795cdda8c67b09a7ed00b2459c25
lucaspcram <lucaspcram@gmail.com>
2018-11-02 16:14:51 -0700

    Added a no color sentinal

--- src/main/java/org/openstreetmap/atlas/utilities/command/AbstractOSMSubcommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/AbstractOSMSubcommand.java
@@ -1,10 +1,25 @@
 package org.openstreetmap.atlas.utilities.command;
 
+import java.util.Arrays;
+
 /**
  * @author lcram
  */
 public abstract class AbstractOSMSubcommand implements OSMSubcommand
 {
+    /*
+     * Until Java supports the ability to do granular TTY checking thru an interface like isatty(2),
+     * we must rely on a sentinel. An external wrapper (bash, perl, etc.) can do the necessary TTY
+     * checking, and then pass this sentinel to tell the subcommand if it should use special
+     * formatting. A ticket to support better TTY in Java checking has been open for years, with no
+     * avail: https://bugs.java.com/bugdatabase/view_bug.do?bug_id=4099017
+     */
+    private static final String NO_COLOR_OPTION = "___AbstractOSMSubcommand__NO_COLOR_SPECIAL_ARG___";
+    private static final String ANSI_BOLD = "\033[1m";
+    private static final String ANSI_RESET = "\033[0m";
+
+    private boolean useColor = true;
+
     public abstract int execute();
 
     public abstract String getName();
@@ -17,11 +32,31 @@ public abstract class AbstractOSMSubcommand implements OSMSubcommand
     {
         final String name = this.getName();
         final String simpleDescription = this.getSimpleDescription();
-        return "NAME\n\t" + name + " -- " + simpleDescription + "\n";
+        final StringBuilder builder = new StringBuilder();
+        builder.append("\n");
+        if (this.useColor)
+        {
+            builder.append(ANSI_BOLD);
+        }
+        builder.append("NAME");
+        if (this.useColor)
+        {
+            builder.append(ANSI_RESET);
+        }
+        builder.append("\n\t" + name + " -- " + simpleDescription + "\n");
+        builder.append("\n");
+        return builder.toString();
     }
 
-    protected void runSubcommandAndExit(final String[] args)
+    protected void runSubcommandAndExit(String... args)
     {
+        // check the last arg to see if we should disable colors
+        if (args.length > 0 && NO_COLOR_OPTION.equals(args[args.length - 1]))
+        {
+            this.useColor = false;
+            args = Arrays.copyOf(args, args.length - 1);
+        }
+
         // fill out appropriate data structures so the execute() implementation can query
         registerOptions();
 
@@ -30,7 +65,7 @@ public abstract class AbstractOSMSubcommand implements OSMSubcommand
         {
             if ("--help".equals(args[0]) || "-h".equals(args[0]))
             {
-                System.err.println(this.getHelpMenu());
+                System.out.println(this.getHelpMenu());
                 System.exit(0);
             }
         }
