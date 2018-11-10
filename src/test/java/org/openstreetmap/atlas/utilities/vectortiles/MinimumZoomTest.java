91c131e89c7ba89470e5c37b24f1a6d737582103
Nicholas Hallahan <nick@theoutpost.io>
2018-11-09 16:12:46 -0800

    MinimumZoomTest

--- src/test/java/org/openstreetmap/atlas/utilities/vectortiles/MinimumZoomTest.java
+++ src/test/java/org/openstreetmap/atlas/utilities/vectortiles/MinimumZoomTest.java
@@ -1,12 +1,20 @@
 package org.openstreetmap.atlas.utilities.vectortiles;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
-public class MinimumZoomTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Basic unit tests for MinimumZoom. The main thing here is that we want to make sure the object
+ * instantiates without any exceptions being thrown. If your minimum-zooms.json is bogus, you'll get
+ * an exception, and then these tests will fail.
+ *
+ * @author hallahan
+ */
+public class MinimumZoomTest
+{
 
     @Test
     public void testLoadingMinimumZoomConfig()
