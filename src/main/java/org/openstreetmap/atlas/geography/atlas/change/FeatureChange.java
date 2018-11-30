95c4d66bd0f9b270f868a35c8cd285889e6b6cc0
matthieun <matthieu.nahoum@gmail.com>
2018-11-29 22:36:19 -0800

    Added feature change mergeability

--- src/main/java/org/openstreetmap/atlas/geography/atlas/change/FeatureChange.java
+++ src/main/java/org/openstreetmap/atlas/geography/atlas/change/FeatureChange.java
@@ -1,14 +1,43 @@
 package org.openstreetmap.atlas.geography.atlas.change;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.Located;
+import org.openstreetmap.atlas.geography.Location;
+import org.openstreetmap.atlas.geography.PolyLine;
+import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.Rectangle;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedArea;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedEdge;
 import org.openstreetmap.atlas.geography.atlas.bloated.BloatedEntity;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedLine;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedNode;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedPoint;
+import org.openstreetmap.atlas.geography.atlas.bloated.BloatedRelation;
+import org.openstreetmap.atlas.geography.atlas.builder.RelationBean;
+import org.openstreetmap.atlas.geography.atlas.builder.RelationBean.RelationBeanItem;
+import org.openstreetmap.atlas.geography.atlas.items.Area;
 import org.openstreetmap.atlas.geography.atlas.items.AtlasEntity;
+import org.openstreetmap.atlas.geography.atlas.items.Edge;
 import org.openstreetmap.atlas.geography.atlas.items.ItemType;
+import org.openstreetmap.atlas.geography.atlas.items.LineItem;
+import org.openstreetmap.atlas.geography.atlas.items.LocationItem;
+import org.openstreetmap.atlas.geography.atlas.items.Node;
+import org.openstreetmap.atlas.geography.atlas.items.Relation;
+import org.openstreetmap.atlas.tags.Taggable;
+import org.openstreetmap.atlas.utilities.collections.Maps;
+import org.openstreetmap.atlas.utilities.collections.Sets;
 
 /**
  * Single feature change, does not include any consistency checks.
@@ -27,6 +56,27 @@ import org.openstreetmap.atlas.geography.atlas.items.ItemType;
 public class FeatureChange implements Located, Serializable
 {
     private static final long serialVersionUID = 9172045162819925515L;
+    private static final BinaryOperator<Map<String, String>> tagMerger = Maps::withMaps;
+    private static final BinaryOperator<Set<Long>> directReferenceMerger = Sets::withSets;
+    private static final BinaryOperator<RelationBean> relationBeanMerger = (leftBean, rightBean) ->
+    {
+        final RelationBean result = new RelationBean();
+        for (final RelationBeanItem leftItem : leftBean)
+        {
+            result.addItem(leftItem);
+        }
+        for (final RelationBeanItem rightItem : rightBean)
+        {
+            if (leftBean.getItemFor(rightItem.getIdentifier(), rightItem.getType()).isPresent())
+            {
+                // Collision, fail
+                throw new CoreException("Unable to merge relation beans. Collision for {} {}",
+                        rightItem.getType(), rightItem.getIdentifier());
+            }
+            result.addItem(rightItem);
+        }
+        return result;
+    };
 
     private final ChangeType changeType;
     private final AtlasEntity reference;
@@ -100,12 +150,220 @@ public class FeatureChange implements Located, Serializable
         return Objects.hash(this.changeType, this.reference);
     }
 
+    /**
+     * Merge two feature changes together. If it cannot succeed, this method will throw a
+     * {@link CoreException} explaining why.
+     *
+     * @param other
+     *            The other to merge into this one.
+     * @return The merged {@link FeatureChange}
+     */
+    public FeatureChange merge(final FeatureChange other)
+    {
+        try
+        {
+            if (this.getIdentifier() != other.getIdentifier()
+                    || this.getItemType() != other.getItemType()
+                    || this.getChangeType() != other.getChangeType())
+            {
+                throw new CoreException(
+                        "Cannot merge two feature changes that are not of the same type.");
+            }
+            if (this.getChangeType() == ChangeType.REMOVE)
+            {
+                return this;
+            }
+            if (this.getChangeType() == ChangeType.ADD)
+            {
+                final AtlasEntity thisReference = this.getReference();
+                final AtlasEntity thatReference = other.getReference();
+                final Map<String, String> mergedTags = mergedMember("tags", thisReference,
+                        thatReference, Taggable::getTags, Optional.of(tagMerger));
+                final Set<Long> mergedParentRelations = mergedMember("parentRelations",
+                        thisReference, thatReference,
+                        atlasEntity -> atlasEntity.relations() == null ? null
+                                : atlasEntity.relations().stream().map(Relation::getIdentifier)
+                                        .collect(Collectors.toSet()),
+                        Optional.of(directReferenceMerger));
+                if (thisReference instanceof LocationItem)
+                {
+                    return mergeLocationItems(other, mergedTags, mergedParentRelations);
+                }
+                else if (thisReference instanceof LineItem)
+                {
+                    return mergeLineItems(other, mergedTags, mergedParentRelations);
+                }
+                else if (thisReference instanceof Area)
+                {
+                    return mergeAreas(other, mergedTags, mergedParentRelations);
+                }
+                else
+                {
+                    // Relation
+                    return mergeRelations(other, mergedTags, mergedParentRelations);
+                }
+
+            }
+            throw new CoreException("Unable to merge {} and {}", this, other);
+        }
+        catch (final Exception e)
+        {
+            throw new CoreException("Cannot merge two feature changes {} and {}.", this, other, e);
+        }
+    }
+
     @Override
     public String toString()
     {
         return "FeatureChange [changeType=" + this.changeType + ", reference={"
                 + this.reference.getType() + "," + this.reference.getIdentifier() + "}, bounds="
                 + bounds() + "]";
+    }
+
+    private FeatureChange mergeAreas(final FeatureChange other,
+            final Map<String, String> mergedTags, final Set<Long> mergedParentRelations)
+    {
+        final AtlasEntity thisReference = this.getReference();
+        final AtlasEntity thatReference = other.getReference();
+        final Polygon mergedPolygon = mergedMember("polygon", thisReference, thatReference,
+                atlasEntity -> ((Area) atlasEntity).asPolygon(), Optional.empty());
+
+        return FeatureChange.add(
+                new BloatedArea(getIdentifier(), mergedPolygon, mergedTags, mergedParentRelations));
+    }
+
+    private <M> M mergedMember(final String memberName, final AtlasEntity left,
+            final AtlasEntity right, final Function<AtlasEntity, M> memberExtractor,
+            final Optional<BinaryOperator<M>> memberMergerOption)
+    {
+        final M result;
+        final M leftMember = memberExtractor.apply(left);
+        final M rightMember = memberExtractor.apply(right);
+        if (leftMember != null && rightMember != null)
+        {
+            // Both are not null, merge evaluated
+            if (leftMember.equals(rightMember))
+            {
+                // They are equal, arbitrarily pick one.
+                result = leftMember;
+            }
+            else if (memberMergerOption.isPresent())
+            {
+                // They are unequal, but we can attempt a merge
+                result = memberMergerOption.get().apply(leftMember, rightMember);
+            }
+            else
+            {
+                // They are unequal and we do not have a tool to merge them.
+                throw new CoreException("Conflicting {}: {} and {}", memberName, leftMember,
+                        rightMember);
+            }
+        }
+        else
+        {
+            // One is not null, or both are.
+            if (leftMember != null)
+            {
+                result = leftMember;
+            }
+            else
+            {
+                result = rightMember;
+            }
+        }
+        return result;
+    }
+
+    private FeatureChange mergeLineItems(final FeatureChange other,
+            final Map<String, String> mergedTags, final Set<Long> mergedParentRelations)
+    {
+        final AtlasEntity thisReference = this.getReference();
+        final AtlasEntity thatReference = other.getReference();
+        final PolyLine mergedPolyLine = mergedMember("polyLine", thisReference, thatReference,
+                atlasEntity -> ((LineItem) atlasEntity).asPolyLine(), Optional.empty());
+        if (thisReference instanceof Edge)
+        {
+            final Long mergedStartNodeIdentifier = mergedMember("startNode", thisReference,
+                    thatReference, edge -> ((Edge) edge).start() == null ? null
+                            : ((Edge) edge).start().getIdentifier(),
+                    Optional.empty());
+            final Long mergedEndNodeIdentifier = mergedMember("endNode", thisReference,
+                    thatReference, edge -> ((Edge) edge).end() == null ? null
+                            : ((Edge) edge).end().getIdentifier(),
+                    Optional.empty());
+            return FeatureChange.add(new BloatedEdge(getIdentifier(), mergedPolyLine, mergedTags,
+                    mergedStartNodeIdentifier, mergedEndNodeIdentifier, mergedParentRelations));
+        }
+        else
+        {
+            // Line
+            return FeatureChange.add(new BloatedLine(getIdentifier(), mergedPolyLine, mergedTags,
+                    mergedParentRelations));
+        }
+    }
+
+    private FeatureChange mergeLocationItems(final FeatureChange other,
+            final Map<String, String> mergedTags, final Set<Long> mergedParentRelations)
+    {
+        final AtlasEntity thisReference = this.getReference();
+        final AtlasEntity thatReference = other.getReference();
+        final Location mergedLocation = mergedMember("location", thisReference, thatReference,
+                atlasEntity -> ((LocationItem) atlasEntity).getLocation(), Optional.empty());
+        if (thisReference instanceof Node)
+        {
+            final SortedSet<Long> mergedInEdgeIdentifiers = (SortedSet<Long>) mergedMember(
+                    "inEdgeIdentifiers", thisReference, thatReference,
+                    atlasEntity -> ((Node) atlasEntity).inEdges() == null ? null
+                            : ((Node) atlasEntity).inEdges().stream().map(Edge::getIdentifier)
+                                    .collect(Collectors.toCollection(TreeSet::new)),
+                    Optional.of(directReferenceMerger));
+            final SortedSet<Long> mergedOutEdgeIdentifiers = (SortedSet<Long>) mergedMember(
+                    "outEdgeIdentifiers", thisReference, thatReference,
+                    atlasEntity -> ((Node) atlasEntity).outEdges() == null ? null
+                            : ((Node) atlasEntity).outEdges().stream().map(Edge::getIdentifier)
+                                    .collect(Collectors.toCollection(TreeSet::new)),
+                    Optional.of(directReferenceMerger));
+            return FeatureChange.add(new BloatedNode(getIdentifier(), mergedLocation, mergedTags,
+                    mergedInEdgeIdentifiers, mergedOutEdgeIdentifiers, mergedParentRelations));
+        }
+        else
+        {
+            // Point
+            return FeatureChange.add(new BloatedPoint(getIdentifier(), mergedLocation, mergedTags,
+                    mergedParentRelations));
+        }
+    }
+
+    private FeatureChange mergeRelations(final FeatureChange other,
+            final Map<String, String> mergedTags, final Set<Long> mergedParentRelations)
+    {
+        final AtlasEntity thisReference = this.getReference();
+        final AtlasEntity thatReference = other.getReference();
+
+        final RelationBean mergedMembers = mergedMember("relationMembers", thisReference,
+                thatReference, entity -> ((Relation) entity).members() == null ? null
+                        : ((Relation) entity).members().asBean(),
+                Optional.of(relationBeanMerger));
+        final Rectangle mergedBounds = Rectangle.forLocated(thisReference, thatReference);
+        final Long mergedOsmRelationIdentifier = mergedMember("osmRelationIdentifier",
+                thisReference, thatReference, entity -> ((Relation) entity).getOsmIdentifier(),
+                Optional.empty());
+        final List<Long> mergedAllRelationsWithSameOsmIdentifier = mergedMember(
+                "allRelationsWithSameOsmIdentifier", thisReference, thatReference,
+                atlasEntity -> ((Relation) atlasEntity).allRelationsWithSameOsmIdentifier() == null
+                        ? null
+                        : ((Relation) atlasEntity).allRelationsWithSameOsmIdentifier().stream()
+                                .map(Relation::getIdentifier).collect(Collectors.toSet()),
+                Optional.of(directReferenceMerger)).stream().collect(Collectors.toList());
+        final RelationBean mergedAllKnownMembers = mergedMember("allKnownOsmMembers", thisReference,
+                thatReference,
+                entity -> ((Relation) entity).allKnownOsmMembers() == null ? null
+                        : ((Relation) entity).allKnownOsmMembers().asBean(),
+                Optional.of(relationBeanMerger));
+
+        return FeatureChange.add(new BloatedRelation(getIdentifier(), mergedTags, mergedBounds,
+                mergedMembers, mergedAllRelationsWithSameOsmIdentifier, mergedAllKnownMembers,
+                mergedOsmRelationIdentifier, mergedParentRelations));
     }
 
     private void validateUsefulFeatureChange()
--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/FeatureChangeTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/FeatureChangeTest.java
@@ -1,5 +1,6 @@
 package org.openstreetmap.atlas.geography.atlas.change;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,6 +11,10 @@ import org.openstreetmap.atlas.geography.atlas.bloated.BloatedLine;
 import org.openstreetmap.atlas.geography.atlas.bloated.BloatedNode;
 import org.openstreetmap.atlas.geography.atlas.bloated.BloatedPoint;
 import org.openstreetmap.atlas.geography.atlas.bloated.BloatedRelation;
+import org.openstreetmap.atlas.geography.atlas.items.Relation;
+import org.openstreetmap.atlas.utilities.collections.Iterables;
+import org.openstreetmap.atlas.utilities.collections.Maps;
+import org.openstreetmap.atlas.utilities.collections.Sets;
 
 /**
  * @author matthieun
@@ -44,6 +49,68 @@ public class FeatureChangeTest
         this.expectedException.expectMessage("does not contain anything useful.");
 
         new FeatureChange(ChangeType.ADD, new BloatedLine(123L, null, null, null));
+    }
+
+    @Test
+    public void testMergeAddRemove()
+    {
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Cannot merge two feature changes");
+
+        final FeatureChange featureChange1 = new FeatureChange(ChangeType.ADD,
+                new BloatedPoint(123L, null, Maps.hashMap(), null));
+        final FeatureChange featureChange2 = new FeatureChange(ChangeType.REMOVE,
+                new BloatedPoint(123L, null, null, null));
+        featureChange1.merge(featureChange2);
+    }
+
+    @Test
+    public void testMergeDifferentType()
+    {
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Cannot merge two feature changes");
+
+        final FeatureChange featureChange1 = new FeatureChange(ChangeType.ADD,
+                new BloatedPoint(123L, null, Maps.hashMap(), null));
+        final FeatureChange featureChange2 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, Maps.hashMap(), null));
+        featureChange1.merge(featureChange2);
+    }
+
+    @Test
+    public void testMergeRelations()
+    {
+        final FeatureChange featureChange1 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, null, Sets.hashSet(456L)));
+        final FeatureChange featureChange2 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, null, Sets.hashSet(567L)));
+        Assert.assertEquals(Sets.hashSet(456L, 567L),
+                Iterables.stream(featureChange1.merge(featureChange2).getReference().relations())
+                        .map(Relation::getIdentifier).collectToSet());
+    }
+
+    @Test
+    public void testMergeTags()
+    {
+        final FeatureChange featureChange1 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, Maps.hashMap("key1", "value1"), null));
+        final FeatureChange featureChange2 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, Maps.hashMap("key2", "value2"), null));
+        Assert.assertEquals(Maps.hashMap("key1", "value1", "key2", "value2"),
+                featureChange1.merge(featureChange2).getReference().getTags());
+    }
+
+    @Test
+    public void testMergeTagsCollision()
+    {
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Cannot merge two feature changes");
+
+        final FeatureChange featureChange1 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, Maps.hashMap("key1", "value1"), null));
+        final FeatureChange featureChange2 = new FeatureChange(ChangeType.ADD,
+                new BloatedArea(123L, null, Maps.hashMap("key1", "value2"), null));
+        featureChange1.merge(featureChange2);
     }
 
     @Test
