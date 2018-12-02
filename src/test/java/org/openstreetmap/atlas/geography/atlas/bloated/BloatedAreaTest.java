39f76b89b1d50377ded57d4f57092f9eada32f91
matthieun <matthieu.nahoum@gmail.com>
2018-12-01 17:02:25 -0800

    Added equality tests

--- src/test/java/org/openstreetmap/atlas/geography/atlas/bloated/BloatedAreaTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/bloated/BloatedAreaTest.java
@@ -12,6 +12,7 @@ import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.items.Area;
 import org.openstreetmap.atlas.geography.atlas.items.Relation;
 import org.openstreetmap.atlas.utilities.collections.Maps;
+import org.openstreetmap.atlas.utilities.collections.Sets;
 
 /**
  * @author matthieun
@@ -20,6 +21,34 @@ public class BloatedAreaTest
 {
     @Rule
     public BloatedTestRule rule = new BloatedTestRule();
+
+    @Test
+    public void testBloatedEquals()
+    {
+        final BloatedArea area11 = new BloatedArea(123L, null, null, null);
+        final BloatedArea area12 = new BloatedArea(123L, null, null, null);
+        final BloatedArea area21 = new BloatedArea(123L, Polygon.SILICON_VALLEY, null, null);
+        final BloatedArea area22 = new BloatedArea(123L, Polygon.SILICON_VALLEY, null, null);
+        final BloatedArea area23 = new BloatedArea(123L, Polygon.SILICON_VALLEY_2, null, null);
+        final BloatedArea area31 = new BloatedArea(123L, null, Maps.hashMap("key", "value"), null);
+        final BloatedArea area32 = new BloatedArea(123L, null, Maps.hashMap("key", "value"), null);
+        final BloatedArea area33 = new BloatedArea(123L, null, Maps.hashMap(), null);
+        final BloatedArea area41 = new BloatedArea(123L, null, null, Sets.hashSet(1L, 2L));
+        final BloatedArea area42 = new BloatedArea(123L, null, null, Sets.hashSet(1L, 2L));
+        final BloatedArea area43 = new BloatedArea(123L, null, null, Sets.hashSet(1L));
+
+        Assert.assertEquals(area11, area12);
+        Assert.assertEquals(area21, area22);
+        Assert.assertEquals(area31, area32);
+        Assert.assertEquals(area41, area42);
+
+        Assert.assertNotEquals(area11, area21);
+        Assert.assertNotEquals(area11, area31);
+        Assert.assertNotEquals(area11, area41);
+        Assert.assertNotEquals(area21, area23);
+        Assert.assertNotEquals(area31, area33);
+        Assert.assertNotEquals(area41, area43);
+    }
 
     @Test
     public void testFull()
