96e12b481793c8629352865fa8a5bd4269f3eb28
lucaspcram <lucaspcram@gmail.com>
2018-12-03 10:29:35 -0800

    Relation diffs and more testing

--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/diff/AtlasDiffTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/diff/AtlasDiffTest.java
@@ -20,56 +20,51 @@ public class AtlasDiffTest
     public AtlasDiffTestRule rule = new AtlasDiffTestRule();
 
     @Test
-    public void simpleTest()
+    public void testNodeAndEdgePropertyDiff()
+    {
+        final Atlas atlasX = this.rule.differentNodeAndEdgeProperties1();
+        final Atlas atlasY = this.rule.differentNodeAndEdgeProperties2();
+
+        // 4 node changes, 2 edge changes as a result of the node change, 1 edge shape point change
+        final int expectedNumberOfChanges = 7;
+
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
+    }
+
+    @Test
+    public void testPointLineAreaPropertyDiff()
+    {
+        final Atlas atlasX = this.rule.differentPointLineArea1();
+        final Atlas atlasY = this.rule.differentPointLineArea2();
+        final int expectedNumberOfChanges = 4;
+
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
+    }
+
+    @Test
+    public void testRelationsDiff()
+    {
+        Atlas atlasX = this.rule.differentRelations1();
+        Atlas atlasY = this.rule.differentRelations2();
+        int expectedNumberOfChanges = 7;
+
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
+
+        atlasX = this.rule.differentRelations3();
+        atlasY = this.rule.differentRelations4();
+        expectedNumberOfChanges = 7;
+
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
+    }
+
+    @Test
+    public void testAggregateDiff()
     {
         final Atlas atlasX = this.rule.simpleAtlas1();
         final Atlas atlasY = this.rule.simpleAtlas2();
         final int expectedNumberOfChanges = 8;
 
-        // First, test with bloated entities
-        final Change changeXToYBloated = new AtlasDiff(atlasX, atlasY).useBloatedEntities(true)
-                .generateChange();
-        Assert.assertEquals(expectedNumberOfChanges, changeXToYBloated.changeCount());
-        final ChangeAtlas changeAtlasY = new ChangeAtlas(atlasX, changeXToYBloated);
-        Assert.assertFalse(new AtlasDiff(changeAtlasY, atlasY).generateChange().hasChanges());
-        Assert.assertEquals(atlasY, changeAtlasY);
-
-        // Now test not-bloated entities
-        final Change changeXToY = new AtlasDiff(atlasX, atlasY).useBloatedEntities(false)
-                .generateChange();
-        Assert.assertEquals(expectedNumberOfChanges, changeXToY.changeCount());
-        final ChangeAtlas changeAtlasY_2 = new ChangeAtlas(atlasX, changeXToY);
-        Assert.assertFalse(new AtlasDiff(changeAtlasY_2, atlasY).generateChange().hasChanges());
-        Assert.assertEquals(atlasY, changeAtlasY_2);
-    }
-
-    @Test
-    public void testNodeLocationsDiff()
-    {
-        final Atlas atlasX = this.rule.differentNodeLocations1();
-        final Atlas atlasY = this.rule.differentNodeLocations2();
-        final int expectedNumberOfChanges = 1;
-
-        final Change changeXToYBloated = new AtlasDiff(atlasX, atlasY).useBloatedEntities(true)
-                .generateChange();
-        Assert.assertEquals(expectedNumberOfChanges, changeXToYBloated.changeCount());
-        final ChangeAtlas changeAtlasY = new ChangeAtlas(atlasX, changeXToYBloated);
-        Assert.assertFalse(new AtlasDiff(changeAtlasY, atlasY).generateChange().hasChanges());
-        Assert.assertEquals(atlasY, changeAtlasY);
-    }
-
-    @Test
-    public void testParentRelationsDiff()
-    {
-        // TODO actually fill in this test once full relation support
-        final Atlas atlasX = this.rule.differentParentRelations1();
-        final Atlas atlasY = this.rule.differentParentRelations2();
-        final int expectedNumberOfChanges = 1;
-
-        final Change changeXToYBloated = new AtlasDiff(atlasX, atlasY).useBloatedEntities(true)
-                .saveAllGeometries(true).generateChange();
-        changeXToYBloated.changes()
-                .forEach(change -> logger.trace("{}: {}", change, change.getReference()));
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
     }
 
     @Test
@@ -79,6 +74,12 @@ public class AtlasDiffTest
         final Atlas atlasY = this.rule.differentTags2();
         final int expectedNumberOfChanges = 3;
 
+        assertChangeAtlasConsistency(atlasX, atlasY, expectedNumberOfChanges);
+    }
+
+    private void assertChangeAtlasConsistency(final Atlas atlasX, final Atlas atlasY,
+            final int expectedNumberOfChanges)
+    {
         // First test with bloated entities
         final Change changeXToYBloated = new AtlasDiff(atlasX, atlasY).useBloatedEntities(true)
                 .generateChange();
--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/diff/AtlasDiffTestRule.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/diff/AtlasDiffTestRule.java
@@ -3,8 +3,12 @@ package org.openstreetmap.atlas.geography.atlas.change.diff;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Area;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Edge;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Line;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Point;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Relation;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Relation.Member;
 
@@ -13,23 +17,17 @@ import org.openstreetmap.atlas.utilities.testing.TestAtlas.Relation.Member;
  */
 public class AtlasDiffTestRule extends CoreTestRule
 {
-    // Inside 12-1350-1870
     private static final String ONE = "15.420563,-61.336198";
     private static final String TWO = "15.429499,-61.332850";
     private static final String TWO_BIS = "15.3907,-61.3112";
-
-    // Inside 12-1350-1869
     private static final String THREE = "15.4855,-61.3041";
     private static final String FOUR = "15.4809,-61.3366";
-
-    // Inside 12-1349-1869
     private static final String FIVE = "15.4852,-61.3816";
     private static final String SIX = "15.4781,-61.3949";
-
-    // Inside 12-1349-1870
     private static final String SEVEN = "15.4145,-61.3826";
     private static final String EIGHT = "15.4073,-61.3749";
     private static final String NINE = "15.4075,-61.3746";
+    private static final String TEN = "15.4081,-61.3741";
 
     @TestAtlas(loadFromJosmOsmResource = "DiffAtlas1.josm.osm")
     private Atlas simpleAtlas1;
@@ -72,25 +70,193 @@ public class AtlasDiffTestRule extends CoreTestRule
 
             nodes = {
 
+                    @Node(id = "1", coordinates = @Loc(value = ONE), tags = { "tag1=value1" }),
+                    @Node(id = "2", coordinates = @Loc(value = TWO), tags = { "tag1=value1" }),
+                    @Node(id = "6", coordinates = @Loc(value = SIX), tags = { "tag1=value1" }),
                     @Node(id = "7", coordinates = @Loc(value = SEVEN), tags = { "tag1=value1" }),
-                    @Node(id = "8", coordinates = @Loc(value = EIGHT), tags = { "tag1=value1" })
+                    @Node(id = "8", coordinates = @Loc(value = EIGHT), tags = { "tag1=value1" }),
+                    @Node(id = "9", coordinates = @Loc(value = NINE), tags = { "tag1=value1" })
+
+            },
+
+            edges = {
+
+                    @Edge(id = "99", coordinates = { @Loc(value = ONE),
+                            @Loc(value = TWO) }, tags = { "highway=secondary" }),
+                    @Edge(id = "100", coordinates = { @Loc(value = SIX),
+                            @Loc(value = SEVEN) }, tags = { "highway=secondary" }),
+                    @Edge(id = "101", coordinates = { @Loc(value = SEVEN),
+                            @Loc(value = EIGHT) }, tags = { "highway=secondary" })
 
             }
 
     )
-    private Atlas differentNodeLocations1;
+    private Atlas differentNodeAndEdgeProperties1;
 
     @TestAtlas(
 
             nodes = {
 
+                    @Node(id = "1", coordinates = @Loc(value = ONE), tags = { "tag1=value1" }),
+                    @Node(id = "2", coordinates = @Loc(value = TWO), tags = { "tag1=value1" }),
+                    @Node(id = "6", coordinates = @Loc(value = SIX), tags = { "tag1=value1" }),
                     @Node(id = "7", coordinates = @Loc(value = SEVEN), tags = { "tag1=value1" }),
-                    @Node(id = "8", coordinates = @Loc(value = NINE), tags = { "tag1=value1" })
+                    @Node(id = "8", coordinates = @Loc(value = EIGHT), tags = { "tag1=value1" }),
+                    @Node(id = "9", coordinates = @Loc(value = TEN), tags = { "tag1=value1" })
+
+            },
+
+            edges = {
+
+                    @Edge(id = "99", coordinates = { @Loc(value = ONE), @Loc(value = THREE),
+                            @Loc(value = TWO) }, tags = { "highway=secondary" }),
+                    @Edge(id = "100", coordinates = { @Loc(value = SEVEN),
+                            @Loc(value = EIGHT) }, tags = { "highway=secondary" }),
+                    @Edge(id = "101", coordinates = { @Loc(value = SIX),
+                            @Loc(value = SEVEN) }, tags = { "highway=secondary" })
 
             }
 
     )
-    private Atlas differentNodeLocations2;
+    private Atlas differentNodeAndEdgeProperties2;
+
+    @TestAtlas(
+
+            areas = {
+
+                    @Area(id = "1", coordinates = { @Loc(value = ONE), @Loc(value = TWO),
+                            @Loc(value = THREE) }, tags = { "landuse=residential" })
+
+            },
+
+            lines = {
+
+                    @Line(id = "1", coordinates = { @Loc(value = FOUR), @Loc(value = FIVE),
+                            @Loc(value = SIX) }, tags = { "power=line" })
+
+            },
+
+            points = {
+
+                    @Point(id = "7", coordinates = @Loc(value = SEVEN)),
+                    @Point(id = "8", coordinates = @Loc(value = EIGHT))
+
+            }
+
+    )
+    private Atlas differentPointLineArea1;
+
+    @TestAtlas(
+
+            areas = {
+
+                    @Area(id = "1", coordinates = { @Loc(value = ONE), @Loc(value = FOUR),
+                            @Loc(value = THREE) }, tags = { "landuse=residential" })
+
+            },
+
+            lines = {
+
+                    @Line(id = "1", coordinates = { @Loc(value = FOUR), @Loc(value = FIVE),
+                            @Loc(value = SEVEN) }, tags = { "power=line" })
+
+            },
+
+            points = {
+
+                    @Point(id = "7", coordinates = @Loc(value = NINE)),
+                    @Point(id = "8", coordinates = @Loc(value = TEN))
+
+            }
+
+    )
+    private Atlas differentPointLineArea2;
+
+    @TestAtlas(
+
+            nodes = {
+
+                    @Node(id = "5", coordinates = @Loc(value = FIVE), tags = { "tag1=value1" }),
+                    @Node(id = "6", coordinates = @Loc(value = SIX), tags = { "tag1=value1" }),
+                    @Node(id = "7", coordinates = @Loc(value = SEVEN), tags = { "tag1=value1" }),
+                    @Node(id = "8", coordinates = @Loc(value = EIGHT), tags = { "tag1=value1" })
+
+            },
+
+            lines = {
+
+                    @Line(id = "6", coordinates = { @Loc(value = ONE), @Loc(value = TWO) })
+
+            },
+
+            relations = {
+
+                    @Relation(id = "31", tags = { "type=relation" }, members = {
+
+                            @Member(id = "5", role = "a", type = "node"),
+
+                    }),
+
+                    @Relation(id = "32", tags = { "type=relation" }, members = {
+
+                            @Member(id = "7", role = "a", type = "node"),
+                            @Member(id = "8", role = "a", type = "node")
+
+                    }),
+
+                    @Relation(id = "33", tags = { "type=relation" }, members = {
+
+                            @Member(id = "6", role = "a", type = "node"),
+
+                    })
+
+            }
+
+    )
+    private Atlas differentRelations1;
+
+    @TestAtlas(
+
+            nodes = {
+
+                    @Node(id = "5", coordinates = @Loc(value = FIVE), tags = { "tag1=value1" }),
+                    @Node(id = "6", coordinates = @Loc(value = SIX), tags = { "tag1=value1" }),
+                    @Node(id = "7", coordinates = @Loc(value = SEVEN), tags = { "tag1=value1" }),
+                    @Node(id = "8", coordinates = @Loc(value = EIGHT), tags = { "tag1=value1" })
+
+            },
+
+            lines = {
+
+                    @Line(id = "6", coordinates = { @Loc(value = ONE), @Loc(value = TWO) })
+
+            },
+
+            relations = {
+
+                    @Relation(id = "31", tags = { "type=relation" }, members = {
+
+                            @Member(id = "5", role = "a", type = "node"),
+                            @Member(id = "7", role = "a", type = "node")
+
+                    }),
+
+                    @Relation(id = "32", tags = { "type=relation" }, members = {
+
+                            @Member(id = "7", role = "a", type = "node"),
+
+                    }),
+
+                    @Relation(id = "33", tags = { "type=relation" }, members = {
+
+                            @Member(id = "6", role = "b", type = "line"),
+
+                    })
+
+            }
+
+    )
+    private Atlas differentRelations2;
 
     @TestAtlas(
 
@@ -115,7 +281,6 @@ public class AtlasDiffTestRule extends CoreTestRule
                     @Relation(id = "32", tags = { "type=relation" }, members = {
 
                             @Member(id = "7", role = "a", type = "node"),
-
                             @Member(id = "8", role = "a", type = "node")
 
                     }),
@@ -129,7 +294,7 @@ public class AtlasDiffTestRule extends CoreTestRule
             }
 
     )
-    private Atlas differentParentRelations1;
+    private Atlas differentRelations3;
 
     @TestAtlas(
 
@@ -160,26 +325,46 @@ public class AtlasDiffTestRule extends CoreTestRule
             }
 
     )
-    private Atlas differentParentRelations2;
+    private Atlas differentRelations4;
 
-    public Atlas differentNodeLocations1()
+    public Atlas differentNodeAndEdgeProperties1()
     {
-        return this.differentNodeLocations1;
+        return this.differentNodeAndEdgeProperties1;
     }
 
-    public Atlas differentNodeLocations2()
+    public Atlas differentNodeAndEdgeProperties2()
     {
-        return this.differentNodeLocations2;
+        return this.differentNodeAndEdgeProperties2;
     }
 
-    public Atlas differentParentRelations1()
+    public Atlas differentPointLineArea1()
     {
-        return this.differentParentRelations1;
+        return this.differentPointLineArea1;
     }
 
-    public Atlas differentParentRelations2()
+    public Atlas differentPointLineArea2()
     {
-        return this.differentParentRelations2;
+        return this.differentPointLineArea2;
+    }
+
+    public Atlas differentRelations1()
+    {
+        return this.differentRelations1;
+    }
+
+    public Atlas differentRelations2()
+    {
+        return this.differentRelations2;
+    }
+
+    public Atlas differentRelations3()
+    {
+        return this.differentRelations3;
+    }
+
+    public Atlas differentRelations4()
+    {
+        return this.differentRelations4;
     }
 
     public Atlas differentTags1()
