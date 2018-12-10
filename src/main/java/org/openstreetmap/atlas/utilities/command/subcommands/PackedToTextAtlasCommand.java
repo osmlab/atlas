e6dce95b42e4954d4c2eedef62a67f323bae61b6
lucaspcram <lucaspcram@gmail.com>
2018-12-10 14:24:49 -0800

    Working on packed to text command for alpha demo

---
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/PackedToTextAtlasCommand.java
@@ -0,0 +1,39 @@
+package org.openstreetmap.atlas.utilities.command.subcommands;
+
+import org.openstreetmap.atlas.utilities.command.AbstractAtlasShellToolsCommand;
+
+public class PackedToTextAtlasCommand extends AbstractAtlasShellToolsCommand
+{
+    @Override
+    public int execute()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getCommandName()
+    {
+        return "packed-to-text";
+    }
+
+    @Override
+    public String getSimpleDescription()
+    {
+        return "Transform a PackedAtlas into a human-readable format";
+    }
+
+    @Override
+    public void registerManualPageSections()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void registerOptionsAndArguments()
+    {
+        // TODO Auto-generated method stub
+
+    }
+}
