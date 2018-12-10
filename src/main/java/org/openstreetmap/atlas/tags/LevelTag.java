4332ce9ca6ff6b6afa67d6042ef1de8f639c9286
sayana-saithu <sayana_saithu@apple.com>
2018-12-10 06:43:09 -0800

    Refactor tag type to string

--- src/main/java/org/openstreetmap/atlas/tags/LevelTag.java
+++ src/main/java/org/openstreetmap/atlas/tags/LevelTag.java
@@ -4,45 +4,26 @@ import java.util.Optional;
 
 import org.openstreetmap.atlas.tags.annotations.Tag;
 import org.openstreetmap.atlas.tags.annotations.TagKey;
-import org.openstreetmap.atlas.tags.annotations.extraction.LongExtractor;
 
 /**
  * OSM level tag
  *
  * @author sayas01
  */
-@Tag(value = Tag.Validation.LONG, taginfo = "http://taginfo.openstreetmap.org/keys/level#values", osm = "http://wiki.openstreetmap.org/wiki/Level")
-
+@Tag(value = Tag.Validation.NON_EMPTY_STRING, taginfo = "http://taginfo.openstreetmap.org/keys/level#values", osm = "http://wiki.openstreetmap.org/wiki/Level")
 public interface LevelTag
 {
     @TagKey
     String KEY = "level";
 
-    static long getMaxLevelValue()
+    static String getTaggedOrImpliedValue(final Taggable taggable, final String impliedValue)
     {
-        return LevelTag.class.getDeclaredAnnotation(Tag.class).range().max();
-    }
-
-    static long getMinLevelValue()
-    {
-        return LevelTag.class.getDeclaredAnnotation(Tag.class).range().min();
-    }
-
-    static Long getTaggedOrImpliedValue(final Taggable taggable, final Long impliedValue)
-    {
-        final Optional<Long> taggedValue = getTaggedValue(taggable);
+        final Optional<String> taggedValue = getTaggedValue(taggable);
         return taggedValue.isPresent() ? taggedValue.get() : impliedValue;
     }
 
-    static Optional<Long> getTaggedValue(final Taggable taggable)
+    static Optional<String> getTaggedValue(final Taggable taggable)
     {
-        final Optional<String> tagValue = taggable.getTag(KEY);
-        if (tagValue.isPresent())
-        {
-            final LongExtractor longExtractor = new LongExtractor();
-            return longExtractor.validateAndExtract(tagValue.get(),
-                    LevelTag.class.getDeclaredAnnotation(Tag.class));
-        }
-        return Optional.empty();
+        return taggable.getTag(KEY);
     }
 }
