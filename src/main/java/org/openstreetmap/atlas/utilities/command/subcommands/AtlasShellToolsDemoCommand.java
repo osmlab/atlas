ed916d7cf57ffeae68777ceb359461f49729f4e6
lucaspcram <lucaspcram@gmail.com>
2018-12-11 16:42:27 -0800

    Refactored demo class

--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/AtlasShellToolsDemoCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/AtlasShellToolsDemoCommand.java
@@ -26,9 +26,92 @@ public class AtlasShellToolsDemoCommand extends AbstractAtlasShellToolsCommand
         // Check if the parser context detected the breakfast usage
         if (getParserContext() == BREAKFAST_CONTEXT)
         {
-            System.exit(executeBreakfastContext());
+            executeBreakfastContext();
+        }
+        else
+        {
+            executeLunchDinnerContext();
         }
 
+        return 0;
+    }
+
+    @Override
+    public String getCommandName()
+    {
+        return "ast-demo";
+    }
+
+    @Override
+    public String getSimpleDescription()
+    {
+        return "a demo of the Atlas Shell Tools subcommand API and features";
+    }
+
+    @Override
+    public void registerManualPageSections()
+    {
+        final String paragraph1 = "This command serves as a demo of the subcommand API and capabilities. "
+                + "This example section is filled out to demonstrate the behavior of the "
+                + "automatic documentation formatting code.";
+        final String paragraph2 = "Here is a second follow up paragraph. Unfortunately, "
+                + "Java does not provide a way to declare multi-line string literals. Due to this "
+                + "shortcoming, declaring these paragraphs in code can be a pain. Thankfully, "
+                + "Eclipse and IntelliJ both automatically format multi-line strings when pasted "
+                + "into an empty \"\". Alternatively, you could have your subcommand read the "
+                + "paragraphs from a resource file.";
+
+        addManualPageSection("DESCRIPTION");
+        addParagraphToSection("DESCRIPTION", paragraph1);
+        addParagraphToSection("DESCRIPTION", paragraph2);
+
+        addManualPageSection("EXAMPLES");
+        addParagraphToSection("EXAMPLES",
+                "Run a dinner command with pizza and wings. Use 805 beer and the default cheese:");
+        addCodeBlockToSection("EXAMPLES", "$ demo dinner pizza wings --beer=805 --cheese");
+        addParagraphToSection("EXAMPLES",
+                "Run a breakfast command with some waffles and pancakes:");
+        addCodeBlockToSection("EXAMPLES", "$ demo breakfast waffles pancakes");
+        addParagraphToSection("EXAMPLES",
+                "Run a lunch command with a salad. Use some tasty parmesan cheese:");
+        addCodeBlockToSection("EXAMPLES", "$ demo lunch salad --cheese=parmesan");
+    }
+
+    @Override
+    public void registerOptionsAndArguments()
+    {
+        final String beerDescription = "Brand of your favorite beer. "
+                + "Currently making this option description really long in"
+                + " order to test out the autoformatting capabilities of"
+                + " the DocumentationFormatter class.";
+
+        setVersion("1.0.0");
+
+        // Register options/arguments for default lunch/dinner context
+        registerOption("capitalize", 'c', "Capitalize the foods list.");
+        registerOptionWithRequiredArgument("beer", beerDescription, "brand");
+        registerOptionWithOptionalArgument("cheese", 'C',
+                "Use cheese. Defaults to cheddar, but will accept a supplied alternative.", "type");
+        registerOptionWithRequiredArgument("repeat", 'R', "Repeat the food list N times.", "N");
+        registerArgument("favoriteMeal", ArgumentArity.UNARY, ArgumentOptionality.REQUIRED);
+        registerArgument("favoriteFoods", ArgumentArity.VARIADIC, ArgumentOptionality.OPTIONAL);
+
+        // Register options/arguments for an alternate breakfast use case
+        registerOption("breakfast", 'b', "Use breakfast mode", BREAKFAST_CONTEXT);
+        registerArgument("favoriteBreakfastFood", ArgumentArity.UNARY, ArgumentOptionality.OPTIONAL,
+                BREAKFAST_CONTEXT);
+    }
+
+    private void executeBreakfastContext()
+    {
+        final String breakfast = getUnaryArgument("favoriteBreakfastFood")
+                .orElse("Default waffles :(");
+        printlnStdout("Using special breakfast mode:");
+        printlnStdout(breakfast, TTYAttribute.BOLD);
+    }
+
+    private void executeLunchDinnerContext()
+    {
         // We registered favoriteFoods as variadic so it comes back as a List.
         final List<String> foods = getVariadicArgument("favoriteFoods");
 
@@ -88,79 +171,5 @@ public class AtlasShellToolsDemoCommand extends AbstractAtlasShellToolsCommand
 
         printStderr("Here is a closing stderr message\n", TTYAttribute.UNDERLINE);
 
-        return 0;
-    }
-
-    @Override
-    public String getCommandName()
-    {
-        return "ast-demo";
-    }
-
-    @Override
-    public String getSimpleDescription()
-    {
-        return "a demo of the Atlas Shell Tools subcommand API and features";
-    }
-
-    @Override
-    public void registerManualPageSections()
-    {
-        final String paragraph1 = "This command serves as a demo of the subcommand API and capabilities. "
-                + "This example section is filled out to demonstrate the behavior of the "
-                + "automatic documentation formatting code.";
-        final String paragraph2 = "Here is a second follow up paragraph. Unfortunately, "
-                + "Java does not provide a way to declare multi-line string literals. Due to this "
-                + "shortcoming, declaring these paragraphs in code can be a pain. Thankfully, "
-                + "Eclipse and IntelliJ both automatically format multi-line strings when pasted "
-                + "into an empty \"\". Alternatively, you could have your subcommand read the "
-                + "paragraphs from a resource file.";
-
-        addManualPageSection("DESCRIPTION");
-        addParagraphToSection("DESCRIPTION", paragraph1);
-        addParagraphToSection("DESCRIPTION", paragraph2);
-
-        addManualPageSection("EXAMPLES");
-        addParagraphToSection("EXAMPLES",
-                "Run a dinner command with pizza and wings. Use 805 beer and the default cheese:");
-        addCodeBlockToSection("EXAMPLES", "$ demo dinner pizza wings --beer=805 --cheese");
-        addParagraphToSection("EXAMPLES",
-                "Run a breakfast command with some waffles and pancakes:");
-        addCodeBlockToSection("EXAMPLES", "$ demo breakfast waffles pancakes");
-        addParagraphToSection("EXAMPLES",
-                "Run a lunch command with a salad. Use some tasty parmesan cheese:");
-        addCodeBlockToSection("EXAMPLES", "$ demo lunch salad --cheese=parmesan");
-    }
-
-    @Override
-    public void registerOptionsAndArguments()
-    {
-        final String beerDescription = "Brand of your favorite beer. "
-                + "Currently making this option description really long in"
-                + " order to test out the autoformatting capabilities of"
-                + " the DocumentationFormatter class.";
-
-        setVersion("1.0.0");
-        registerOption("capitalize", 'c', "Capitalize the foods list.");
-        registerOptionWithRequiredArgument("beer", beerDescription, "brand");
-        registerOptionWithOptionalArgument("cheese", 'C',
-                "Use cheese. Defaults to cheddar, but will accept a supplied alternative.", "type");
-        registerOptionWithRequiredArgument("repeat", 'R', "Repeat the food list N times.", "N");
-        registerArgument("favoriteMeal", ArgumentArity.UNARY, ArgumentOptionality.REQUIRED);
-        registerArgument("favoriteFoods", ArgumentArity.VARIADIC, ArgumentOptionality.OPTIONAL);
-
-        registerOption("breakfast", 'b', "Use breakfast mode", BREAKFAST_CONTEXT);
-        registerArgument("favoriteBreakfastFood", ArgumentArity.UNARY, ArgumentOptionality.REQUIRED,
-                BREAKFAST_CONTEXT);
-    }
-
-    private int executeBreakfastContext()
-    {
-        final String breakfast = getUnaryArgument("favoriteBreakfastFood")
-                .orElseThrow(AtlasShellToolsException::new);
-        printlnStdout("Using special breakfast mode:");
-        printlnStdout(breakfast, TTYAttribute.BOLD);
-
-        return 0;
     }
 }
