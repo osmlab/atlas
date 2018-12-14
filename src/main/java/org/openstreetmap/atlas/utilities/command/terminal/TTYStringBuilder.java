8e64378458987e0c18ac958fe6c9a1b75c6f8f00
lucaspcram <lucaspcram@gmail.com>
2018-12-13 18:21:57 -0800

    Working on indent for TTYStringBUilder

--- src/main/java/org/openstreetmap/atlas/utilities/command/terminal/TTYStringBuilder.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/terminal/TTYStringBuilder.java
@@ -9,15 +9,23 @@ public class TTYStringBuilder
 {
     private final StringBuilder builder;
     private final boolean useColors;
+    private int indentWidth;
 
     public TTYStringBuilder(final boolean useColors)
     {
         this.builder = new StringBuilder();
         this.useColors = useColors;
+        this.indentWidth = 0;
     }
 
     public TTYStringBuilder append(final Object object, final TTYAttribute... attributes)
     {
+        // Append whitespace for the indent setting
+        for (int i = 0; i < this.indentWidth; i++)
+        {
+            this.builder.append(" ");
+        }
+
         if (this.useColors)
         {
             for (final TTYAttribute attribute : attributes)
@@ -37,27 +45,11 @@ public class TTYStringBuilder
         return this;
     }
 
-    public TTYStringBuilder append(final String string, final TTYAttribute... attributes)
-    {
-        if (this.useColors)
-        {
-            for (final TTYAttribute attribute : attributes)
-            {
-                this.builder.append(attribute.getANSISequence());
-            }
-        }
-
-        this.builder.append(string);
-
-        // If an attribute was supplied, we need to reset the TTY
-        if (this.useColors && attributes.length > 0)
-        {
-            this.builder.append(TTYAttribute.RESET.getANSISequence());
-        }
-
-        return this;
-    }
-
+    /**
+     * Append a newline to this builder.
+     *
+     * @return the updated builder
+     */
     public TTYStringBuilder newline()
     {
         this.builder.append(System.getProperty("line.separator"));
@@ -68,5 +60,11 @@ public class TTYStringBuilder
     public String toString()
     {
         return this.builder.toString();
+    }
+
+    public TTYStringBuilder withIndentWidth(final int width)
+    {
+        this.indentWidth = width;
+        return this;
     }
 }
