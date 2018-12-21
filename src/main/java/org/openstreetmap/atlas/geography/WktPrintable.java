608ca0fbe45d705f75de5c63a7e0a08671e4105f
matthieun <matthieun@users.noreply.github.com>
2018-12-21 15:15:41 -0800

    DynamicAtlas: Aggressively load relations in preemptive loading (#312)
    * work on relations, with test failures
    
    * Updates for tests
    
    * fix test count
    
    * update the geojson ones
    
    * enhancements for Sonar
    
    * checkstyle
    
    * Make sure to respect the expansion predicate

--- src/main/java/org/openstreetmap/atlas/geography/WktPrintable.java
+++ src/main/java/org/openstreetmap/atlas/geography/WktPrintable.java
@@ -1,9 +1,22 @@
 package org.openstreetmap.atlas.geography;
 
+import org.openstreetmap.atlas.utilities.collections.StringList;
+
 /**
  * @author matthieun
  */
 public interface WktPrintable
 {
+    static String toWktCollection(final Iterable<? extends WktPrintable> input)
+    {
+        final StringList wktList = new StringList();
+        input.forEach(wktPrintable -> wktList.add(wktPrintable.toWkt()));
+        final StringBuilder builder = new StringBuilder();
+        builder.append("GEOMETRYCOLLECTION (");
+        builder.append(wktList.join(", "));
+        builder.append(")");
+        return builder.toString();
+    }
+
     String toWkt();
 }
