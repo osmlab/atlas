baf924b56cf0459484833832f9c5b572e4403d27
Bentley Breithaupt <bentleysb@gmail.com>
2019-01-18 15:35:43 -0800

    suppress sonar

--- src/main/java/org/openstreetmap/atlas/tags/ConstructionTag.java
+++ src/main/java/org/openstreetmap/atlas/tags/ConstructionTag.java
@@ -10,6 +10,7 @@ import org.openstreetmap.atlas.tags.annotations.TagKey;
  */
 @Tag(with = { BuildingTag.class, HighwayTag.class, LandUseTag.class,
         RailwayTag.class }, taginfo = "https://taginfo.openstreetmap.org/keys/construction", osm = "https://wiki.openstreetmap.org/wiki/Key:construction")
+@SuppressWarnings("squid:S1214")
 public interface ConstructionTag
 {
     @TagKey
