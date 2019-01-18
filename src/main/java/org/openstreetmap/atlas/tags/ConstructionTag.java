ec3a54b5190b4b10b7dad9e47117b780d6c8d56c
Bentley Breithaupt <bentleysb@gmail.com>
2019-01-18 15:06:47 -0800

    ConstructionTag

---
+++ src/main/java/org/openstreetmap/atlas/tags/ConstructionTag.java
@@ -0,0 +1,17 @@
+package org.openstreetmap.atlas.tags;
+
+import org.openstreetmap.atlas.tags.annotations.Tag;
+import org.openstreetmap.atlas.tags.annotations.TagKey;
+
+/**
+ * OSM Construction tag. Inherits values from Bridge, Highway, LandUse and Railway tags.
+ *
+ * @author bbreithaupt
+ */
+@Tag(with = { BuildingTag.class, HighwayTag.class, LandUseTag.class,
+        RailwayTag.class }, taginfo = "https://taginfo.openstreetmap.org/keys/construction", osm = "https://wiki.openstreetmap.org/wiki/Key:construction")
+public interface ConstructionTag
+{
+    @TagKey
+    String KEY = "construction";
+}
