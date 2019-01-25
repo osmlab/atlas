2b1ff22ee5061ec4cc341b1c7d52486d561a2d15
lucaspcram <lucaspcram@gmail.com>
2019-01-25 07:46:07 -0800

    Added a polyline length method.

--- pyatlas/pyatlas/geometry.py
+++ pyatlas/pyatlas/geometry.py
@@ -4,6 +4,7 @@ functions for manipulating the geometry. These primitives are built using
 lat-long locations on the Earth.
 """
 
+import math
 import shapely.geometry
 
 # --- Location definition constants ---
@@ -231,6 +232,20 @@ class PolyLine(Boundable):
         """
         return self.location_list
 
+    def length(self):
+        """
+        Get the length of this PolyLine along the surface of the Earth, in meters.
+        """
+        prev_location = None
+        sum_distance = 0
+
+        for cur_location in self.locations():
+            if prev_location is not None:
+                sum_distance += location_haversine_distance(cur_location, prev_location)
+            prev_location = cur_location
+
+        return sum_distance
+
     def locations(self):
         """
         Get a generator for the Locations in this PolyLine.
@@ -456,6 +471,40 @@ def bounds_atlasentities(entities):
         raise ValueError('entity iterable must yield at least one value')
 
     return Rectangle(Location(lower_lat, left_lon), Location(upper_lat, right_lon))
+
+
+def location_haversine_distance(location1, location2):
+    """
+    Given two locations, compute the Haversine distance between them. This is
+    the great circle distance between the points on the sphere of the Earth.
+    See https://en.wikipedia.org/wiki/Haversine_formula.
+    """
+    mean_radius = 6371000  # meters
+    lat1 = convert_to_radians(location1.get_latitude_deg())
+    lat2 = convert_to_radians(location2.get_latitude_deg())
+    delta_lat = lat2 - lat1
+    delta_lon = convert_to_radians(location2.get_longitude_deg()) - convert_to_radians(
+        location1.get_longitude_deg())
+
+    hav = (math.sin(delta_lat / 2)**
+           2) + math.cos(lat1) * math.cos(lat2) * (math.sin(delta_lon / 2)**2)
+    constant = 2 * math.atan2(math.sqrt(hav), math.sqrt(1 - hav))
+
+    return constant * mean_radius
+
+
+def convert_to_radians(degree):
+    """
+    Convert an angle in degrees to the equivalent angle in radians.
+    """
+    return degree * (math.pi / 180)
+
+
+def convert_to_degrees(radian):
+    """
+    Convert an angle in radians to the equivalent angle in degrees.
+    """
+    return radian * (180 / math.pi)
 
 
 def boundable_to_shapely_box(boundable):
