17bb424715bb62562e4180d11d96a9ee5486cad8
lucaspcram <lucaspcram@gmail.com>
2019-01-30 09:36:20 -0800

    Refactored ConcatenateAtlasCommand to use the new AtlasLoader

--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/ConcatenateAtlasCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/ConcatenateAtlasCommand.java
@@ -2,26 +2,32 @@ package org.openstreetmap.atlas.utilities.command.subcommands;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.openstreetmap.atlas.geography.atlas.AtlasResourceLoader;
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.geography.atlas.multi.MultiAtlas;
 import org.openstreetmap.atlas.geography.atlas.packed.PackedAtlas;
 import org.openstreetmap.atlas.geography.atlas.packed.PackedAtlasCloner;
 import org.openstreetmap.atlas.streaming.resource.File;
+import org.openstreetmap.atlas.utilities.command.AtlasShellToolsException;
 import org.openstreetmap.atlas.utilities.command.abstractcommand.CommandOutputDelegate;
 import org.openstreetmap.atlas.utilities.command.abstractcommand.OptionAndArgumentDelegate;
-import org.openstreetmap.atlas.utilities.command.subcommands.templates.VariadicAtlasLoaderCommand;
+import org.openstreetmap.atlas.utilities.command.subcommands.templates.AtlasLoaderCommand;
 
 /**
  * @author lcram
  */
-public class ConcatenateAtlasCommand extends VariadicAtlasLoaderCommand
+public class ConcatenateAtlasCommand extends AtlasLoaderCommand
 {
     private static final String OUTPUT_ATLAS = "output.atlas";
 
     private final OptionAndArgumentDelegate optionAndArgumentDelegate;
     private final CommandOutputDelegate outputDelegate;
+
+    private final List<Atlas> atlases = new ArrayList<>();
+    private Optional<Path> outputParentPath;
 
     public static void main(final String[] args)
     {
@@ -33,42 +39,6 @@ public class ConcatenateAtlasCommand extends VariadicAtlasLoaderCommand
         super();
         this.optionAndArgumentDelegate = this.getOptionAndArgumentDelegate();
         this.outputDelegate = this.getCommandOutputDelegate();
-    }
-
-    @Override
-    public int execute()
-    {
-        final List<File> atlasResourceList = this.getInputAtlasResources();
-        if (atlasResourceList.isEmpty())
-        {
-            this.outputDelegate.printlnErrorMessage("no input atlases");
-            return 1;
-        }
-
-        final Optional<Path> outputParentPath = this.getOutputPath();
-        if (!outputParentPath.isPresent())
-        {
-            this.outputDelegate.printlnErrorMessage("invalid output path");
-            return 1;
-        }
-
-        if (this.optionAndArgumentDelegate.hasVerboseOption())
-        {
-            this.outputDelegate.printlnStdout("Cloning...");
-        }
-        final PackedAtlas outputAtlas = new PackedAtlasCloner()
-                .cloneFrom(new AtlasResourceLoader().load(atlasResourceList));
-        final Path concatenatedPath = Paths.get(outputParentPath.get().toAbsolutePath().toString(),
-                OUTPUT_ATLAS);
-        final File outputFile = new File(concatenatedPath.toAbsolutePath().toString());
-        outputAtlas.save(outputFile);
-
-        if (this.optionAndArgumentDelegate.hasVerboseOption())
-        {
-            this.outputDelegate.printlnStdout("Saved to " + concatenatedPath.toString());
-        }
-
-        return 0;
     }
 
     @Override
@@ -91,5 +61,53 @@ public class ConcatenateAtlasCommand extends VariadicAtlasLoaderCommand
         addManualPageSection("EXAMPLES", ConcatenateAtlasCommand.class
                 .getResourceAsStream("ConcatenateAtlasCommandExamplesSection.txt"));
         super.registerManualPageSections();
+    }
+
+    @Override
+    protected int finish()
+    {
+        if (this.atlases.isEmpty())
+        {
+            this.outputDelegate.printlnErrorMessage("could not load atlas(es)");
+            return 1;
+        }
+
+        final Atlas atlas = new MultiAtlas(this.atlases);
+
+        if (this.optionAndArgumentDelegate.hasVerboseOption())
+        {
+            this.outputDelegate.printlnCommandMessage("cloning...");
+        }
+        final PackedAtlas outputAtlas = new PackedAtlasCloner().cloneFrom(atlas);
+        final Path concatenatedPath = Paths.get(this.outputParentPath
+                .orElseThrow(AtlasShellToolsException::new).toAbsolutePath().toString(),
+                OUTPUT_ATLAS);
+        final File outputFile = new File(concatenatedPath.toAbsolutePath().toString());
+        outputAtlas.save(outputFile);
+
+        if (this.optionAndArgumentDelegate.hasVerboseOption())
+        {
+            this.outputDelegate.printlnCommandMessage("saved to " + concatenatedPath.toString());
+        }
+
+        return 0;
+    }
+
+    @Override
+    protected void processAtlas(final Atlas atlas)
+    {
+        this.atlases.add(atlas);
+    }
+
+    @Override
+    protected int start()
+    {
+        this.outputParentPath = this.getOutputPath();
+        if (!this.outputParentPath.isPresent())
+        {
+            this.outputDelegate.printlnErrorMessage("invalid output path");
+            return 1;
+        }
+        return 0;
     }
 }
