cda0fa3f1a794fdde35a637e0c5901677a834974
lucaspcram <lucaspcram@gmail.com>
2019-02-01 14:59:32 -0800

    Working on iso command

--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IdentifierSearchCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IdentifierSearchCommand.java
@@ -3,6 +3,14 @@ package org.openstreetmap.atlas.utilities.command.subcommands;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.utilities.command.subcommands.templates.AtlasLoaderCommand;
 
+/**
+ * Search atlases for some given feature identifiers, with various options and restrictions. Based
+ * on similar identifier locater commands by cstaylor and bbreithaupt.
+ *
+ * @author lcram
+ * @author cstaylor
+ * @author bbreithaupt
+ */
 public class IdentifierSearchCommand extends AtlasLoaderCommand
 {
     public static void main(final String[] args)
--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IsoCountryCodeCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IsoCountryCodeCommand.java
@@ -1,17 +1,66 @@
 package org.openstreetmap.atlas.utilities.command.subcommands;
 
-import org.openstreetmap.atlas.utilities.command.abstractcommand.AbstractAtlasShellToolsCommand;
+import java.util.List;
+import java.util.Optional;
 
+import org.openstreetmap.atlas.locale.IsoCountry;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.AbstractAtlasShellToolsCommand;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.CommandOutputDelegate;
+import org.openstreetmap.atlas.utilities.command.abstractcommand.OptionAndArgumentDelegate;
+import org.openstreetmap.atlas.utilities.command.parsing.ArgumentArity;
+import org.openstreetmap.atlas.utilities.command.parsing.ArgumentOptionality;
+import org.openstreetmap.atlas.utilities.command.terminal.TTYAttribute;
+
+/**
+ * @author lcram
+ */
 public class IsoCountryCodeCommand extends AbstractAtlasShellToolsCommand
 {
+    private static final String QUERY_HINT = "query";
+
+    private final OptionAndArgumentDelegate optionAndArgumentDelegate;
+    private final CommandOutputDelegate outputDelegate;
+
     public static void main(final String[] args)
     {
         new IsoCountryCodeCommand().runSubcommandAndExit(args);
     }
 
+    public IsoCountryCodeCommand()
+    {
+        this.optionAndArgumentDelegate = this.getOptionAndArgumentDelegate();
+        this.outputDelegate = this.getCommandOutputDelegate();
+    }
+
     @Override
     public int execute()
     {
+        final List<String> queries = this.optionAndArgumentDelegate.getVariadicArgument(QUERY_HINT);
+
+        for (final String query : queries)
+        {
+            final Optional<IsoCountry> forCode = IsoCountry.forCountryCode(query);
+            final Optional<IsoCountry> forDisplay = IsoCountry.forDisplayCountryClosestMatch(query);
+
+            // check for exact country code first
+            if (forCode.isPresent())
+            {
+                this.outputDelegate.printlnStdout(
+                        forCode.get().toString() + ", " + forCode.get().getIso3CountryCode(),
+                        TTYAttribute.BOLD);
+            }
+            else if (forDisplay.isPresent())
+            {
+                this.outputDelegate.printlnStdout(
+                        forDisplay.get().toString() + ", " + forDisplay.get().getIso3CountryCode(),
+                        TTYAttribute.BOLD);
+            }
+            else
+            {
+                this.outputDelegate.printlnErrorMessage("unmatchable query " + query);
+            }
+        }
+
         return 0;
     }
 
@@ -30,6 +79,16 @@ public class IsoCountryCodeCommand extends AbstractAtlasShellToolsCommand
     @Override
     public void registerManualPageSections()
     {
+        addManualPageSection("DESCRIPTION", IsoCountryCodeCommand.class
+                .getResourceAsStream("IsoCountryCodeCommandDescriptionSection.txt"));
+        addManualPageSection("EXAMPLES", IsoCountryCodeCommand.class
+                .getResourceAsStream("IsoCountryCodeCommandExamplesSection.txt"));
+    }
 
+    @Override
+    public void registerOptionsAndArguments()
+    {
+        registerArgument(QUERY_HINT, ArgumentArity.VARIADIC, ArgumentOptionality.REQUIRED);
+        super.registerOptionsAndArguments();
     }
 }
