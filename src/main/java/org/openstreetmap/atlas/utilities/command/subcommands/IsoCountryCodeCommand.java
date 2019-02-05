116c532aa310367695aa34e9169c0145b65612cc
lucaspcram <lucaspcram@gmail.com>
2019-02-05 12:47:20 -0800

    Fixed iso country command

--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IsoCountryCodeCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/IsoCountryCodeCommand.java
@@ -9,6 +9,7 @@ import org.openstreetmap.atlas.utilities.command.abstractcommand.CommandOutputDe
 import org.openstreetmap.atlas.utilities.command.abstractcommand.OptionAndArgumentDelegate;
 import org.openstreetmap.atlas.utilities.command.parsing.ArgumentArity;
 import org.openstreetmap.atlas.utilities.command.parsing.ArgumentOptionality;
+import org.openstreetmap.atlas.utilities.command.parsing.OptionOptionality;
 import org.openstreetmap.atlas.utilities.command.terminal.TTYAttribute;
 
 /**
@@ -16,6 +17,14 @@ import org.openstreetmap.atlas.utilities.command.terminal.TTYAttribute;
  */
 public class IsoCountryCodeCommand extends AbstractAtlasShellToolsCommand
 {
+    private static final int DEFAULT_MATCH_NUMBER = 3;
+
+    private static final char NUMBER_OPTION_SHORT = 'n';
+    private static final String NUMBER_OPTION_LONG = "number";
+    private static final String NUMBER_OPTION_DESCRIPTION = "The number of matches to display. Defaults to "
+            + DEFAULT_MATCH_NUMBER + ".";
+    private static final String NUMBER_OPTION_HINT = "n";
+
     private static final String QUERY_HINT = "query";
 
     private final OptionAndArgumentDelegate optionAndArgumentDelegate;
@@ -37,30 +46,54 @@ public class IsoCountryCodeCommand extends AbstractAtlasShellToolsCommand
     {
         final List<String> queries = this.optionAndArgumentDelegate.getVariadicArgument(QUERY_HINT);
 
-        for (final String query : queries)
+        for (int i = 0; i < queries.size(); i++)
         {
+            final String query = queries.get(i);
             final Optional<IsoCountry> forCode = IsoCountry.forCountryCode(query);
-            final Optional<IsoCountry> forDisplay = IsoCountry.forDisplayCountryClosestMatch(query);
+            final Optional<IsoCountry> forDisplayExact = IsoCountry
+                    .forDisplayCountryIgnoreCase(query);
+            final List<IsoCountry> forDisplayTopMatches = IsoCountry
+                    .forDisplayCountryTopMatches(this.optionAndArgumentDelegate
+                            .getOptionArgument(NUMBER_OPTION_LONG, Integer::parseInt)
+                            .orElse(DEFAULT_MATCH_NUMBER), query);
 
             // check for exact country code first
             if (forCode.isPresent())
             {
-                this.outputDelegate.printlnStdout("ISO code \'" + query + "\' matched: ");
+                this.outputDelegate.printlnStdout("ISO code \'" + query + "\' matched: ",
+                        TTYAttribute.BOLD);
                 this.outputDelegate.printlnStdout(
                         forCode.get().toString() + ", " + forCode.get().getIso3CountryCode(),
-                        TTYAttribute.BOLD);
+                        TTYAttribute.GREEN);
             }
-            else if (forDisplay.isPresent())
+            else if (forDisplayExact.isPresent())
             {
-                this.outputDelegate
-                        .printlnStdout("Display country name \'" + query + "\' matched: ");
                 this.outputDelegate.printlnStdout(
-                        forDisplay.get().toString() + ", " + forDisplay.get().getIso3CountryCode(),
+                        "Display country name \'" + query + "\' matched: ", TTYAttribute.BOLD);
+                this.outputDelegate.printlnStdout(forDisplayExact.get().toString() + ", "
+                        + forDisplayExact.get().getIso3CountryCode(), TTYAttribute.GREEN);
+            }
+            else if (!forDisplayTopMatches.isEmpty())
+            {
+                this.outputDelegate.printlnStdout(
+                        "Display country name \'" + query + "\' had no exact matches. "
+                                + forDisplayTopMatches.size() + " closest matches are:",
                         TTYAttribute.BOLD);
+                for (final IsoCountry country : forDisplayTopMatches)
+                {
+                    this.outputDelegate.printlnStdout(
+                            country.toString() + ", " + country.getIso3CountryCode(),
+                            TTYAttribute.GREEN);
+                }
             }
             else
             {
                 this.outputDelegate.printlnErrorMessage("unmatchable query " + query);
+            }
+
+            if (i < queries.size() - 1)
+            {
+                this.outputDelegate.printlnStdout("");
             }
         }
 
@@ -92,6 +125,8 @@ public class IsoCountryCodeCommand extends AbstractAtlasShellToolsCommand
     public void registerOptionsAndArguments()
     {
         registerArgument(QUERY_HINT, ArgumentArity.VARIADIC, ArgumentOptionality.REQUIRED);
+        registerOptionWithRequiredArgument(NUMBER_OPTION_LONG, NUMBER_OPTION_SHORT,
+                NUMBER_OPTION_DESCRIPTION, OptionOptionality.OPTIONAL, NUMBER_OPTION_HINT);
         super.registerOptionsAndArguments();
     }
 }
