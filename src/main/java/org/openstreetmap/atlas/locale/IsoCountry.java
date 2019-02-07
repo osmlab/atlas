ac032619eac26ffb5a9a1520083af836ce924ea1
lucaspcram <lucaspcram@gmail.com>
2019-02-07 14:00:30 -0800

    Fixed typo in IsoCountry

--- src/main/java/org/openstreetmap/atlas/locale/IsoCountry.java
+++ src/main/java/org/openstreetmap/atlas/locale/IsoCountry.java
@@ -185,8 +185,7 @@ public final class IsoCountry implements Serializable
 
     /**
      * Provides IsoCountry for a valid country display name. Ignores capitalization (e.g. "united
-     * stAtes" and "United States" are the same) Provides IsoCountry for a valid country display
-     * name.
+     * stAtes" and "United States" are the same)
      *
      * @param displayCountry
      *            the display country name, e.g. "United States"
