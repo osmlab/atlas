de10d33e7da04e3d103b5724bd01ea04ef529002
lucaspcram <lucaspcram@gmail.com>
2019-02-07 12:36:48 -0800

    Deprecated a constructor in InputStreamResource (#339)
    * Deprecated a constructor in InputStreamResource
    
    * Fixed javadoc error
    
    * Added deprecated tag

--- src/main/java/org/openstreetmap/atlas/streaming/resource/InputStreamResource.java
+++ src/main/java/org/openstreetmap/atlas/streaming/resource/InputStreamResource.java
@@ -16,11 +16,29 @@ public class InputStreamResource extends AbstractResource
 {
     private final Supplier<InputStream> inputStreamSupplier;
 
+    /**
+     * This constructor is deprecated. Creating an InputStreamResource using a raw InputStream leads
+     * to many gotchas, since the resource becomes read-once. For e.g. loading an atlas from an
+     * InputStreamResource will fail, since atlas uses an under-the-hood read to determine the
+     * serialization format.
+     * 
+     * @deprecated
+     * @param input
+     *            the input stream
+     */
+    @Deprecated
     public InputStreamResource(final InputStream input)
     {
         this(() -> input);
     }
 
+    /**
+     * The supplier given to this constructor should return a new input stream at each invocation to
+     * avoid any read-once gotchas. E.g. of a good supplier: () -&gt; new FileInputStream("foo.txt")
+     *
+     * @param inputStreamSupplier
+     *            the stream supplier
+     */
     public InputStreamResource(final Supplier<InputStream> inputStreamSupplier)
     {
         this.inputStreamSupplier = inputStreamSupplier;
