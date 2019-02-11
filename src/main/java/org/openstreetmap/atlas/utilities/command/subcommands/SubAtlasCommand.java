798ffefb5964b36b9cbbdbb139b3cdde1fe9b89d
lucaspcram <lucaspcram@gmail.com>
2019-02-11 15:04:21 -0800

    Super crappy basic predicate functionality

--- src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/SubAtlasCommand.java
+++ src/main/java/org/openstreetmap/atlas/utilities/command/subcommands/SubAtlasCommand.java
@@ -5,15 +5,15 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.geography.atlas.items.AtlasEntity;
 import org.openstreetmap.atlas.geography.atlas.sub.AtlasCutType;
-import org.openstreetmap.atlas.geography.converters.jts.JtsPolygonConverter;
 import org.openstreetmap.atlas.streaming.resource.File;
 import org.openstreetmap.atlas.streaming.resource.FileSuffix;
 import org.openstreetmap.atlas.utilities.collections.StringList;
-import org.openstreetmap.atlas.utilities.command.AtlasShellToolsException;
 import org.openstreetmap.atlas.utilities.command.abstractcommand.CommandOutputDelegate;
 import org.openstreetmap.atlas.utilities.command.abstractcommand.OptionAndArgumentDelegate;
 import org.openstreetmap.atlas.utilities.command.parsing.OptionOptionality;
@@ -21,10 +21,8 @@ import org.openstreetmap.atlas.utilities.command.subcommands.templates.AtlasLoad
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.io.ParseException;
-import com.vividsolutions.jts.io.WKTReader;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 
 /**
  * @author lcram
@@ -34,8 +32,12 @@ public class SubAtlasCommand extends AtlasLoaderCommand
     private static final Logger logger = LoggerFactory.getLogger(SubAtlasCommand.class);
 
     private static final String WKT_OPTION_LONG = "wkt";
-    private static final String WKT_OPTION_DESCRIPTION = "WKT of the polygon with which to cut";
+    private static final String WKT_OPTION_DESCRIPTION = "The WKT of the polygon with which to cut.";
     private static final String WKT_OPTION_HINT = "wkt";
+
+    private static final String PREDICATE_OPTION_LONG = "predicate";
+    private static final String PREDICATE_OPTION_DESCRIPTION = "The feature filter predicate for the subatlas, in Groovy.";
+    private static final String PREDICATE_OPTION_HINT = "groovy-predicate";
 
     private static final List<String> CUT_TYPE_STRINGS = Arrays.stream(AtlasCutType.values())
             .map(AtlasCutType::toString).collect(Collectors.toList());
@@ -89,8 +91,10 @@ public class SubAtlasCommand extends AtlasLoaderCommand
     {
         this.registerOptionWithRequiredArgument(WKT_OPTION_LONG, WKT_OPTION_DESCRIPTION,
                 OptionOptionality.REQUIRED, WKT_OPTION_HINT);
+        this.registerOptionWithRequiredArgument(PREDICATE_OPTION_LONG, PREDICATE_OPTION_DESCRIPTION,
+                OptionOptionality.REQUIRED, PREDICATE_OPTION_HINT, 3, 4);
         this.registerOptionWithRequiredArgument(CUT_TYPE_OPTION_LONG, CUT_TYPE_OPTION_DESCRIPTION,
-                OptionOptionality.OPTIONAL, CUT_TYPE_OPTION_HINT);
+                OptionOptionality.OPTIONAL, CUT_TYPE_OPTION_HINT, 3, 4);
         super.registerOptionsAndArguments();
     }
 
@@ -98,32 +102,42 @@ public class SubAtlasCommand extends AtlasLoaderCommand
     protected void processAtlas(final Atlas atlas, final String atlasFileName,
             final File atlasResource)
     {
-        final String wkt = this.optionAndArgumentDelegate.getOptionArgument(WKT_OPTION_LONG)
-                .orElseThrow(AtlasShellToolsException::new);
+        // final String wkt = this.optionAndArgumentDelegate.getOptionArgument(WKT_OPTION_LONG)
+        // .orElseThrow(AtlasShellToolsException::new);
 
-        final WKTReader reader = new WKTReader();
-        Geometry geometry = null;
-        try
-        {
-            geometry = reader.read(wkt);
-        }
-        catch (final ParseException exception)
-        {
-            logger.error("unable to parse {}", wkt, exception);
-        }
-
+        final Binding binding = new Binding();
+        final GroovyShell shell = new GroovyShell(binding);
+        // works, but you have to do
+        // "java.util.function.Predicate<org.openstreetmap.foo.AtlasEntity> pred = { blah -> true }"
+        // and you need to specify the full package and class name of everything
+        final Predicate<AtlasEntity> matcher = (Predicate<AtlasEntity>) shell.evaluate(
+                this.optionAndArgumentDelegate.getOptionArgument(PREDICATE_OPTION_LONG).get());
         final Optional<Atlas> subbedAtlas;
-        if (geometry instanceof Polygon)
-        {
-            final org.openstreetmap.atlas.geography.Polygon polygon = new JtsPolygonConverter()
-                    .backwardConvert((Polygon) geometry);
-            subbedAtlas = atlas.subAtlas(polygon, this.cutType);
-        }
-        else
-        {
-            this.outputDelegate.printlnErrorMessage("unsupported geometry type " + wkt);
-            subbedAtlas = Optional.empty();
-        }
+        subbedAtlas = atlas.subAtlas(matcher, AtlasCutType.SOFT_CUT);
+
+        // final WKTReader reader = new WKTReader();
+        // Geometry geometry = null;
+        // try
+        // {
+        // geometry = reader.read(wkt);
+        // }
+        // catch (final ParseException exception)
+        // {
+        // logger.error("unable to parse {}", wkt, exception);
+        // }
+        //
+        // //final Optional<Atlas> subbedAtlas;
+        // if (geometry instanceof Polygon)
+        // {
+        // final org.openstreetmap.atlas.geography.Polygon polygon = new JtsPolygonConverter()
+        // .backwardConvert((Polygon) geometry);
+        // subbedAtlas = atlas.subAtlas(polygon, this.cutType);
+        // }
+        // else
+        // {
+        // this.outputDelegate.printlnErrorMessage("unsupported geometry type " + wkt);
+        // subbedAtlas = Optional.empty();
+        // }
 
         if (subbedAtlas.isPresent())
         {
