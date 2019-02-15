b8620346f368921739b4d444895b3560e99bcf5f
Yazad Khambata <ykhambata@apple.com>
2019-02-14 20:51:53 -0800

    Tag change rule added.

--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/TagChangeTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/TagChangeTest.java
@@ -1,7 +1,18 @@
 package org.openstreetmap.atlas.geography.atlas.change;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 /**
  * @author Yazad Khambata
  */
 public class TagChangeTest {
+
+    @Rule
+    public TagChangeTestRule tagChangeTestRule = new TagChangeTestRule();
+
+    @Test
+    public void test() {
+        System.out.println(tagChangeTestRule.getAtlas());
+    }
 }
