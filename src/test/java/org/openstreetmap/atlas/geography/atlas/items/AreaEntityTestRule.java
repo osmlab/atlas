6d5609a645451dcde22931d7c2ade851a47cec6a
Yazad Khambata <ykhambata@apple.com>
2019-03-01 14:16:47 -0800

    Test cases

---
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/items/AreaEntityTestRule.java
@@ -0,0 +1,30 @@
+package org.openstreetmap.atlas.geography.atlas.items;
+
+import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Area;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Node;
+
+/**
+ * @author mgostintsev
+ */
+public class AreaEntityTestRule extends CoreTestRule
+{
+    private static final String ONE = "37,-122.00";
+    private static final String TWO = "37,-122.01";
+
+    @TestAtlas(
+
+            nodes = {
+                    @Node(id = "1", coordinates = @Loc(value = ONE), tags = {"name=abc"}),
+                    @Node(id = "2", coordinates = @Loc(value = TWO)),
+            })
+    private Atlas atlas;
+
+    public Atlas getAtlas()
+    {
+        return this.atlas;
+    }
+}
