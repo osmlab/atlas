c2cb56851beddae830987c91a9f1c440d53e7583
Jack Klamer <jfklamer@gmail.com>
2019-03-04 14:05:38 -0800

    Tests that will fail

--- src/test/java/org/openstreetmap/atlas/geography/SegmentTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/SegmentTest.java
@@ -8,6 +8,17 @@ import org.junit.Test;
  */
 public class SegmentTest
 {
+
+    private static Segment SEGMENT_SAME_END_1 = PolyLine
+            .wkt("LINESTRING (112.9699474 -84.7999528, 112.9699948 -84.7999669)").segments().get(0);
+    private static Segment SEGMENT_SAME_END_2 = PolyLine
+            .wkt("LINESTRING (112.9650809 -84.7999622, 112.9699948 -84.7999669)").segments().get(0);
+
+    private static Segment SEGMENT_SAME_END_1_ANTI_MERIDIAN = PolyLine
+            .wkt("LINESTRING (-180.0 90.0, -180.0 -90.0)").segments().get(0);
+    private static Segment SEGMENT_SAME_END_2_ANTI_MERIDIAN = PolyLine
+            .wkt("LINESTRING (179.9999999 90.0, -180.0 -90.0)").segments().get(0);
+
     @Test
     public void testIntersection()
     {
@@ -23,4 +34,27 @@ public class SegmentTest
         Assert.assertEquals(true, new Segment(Location.TEST_1, Location.TEST_3)
                 .intersects(new Segment(Location.TEST_4, Location.TEST_2)));
     }
+
+    @Test
+    public void testIntersectionWithSameEnd()
+    {
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_1.reversed().intersects(SEGMENT_SAME_END_2.reversed()));
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_2.reversed().intersects(SEGMENT_SAME_END_1.reversed()));
+        Assert.assertTrue("Same End is broken",
+                SEGMENT_SAME_END_1.intersects(SEGMENT_SAME_END_2));
+        Assert.assertTrue("Same End is broken",
+                SEGMENT_SAME_END_2.intersects(SEGMENT_SAME_END_1));
+
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_1_ANTI_MERIDIAN.reversed().intersects(SEGMENT_SAME_END_2_ANTI_MERIDIAN.reversed()));
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_2_ANTI_MERIDIAN.reversed().intersects(SEGMENT_SAME_END_1_ANTI_MERIDIAN.reversed()));
+        Assert.assertTrue("Same End is broken",
+                SEGMENT_SAME_END_1_ANTI_MERIDIAN.intersects(SEGMENT_SAME_END_2_ANTI_MERIDIAN));
+        Assert.assertTrue("Same End is broken",
+                SEGMENT_SAME_END_2_ANTI_MERIDIAN.intersects(SEGMENT_SAME_END_1_ANTI_MERIDIAN));
+    }
+
 }
