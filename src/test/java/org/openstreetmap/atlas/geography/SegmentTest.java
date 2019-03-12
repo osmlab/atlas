2a0d99bcf01c3ccb323266d0f00269dc6fe06cfc
Jack Klamer <jfklamer@gmail.com>
2019-03-12 11:34:51 -0700

    Segment intersection bug fix (#373)
    * Tests that will fail
    
    * Tests that will fail on overflow
    
    * apprximate as last resort
    
    * Sonar issues
    
    * Sonar cude final issues

--- src/test/java/org/openstreetmap/atlas/geography/SegmentTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/SegmentTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
  */
 public class SegmentTest
 {
+
+    private static Segment SEGMENT_SAME_END_1 = PolyLine
+            .wkt("LINESTRING (112.9699474 -84.7999528, 112.9699948 -84.7999669)").segments().get(0);
+    private static Segment SEGMENT_SAME_END_2 = PolyLine
+            .wkt("LINESTRING (112.9650809 -84.7999622, 112.9699948 -84.7999669)").segments().get(0);
+
     @Test
     public void testIntersection()
     {
@@ -22,5 +28,31 @@ public class SegmentTest
                         .intersection(new Segment(Location.TEST_4, Location.TEST_2)));
         Assert.assertEquals(true, new Segment(Location.TEST_1, Location.TEST_3)
                 .intersects(new Segment(Location.TEST_4, Location.TEST_2)));
+    }
+
+    @Test
+    public void testIntersectionWithSameEnd()
+    {
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_1.reversed().intersects(SEGMENT_SAME_END_2.reversed()));
+        Assert.assertTrue("Same start is broken.",
+                SEGMENT_SAME_END_2.reversed().intersects(SEGMENT_SAME_END_1.reversed()));
+        Assert.assertTrue("Same End is broken", SEGMENT_SAME_END_1.intersects(SEGMENT_SAME_END_2));
+        Assert.assertTrue("Same End is broken", SEGMENT_SAME_END_2.intersects(SEGMENT_SAME_END_1));
+    }
+
+    @Test
+    public void testIntersectionOverflow()
+    {
+        final Segment maxXandYMovement = new Segment(
+                new Location(Latitude.MINIMUM, Longitude.MINIMUM),
+                new Location(Latitude.MAXIMUM, Longitude.MAXIMUM));
+        final Segment maxXandNegativeYMovement = new Segment(
+                new Location(Latitude.MAXIMUM, Longitude.MINIMUM),
+                new Location(Latitude.MINIMUM, Longitude.MAXIMUM));
+
+        Assert.assertTrue("Overflow issue", maxXandYMovement.intersects(maxXandNegativeYMovement));
+        Assert.assertTrue("Overflow issue", maxXandNegativeYMovement.intersects(maxXandYMovement));
+
     }
 }
