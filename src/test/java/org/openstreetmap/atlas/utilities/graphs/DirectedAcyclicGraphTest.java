51f22c8553239a0921bc3a9a0c2647ae10d11b98
matthieun <matthieu.nahoum@gmail.com>
2019-03-26 13:58:00 -0700

    Fix source-sink bug with DAG processGroups

--- src/test/java/org/openstreetmap/atlas/utilities/graphs/DirectedAcyclicGraphTest.java
+++ src/test/java/org/openstreetmap/atlas/utilities/graphs/DirectedAcyclicGraphTest.java
@@ -24,6 +24,13 @@ import org.openstreetmap.atlas.utilities.collections.Sets;
  */
 public class DirectedAcyclicGraphTest
 {
+    private static final String PLATINUM = "Platinum";
+    private static final String GOLD = "Gold";
+    private static final String COPPER = "Copper";
+    private static final String PRECIOUS = "Precious";
+    private static final String COINAGE = "Coinage";
+    private static final String METAL = "Metal";
+
     private final DirectedAcyclicGraph<String> graph;
 
     public DirectedAcyclicGraphTest()
@@ -35,38 +42,38 @@ public class DirectedAcyclicGraphTest
     public void setup()
     {
         // Vertices
-        this.graph.addVertex("Platinum");
-        this.graph.addVertex("Gold");
-        this.graph.addVertex("Copper");
-        this.graph.addVertex("Precious");
-        this.graph.addVertex("Coinage");
-        this.graph.addVertex("Metal");
+        this.graph.addVertex(PLATINUM);
+        this.graph.addVertex(GOLD);
+        this.graph.addVertex(COPPER);
+        this.graph.addVertex(PRECIOUS);
+        this.graph.addVertex(COINAGE);
+        this.graph.addVertex(METAL);
 
         // Edges
-        this.graph.addEdge("Metal", "Coinage");
-        this.graph.addEdge("Metal", "Precious");
-        this.graph.addEdge("Precious", "Platinum");
-        this.graph.addEdge("Precious", "Gold");
-        this.graph.addEdge("Coinage", "Gold");
-        this.graph.addEdge("Coinage", "Copper");
+        this.graph.addEdge(METAL, COINAGE);
+        this.graph.addEdge(METAL, PRECIOUS);
+        this.graph.addEdge(PRECIOUS, PLATINUM);
+        this.graph.addEdge(PRECIOUS, GOLD);
+        this.graph.addEdge(COINAGE, GOLD);
+        this.graph.addEdge(COINAGE, COPPER);
     }
 
     @Test
     public void testDeepestLevel()
     {
-        assertEquals(this.graph.getDeepestLevel("Metal"), 1);
-        assertEquals(this.graph.getDeepestLevel("Gold"), 3);
+        assertEquals(this.graph.getDeepestLevel(METAL), 1);
+        assertEquals(this.graph.getDeepestLevel(GOLD), 3);
     }
 
     @Test
     public void testPaths()
     {
-        assertTrue(this.graph.hasPath("Metal", "Copper"));
-        assertTrue(this.graph.hasPath("Metal", "Platinum"));
-        assertFalse(this.graph.hasPath("Coinage", "Platinum"));
+        assertTrue(this.graph.hasPath(METAL, COPPER));
+        assertTrue(this.graph.hasPath(METAL, PLATINUM));
+        assertFalse(this.graph.hasPath(COINAGE, PLATINUM));
 
         // Adding this edge will cause a cycle, which is not allowed in a DAG
-        Assert.assertTrue(!this.graph.addEdge("Copper", "Metal"));
+        Assert.assertTrue(!this.graph.addEdge(COPPER, METAL));
     }
 
     @Test
@@ -74,9 +81,9 @@ public class DirectedAcyclicGraphTest
     {
         final List<Set<String>> processGroups = this.graph.processGroups();
         final List<Set<String>> expected = new ArrayList<>();
-        final Set<String> first = Sets.hashSet("Metal");
-        final Set<String> second = Sets.hashSet("Precious", "Coinage");
-        final Set<String> third = Sets.hashSet("Gold", "Platinum", "Copper");
+        final Set<String> first = Sets.hashSet(METAL);
+        final Set<String> second = Sets.hashSet(PRECIOUS, COINAGE);
+        final Set<String> third = Sets.hashSet(GOLD, PLATINUM, COPPER);
         expected.add(first);
         expected.add(second);
         expected.add(third);
@@ -84,32 +91,41 @@ public class DirectedAcyclicGraphTest
     }
 
     @Test
+    public void testProcessGroupsWithSourceSinks()
+    {
+        final DirectedAcyclicGraph<String> dag = new DirectedAcyclicGraph<>();
+        dag.addVertex("a");
+        dag.addVertex("b");
+        dag.addVertex("c");
+        final List<Set<String>> processGroups = dag.processGroups();
+        Assert.assertEquals(1, processGroups.size());
+    }
+
+    @Test
     public void testSinks()
     {
-        final Set<String> expectedSinks = new HashSet<>(
-                Arrays.asList("Platinum", "Gold", "Copper"));
-        assertTrue(this.graph.getSinks().stream().allMatch(sink -> expectedSinks.remove(sink))
+        final Set<String> expectedSinks = new HashSet<>(Arrays.asList(PLATINUM, GOLD, COPPER));
+        assertTrue(this.graph.getSinks().stream().allMatch(expectedSinks::remove)
                 && expectedSinks.isEmpty());
     }
 
     @Test
     public void testSources()
     {
-        final Set<String> expectedSources = new HashSet<>(Arrays.asList("Metal"));
-        assertTrue(
-                this.graph.getSources().stream().allMatch(source -> expectedSources.remove(source))
-                        && expectedSources.isEmpty());
+        final Set<String> expectedSources = new HashSet<>(Arrays.asList(METAL));
+        assertTrue(this.graph.getSources().stream().allMatch(expectedSources::remove)
+                && expectedSources.isEmpty());
     }
 
     @Test
     public void testTopologicalSort()
     {
         final Stack<String> topologicalSort = this.graph.getTopologicalSortedList();
-        Assert.assertEquals("Metal", topologicalSort.pop());
-        Assert.assertEquals("Coinage", topologicalSort.pop());
-        Assert.assertEquals("Precious", topologicalSort.pop());
-        Assert.assertEquals("Copper", topologicalSort.pop());
-        Assert.assertEquals("Gold", topologicalSort.pop());
-        Assert.assertEquals("Platinum", topologicalSort.pop());
+        Assert.assertEquals(METAL, topologicalSort.pop());
+        Assert.assertEquals(COINAGE, topologicalSort.pop());
+        Assert.assertEquals(PRECIOUS, topologicalSort.pop());
+        Assert.assertEquals(COPPER, topologicalSort.pop());
+        Assert.assertEquals(GOLD, topologicalSort.pop());
+        Assert.assertEquals(PLATINUM, topologicalSort.pop());
     }
 }
