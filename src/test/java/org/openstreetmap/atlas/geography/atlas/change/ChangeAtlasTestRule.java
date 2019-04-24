463cb7796bd876792d6fc29e1c61f50a3151291d
lucaspcram <lucaspcram@gmail.com>
2019-04-24 10:20:30 -0700

    Added test to expose bug

--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/ChangeAtlasTestRule.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/ChangeAtlasTestRule.java
@@ -3,17 +3,33 @@ package org.openstreetmap.atlas.geography.atlas.change;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.utilities.testing.CoreTestRule;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Loc;
+import org.openstreetmap.atlas.utilities.testing.TestAtlas.Point;
 
 /**
  * @author matthieun
  */
 public class ChangeAtlasTestRule extends CoreTestRule
 {
+    private static final String ONE = "15.420563,-61.336198";
+    private static final String TWO = "15.429499,-61.332850";
+
     @TestAtlas(loadFromJosmOsmResource = "ChangeAtlasTest.josm.osm")
     private Atlas atlas;
 
     @TestAtlas(loadFromJosmOsmResource = "ChangeAtlasTestEdge.josm.osm")
     private Atlas atlasEdge;
+
+    @TestAtlas(
+
+            points = {
+
+                    @Point(id = "1", coordinates = @Loc(value = ONE), tags = { "a=1", "b=2" }),
+
+            }
+
+    )
+    private Atlas tagAtlas;
 
     public Atlas getAtlas()
     {
@@ -23,5 +39,10 @@ public class ChangeAtlasTestRule extends CoreTestRule
     public Atlas getAtlasEdge()
     {
         return this.atlasEdge;
+    }
+
+    public Atlas getTagAtlas()
+    {
+        return this.tagAtlas;
     }
 }
