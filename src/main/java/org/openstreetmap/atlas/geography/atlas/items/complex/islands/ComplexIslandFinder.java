51938a3f5dc4863e16aedeae8de7fbc2ddda5f83
Shridhar Bhalekar <shridhar.bhalekar@gmail.com>
2019-05-01 17:39:38 -0700

    separate out configuration reader from ComplexWaterEntityFinder

--- src/main/java/org/openstreetmap/atlas/geography/atlas/items/complex/islands/ComplexIslandFinder.java
+++ src/main/java/org/openstreetmap/atlas/geography/atlas/items/complex/islands/ComplexIslandFinder.java
@@ -1,6 +1,5 @@
 package org.openstreetmap.atlas.geography.atlas.items.complex.islands;
 
-import java.util.Map;
 import java.util.Set;
 
 import org.openstreetmap.atlas.geography.atlas.Atlas;
@@ -8,14 +7,11 @@ import org.openstreetmap.atlas.geography.atlas.items.Relation;
 import org.openstreetmap.atlas.geography.atlas.items.Relation.Ring;
 import org.openstreetmap.atlas.geography.atlas.items.complex.Finder;
 import org.openstreetmap.atlas.geography.atlas.items.complex.waters.ComplexWaterEntityFinder;
-import org.openstreetmap.atlas.streaming.resource.File;
+import org.openstreetmap.atlas.geography.atlas.items.complex.waters.WaterConfigurationHandler;
 import org.openstreetmap.atlas.streaming.resource.InputStreamResource;
+import org.openstreetmap.atlas.streaming.resource.Resource;
 import org.openstreetmap.atlas.utilities.collections.Iterables;
 import org.openstreetmap.atlas.utilities.collections.Sets;
-import org.openstreetmap.atlas.utilities.configuration.Configuration;
-import org.openstreetmap.atlas.utilities.configuration.ConfigurationReader;
-import org.openstreetmap.atlas.utilities.configuration.ConfiguredFilter;
-import org.openstreetmap.atlas.utilities.configuration.StandardConfiguration;
 
 /**
  * @author Sid
@@ -23,42 +19,30 @@ import org.openstreetmap.atlas.utilities.configuration.StandardConfiguration;
  */
 public class ComplexIslandFinder implements Finder<ComplexIsland>
 {
-    private final Configuration configuration;
-
-    private final Set<String> validWaterTypes;
+    private final WaterConfigurationHandler waterConfigurationHandler;
 
     private static final Set<String> ACCEPTABLE_WATER_FEATURES = Sets.hashSet("lake", "river",
             "reservoir");
 
     public ComplexIslandFinder()
     {
-        this.configuration = new StandardConfiguration(
-                new InputStreamResource(() -> ComplexWaterEntityFinder.class
-                        .getResourceAsStream(ComplexWaterEntityFinder.WATER_RESOURCE)));
-        this.validWaterTypes = getWaterTypesFromConfiguration();
+        this(new InputStreamResource(() -> ComplexWaterEntityFinder.class
+                .getResourceAsStream(ComplexWaterEntityFinder.WATER_RESOURCE)));
     }
 
-    public ComplexIslandFinder(final String resource)
+    public ComplexIslandFinder(final Resource resource)
     {
-        this.configuration = new StandardConfiguration(new File(resource));
-        this.validWaterTypes = getWaterTypesFromConfiguration();
-    }
-
-    private Set<String> getWaterTypesFromConfiguration()
-    {
-        final Set<String> validWaterTypes = new ConfigurationReader(
-                ConfiguredFilter.CONFIGURATION_ROOT).configurationValue(configuration,
-                        Map<String, Object>::keySet);
-
-        // Right now we handle only islands in Lakes, Rivers and Reservoirs.
-        validWaterTypes.retainAll(ACCEPTABLE_WATER_FEATURES);
-        return validWaterTypes;
+        this.waterConfigurationHandler = new WaterConfigurationHandler(resource);
     }
 
     private boolean testWaterType(final Relation relation)
     {
-        return this.validWaterTypes.stream()
-                .anyMatch(type -> ConfiguredFilter.from(type, configuration).test(relation));
+        return this.waterConfigurationHandler.getWaterHandlers().entrySet().stream()
+                .filter(entry -> ACCEPTABLE_WATER_FEATURES.contains(entry.getKey()))
+                .anyMatch(entry ->
+                {
+                    return entry.getValue().test(relation);
+                });
     }
 
     @Override
