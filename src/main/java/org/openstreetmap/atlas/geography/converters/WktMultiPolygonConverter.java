d4db7e341dc62fc5a2d8c41caaafae7111dc61e6
matthieun <matthieu.nahoum@gmail.com>
2019-05-23 13:04:28 -0700

    Pbf2Atlas command

--- src/main/java/org/openstreetmap/atlas/geography/converters/WktMultiPolygonConverter.java
+++ src/main/java/org/openstreetmap/atlas/geography/converters/WktMultiPolygonConverter.java
@@ -1,11 +1,14 @@
 package org.openstreetmap.atlas.geography.converters;
 
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 import org.locationtech.jts.io.WKTWriter;
 import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.MultiPolygon;
 import org.openstreetmap.atlas.geography.converters.jts.JtsMultiPolygonToMultiPolygonConverter;
+import org.openstreetmap.atlas.geography.converters.jts.JtsPolygonConverter;
 import org.openstreetmap.atlas.utilities.conversion.TwoWayConverter;
 
 /**
@@ -18,17 +21,29 @@ public class WktMultiPolygonConverter implements TwoWayConverter<MultiPolygon, S
     @Override
     public MultiPolygon backwardConvert(final String wkt)
     {
-        org.locationtech.jts.geom.MultiPolygon geometry = null;
         final WKTReader myReader = new WKTReader();
         try
         {
-            geometry = (org.locationtech.jts.geom.MultiPolygon) myReader.read(wkt);
+            final Geometry result = myReader.read(wkt);
+            if (result instanceof org.locationtech.jts.geom.Polygon)
+            {
+                return MultiPolygon
+                        .forPolygon(new JtsPolygonConverter().backwardConvert((Polygon) result));
+            }
+            else if (result instanceof org.locationtech.jts.geom.MultiPolygon)
+            {
+                return new JtsMultiPolygonToMultiPolygonConverter()
+                        .convert((org.locationtech.jts.geom.MultiPolygon) result);
+            }
+            else
+            {
+                throw new CoreException("Unknown type: {}", result.getClass().getCanonicalName());
+            }
         }
         catch (final ParseException | ClassCastException e)
         {
-            throw new CoreException("Cannot parse wkt : {}", wkt);
+            throw new CoreException("Cannot parse wkt : {}", wkt, e);
         }
-        return new JtsMultiPolygonToMultiPolygonConverter().convert(geometry);
     }
 
     @Override
