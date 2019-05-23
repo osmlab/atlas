e08c7174bdc30461bb86d12af4c523a27eb4ef5e
Yazad Khambata <yazad3@gmail.com>
2019-05-23 09:17:22 -0700

    Multi Level Cascade Test extended to add 2 more levels of cascade into Relationships (#442)
    * Multi Cascade between Edge and Node tested
    
    * formatting
    
    * Cascade level 3 changes
    
    * Conflicit resolved
    
    * rule updated

--- src/test/java/org/openstreetmap/atlas/geography/atlas/change/MultiCascadeDeleteTestRule.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/change/MultiCascadeDeleteTestRule.java
@@ -16,16 +16,17 @@ public class MultiCascadeDeleteTestRule extends CoreTestRule
     public static final String strNodeA = "1";
     public static final String strNodeB = "2";
     public static final String strNodeC = "3";
-
     public static final String strEdgeA = "1";
     public static final String strEdgeB = "2";
+    public static final String strRelationX = "1";
 
     public static final Long nodeA = Long.valueOf(strNodeA);
     public static final Long nodeB = Long.valueOf(strNodeB);
     public static final Long nodeC = Long.valueOf(strNodeC);
 
     public static final Long edgeA = Long.valueOf(strEdgeA);
-    public static final Long edgeB = Long.valueOf(strEdgeB);
+
+    public static final Long relationX = Long.valueOf(strRelationX);
 
     @TestAtlas(nodes = {
             @TestAtlas.Node(id = strNodeA, coordinates = @TestAtlas.Loc(value = LOC_A)),
@@ -36,7 +37,12 @@ public class MultiCascadeDeleteTestRule extends CoreTestRule
                     @TestAtlas.Edge(id = strEdgeA, coordinates = { @TestAtlas.Loc(value = LOC_A),
                             @TestAtlas.Loc(value = LOC_B) }),
                     @TestAtlas.Edge(id = strEdgeB, coordinates = { @TestAtlas.Loc(value = LOC_B),
-                            @TestAtlas.Loc(value = LOC_C) }) })
+                            @TestAtlas.Loc(value = LOC_C) }), },
+
+            relations = { @TestAtlas.Relation(id = strRelationX, members = {
+                    @TestAtlas.Relation.Member(id = strEdgeA, role = "x", type = "edge"),
+                    @TestAtlas.Relation.Member(id = strEdgeB, role = "y", type = "edge"),
+                    @TestAtlas.Relation.Member(id = strNodeB, role = "y", type = "node"), }) })
     private Atlas atlas;
 
     public Atlas getAtlas()
