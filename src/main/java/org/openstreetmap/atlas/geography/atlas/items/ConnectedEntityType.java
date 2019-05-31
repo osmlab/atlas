0a1bfe5aa642d39e7a657ec37b3fea5e9530bf3c
Yazad Khambata <ykhambata@apple.com>
2019-05-31 15:04:24 -0700

    Start and End Node connected type.

--- src/main/java/org/openstreetmap/atlas/geography/atlas/items/ConnectedEntityType.java
+++ src/main/java/org/openstreetmap/atlas/geography/atlas/items/ConnectedEntityType.java
@@ -3,11 +3,16 @@ package org.openstreetmap.atlas.geography.atlas.items;
 import java.util.function.Function;
 
 /**
+ * An interface that generalizes enum based declarative connectivity information.
+ *
+ * @param <ME> - The current entity.
+ * @param <CONNECTED> - The connected entity.
+ *
  * @author Yazad Khambata
  */
-public interface ConnectedEntityType<ME extends AtlasEntity, RELATED>
+public interface ConnectedEntityType<ME extends AtlasEntity, CONNECTED>
 {
     String getPropertyName();
 
-    Function<ME, RELATED> getAccessFunction();
+    Function<ME, CONNECTED> getAccessFunction();
 }
