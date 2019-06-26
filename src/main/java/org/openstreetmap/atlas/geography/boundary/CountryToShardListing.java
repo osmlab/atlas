2e7f68aaeca85b027935d0cc6e6983f67a0ff052
Sam Gass <adahn6@gmail.com>
2019-06-26 16:28:19 -0700

    reverted issue with country to shard listing

--- src/main/java/org/openstreetmap/atlas/geography/boundary/CountryToShardListing.java
+++ src/main/java/org/openstreetmap/atlas/geography/boundary/CountryToShardListing.java
@@ -58,9 +58,8 @@ public class CountryToShardListing extends Command
         try (SafeBufferedWriter writer = output.writer())
         {
             CountryShardListing.countryToShardList(countries, boundaries, sharding)
-                    .forEach((country, shardSet) -> shardSet.forEach(
-                            shard -> writer.writeLine(new CountryShard(country, shard).toString()
-                                    + " " + new CountryShard(country, shard).bounds())));
+                    .forEach((country, shardSet) -> shardSet.forEach(shard -> writer
+                            .writeLine(new CountryShard(country, shard).toString())));
         }
         catch (final Exception e)
         {
