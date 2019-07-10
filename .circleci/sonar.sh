1fa69cf9f3dca091bd5481720da4ae9c55bbbddb
matthieun <matthieu.nahoum@gmail.com>
2019-07-09 18:10:40 -0700

    Get the target branch

--- .circleci/sonar.sh
+++ .circleci/sonar.sh
@@ -3,6 +3,7 @@
 if [ -z "$CIRCLE_PR_NUMBER" ];
 then
     echo "Running sonarqube in a regular build"
+    echo "sonar.branch.name=$CIRCLE_BRANCH"
 	#TODO: Remove echo below
 	echo ./gradlew jacocoTestReport sonarqube \
 		-Dsonar.branch.name=$CIRCLE_BRANCH \
@@ -13,11 +14,12 @@ then
 		-Dsonar.coverage.jacoco.xmlReportPaths=build/reports/jacoco/test/jacocoTestReport.xml
 else
     CIRCLE_PR_BRANCH=`curl -s https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER | jq -r '.head.ref'`
+    CIRCLE_PR_TARGET_BRANCH=`curl -s https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$CIRCLE_PR_NUMBER | jq -r '.base.ref'`
 	SONAR_PULLREQUEST_BRANCH="$CIRCLE_PR_USERNAME/$CIRCLE_PR_BRANCH"
 	echo "Running sonarqube in Pull Request $CIRCLE_PR_NUMBER"
 	echo "sonar.pullrequest.key=$CIRCLE_PR_NUMBER"
 	echo "sonar.pullrequest.branch=$SONAR_PULLREQUEST_BRANCH"
-	echo "sonar.pullrequest.base=$CIRCLE_BRANCH"
+	echo "sonar.pullrequest.base=$CIRCLE_PR_TARGET_BRANCH"
 	#TODO: Remove echo below
 	echo ./gradlew jacocoTestReport sonarqube \
 		-Dsonar.organization=osmlab \
@@ -27,7 +29,7 @@ else
 		-Dsonar.coverage.jacoco.xmlReportPaths=build/reports/jacoco/test/jacocoTestReport.xml \
 		-Dsonar.pullrequest.key=$CIRCLE_PR_NUMBER \
 		-Dsonar.pullrequest.branch=$SONAR_PULLREQUEST_BRANCH \
-		-Dsonar.pullrequest.base=$CIRCLE_BRANCH \
+		-Dsonar.pullrequest.base=$CIRCLE_PR_TARGET_BRANCH \
 		-Dsonar.pullrequest.provider=github \
 		-Dsonar.pullrequest.github.repository=osmlab/atlas \
 		-Dsonar.pullrequest.github.endpoint=https://api.github.com/ \
