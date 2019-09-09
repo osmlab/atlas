3d6280f943e727b42d87af5648083399c1e18362
matthieun <matthieu.nahoum@gmail.com>
2019-09-09 13:46:43 -0700

    Truncate after message re-construction

--- src/test/java/org/openstreetmap/atlas/exception/change/FeatureChangeMergeExceptionTest.java
+++ src/test/java/org/openstreetmap/atlas/exception/change/FeatureChangeMergeExceptionTest.java
@@ -2,6 +2,7 @@ package org.openstreetmap.atlas.exception.change;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openstreetmap.atlas.exception.CoreException;
 
 /**
  * @author matthieun
@@ -23,5 +24,24 @@ public class FeatureChangeMergeExceptionTest
         Assert.assertEquals(2100, two.length());
         Assert.assertEquals(FeatureChangeMergeException.MAXIMUM_MESSAGE_SIZE,
                 FeatureChangeMergeException.truncate(two).length());
+    }
+
+    @Test
+    public void testTruncateInConstructor()
+    {
+        final String one = "abc{}d";
+        final StringBuilder twoBuilder = new StringBuilder();
+        for (int index = 0; index < 2100; index++)
+        {
+            twoBuilder.append("a");
+        }
+        final String two = twoBuilder.toString();
+
+        final FeatureChangeMergeException fcme = new FeatureChangeMergeException(
+                MergeFailureType.HIGHEST_LEVEL_MERGE_FAILURE, one, two,
+                new CoreException("I am the cause"));
+        Assert.assertEquals(FeatureChangeMergeException.MAXIMUM_MESSAGE_SIZE,
+                fcme.getMessage().length());
+        Assert.assertEquals(CoreException.class, fcme.getCause().getClass());
     }
 }
