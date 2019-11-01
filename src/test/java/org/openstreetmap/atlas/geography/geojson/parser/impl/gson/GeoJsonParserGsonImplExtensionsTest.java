8bc11584fedc99bbfd31b20a8ed5d054a53e64f3
Yazad Khambata <ykhambata@apple.com>
2019-11-01 12:08:26 -0700

    exception tests

--- src/test/java/org/openstreetmap/atlas/geography/geojson/parser/impl/gson/GeoJsonParserGsonImplExtensionsTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/geojson/parser/impl/gson/GeoJsonParserGsonImplExtensionsTest.java
@@ -16,161 +16,161 @@ import org.slf4j.LoggerFactory;
  */
 public class GeoJsonParserGsonImplExtensionsTest extends AbstractGeoJsonParserGsonImplTest
 {
-    private static final Logger log = LoggerFactory.getLogger(
-            GeoJsonParserGsonImplExtensionsTest.class);
-    
+    private static final Logger log = LoggerFactory
+            .getLogger(GeoJsonParserGsonImplExtensionsTest.class);
+
     @Test
     public void beanA()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final BeanA beanA = geoJsonParser.deserializeExtension(json, BeanA.class);
         log.info("beanA:: {}.", beanA);
-        
+
         Assert.assertNotNull(beanA);
         Assert.assertFalse(beanA.getTags().isEmpty());
     }
-    
+
     @Test
     public void beanBWithArray()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final BeanB beanB = geoJsonParser.deserializeExtension(json, BeanB.class);
         log.info("beanB:: {}.", beanB);
-        
+
         Assert.assertNotNull(beanB);
         Assert.assertFalse(beanB.getBeanA().getTags().isEmpty());
         Assert.assertEquals(2, beanB.getBeanAs().length);
     }
-    
+
     @Test
     public void beanBWithoutArray()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final BeanB beanB = geoJsonParser.deserializeExtension(json, BeanB.class);
         log.info("beanB:: {}.", beanB);
-        
+
         Assert.assertNotNull(beanB);
         Assert.assertFalse(beanB.getBeanA().getTags().isEmpty());
     }
-    
+
     @Test
     public void description1()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final Description description = geoJsonParser.deserializeExtension(json, Description.class);
         log.info("description:: {}.", description);
-        
+
         Assert.assertNotNull(description);
         Assert.assertEquals(9, description.getDescriptors().length);
     }
-    
+
     @Test
     public void descriptor1()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final Descriptor descriptor = geoJsonParser.deserializeExtension(json, Descriptor.class);
         log.info("descriptor:: {}.", descriptor);
-        
+
         Assert.assertNotNull(descriptor);
         Assert.assertEquals("ADD", descriptor.getType());
     }
-    
+
     @Test
     public void descriptor2()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final Descriptor descriptor = geoJsonParser.deserializeExtension(json, Descriptor.class);
         log.info("descriptor:: {}.", descriptor);
-        
+
         Assert.assertNotNull(descriptor);
         Assert.assertEquals("UPDATE", descriptor.getType());
     }
-    
+
     @Test
     public void descriptor3()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         final Descriptor descriptor = geoJsonParser.deserializeExtension(json, Descriptor.class);
         log.info("descriptor:: {}.", descriptor);
-        
+
         Assert.assertNotNull(descriptor);
         Assert.assertEquals("b", descriptor.getKey());
         Assert.assertEquals("2a", descriptor.getValue());
     }
-    
+
     @Test
     public void featureChangeProperties()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
-        final FeatureChangeProperties featureChangeProperties = geoJsonParser.deserializeExtension(
-                json, FeatureChangeProperties.class);
+
+        final FeatureChangeProperties featureChangeProperties = geoJsonParser
+                .deserializeExtension(json, FeatureChangeProperties.class);
         log.info("featureChangeProperties:: {}.", featureChangeProperties);
-        
+
         Assert.assertEquals(9, featureChangeProperties.getDescription().getDescriptors().length);
         Assert.assertFalse(featureChangeProperties.getWKT().isEmpty());
         Assert.assertFalse(featureChangeProperties.getBboxWKT().isEmpty());
     }
-    
+
     @Test
     public void featureChangePropertiesBad1()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         try
         {
-            final FeatureChangeProperties featureChangeProperties = geoJsonParser.deserializeExtension(
-                    json, FeatureChangeProperties.class);
+            final FeatureChangeProperties featureChangeProperties = geoJsonParser
+                    .deserializeExtension(json, FeatureChangeProperties.class);
         }
         catch (final Exception e)
         {
             final String message1 = e.getCause().getMessage();
             Assert.assertTrue(message1.startsWith("Population failed. propertyDescriptor name"));
-            String message2 = e.getCause().getCause().getCause().getMessage();
+            final String message2 = e.getCause().getCause().getCause().getMessage();
             Assert.assertTrue(
-                    message2.equals("Can not call newInstance() on the Class for java.lang.Class"));
+                    "Can not call newInstance() on the Class for java.lang.Class".equals(message2));
             return;
         }
-        
+
         Assert.fail("field name `class` should have caused a failure.");
     }
-    
+
     @Test
     public void featureChangePropertiesBad2()
     {
         final String json = extractJsonForExtension();
-        
+
         final GeoJsonParser geoJsonParser = GeoJsonParserGsonImpl.instance;
-        
+
         try
         {
-            final FeatureChangeProperties featureChangeProperties = geoJsonParser.deserializeExtension(
-                    json, FeatureChangeProperties.class);
+            final FeatureChangeProperties featureChangeProperties = geoJsonParser
+                    .deserializeExtension(json, FeatureChangeProperties.class);
         }
         catch (final Exception e)
         {
@@ -180,7 +180,7 @@ public class GeoJsonParserGsonImplExtensionsTest extends AbstractGeoJsonParserGs
             Assert.assertEquals(ClassCastException.class, e.getCause().getCause().getClass());
             return;
         }
-        
+
         Assert.fail("data data value should have caused a failure.");
     }
 }
