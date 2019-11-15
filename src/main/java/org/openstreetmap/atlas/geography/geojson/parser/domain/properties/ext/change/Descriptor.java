3cf77c34e19b62a87cdc113b7a8b5bfe0e2d8f1d
Yazad Khambata <ykhambata@apple.com>
2019-11-15 09:49:55 -0800

    geojson one word

--- src/main/java/org/openstreetmap/atlas/geography/geojson/parser/domain/properties/ext/change/Descriptor.java
+++ src/main/java/org/openstreetmap/atlas/geography/geojson/parser/domain/properties/ext/change/Descriptor.java
@@ -13,144 +13,111 @@ import org.openstreetmap.atlas.geography.geojson.parser.domain.annotation.Foreig
 @Foreign
 public class Descriptor implements Serializable
 {
-    private Long id;
     private String name;
     private String type;
-    private String itemType;
-    private String role;
     private String key;
     private String value;
     private String originalValue;
     private String position;
     private String beforeView;
     private String afterView;
-
+    
     public Descriptor()
     {
     }
-
+    
     @Override
     public boolean equals(final Object that)
     {
         return EqualsBuilder.reflectionEquals(this, that);
     }
-
+    
     public String getAfterView()
     {
         return this.afterView;
     }
-
+    
     public String getBeforeView()
     {
         return this.beforeView;
     }
-
-    public Long getId()
-    {
-        return this.id;
-    }
-
-    public String getItemType()
-    {
-        return this.itemType;
-    }
-
+    
     public String getKey()
     {
         return this.key;
     }
-
+    
     public String getName()
     {
         return this.name;
     }
-
+    
     public String getOriginalValue()
     {
         return this.originalValue;
     }
-
+    
     public String getPosition()
     {
         return this.position;
     }
-
-    public String getRole()
-    {
-        return this.role;
-    }
-
+    
     public String getType()
     {
         return this.type;
     }
-
+    
     public String getValue()
     {
         return this.value;
     }
-
+    
     @Override
     public int hashCode()
     {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-
+    
     public void setAfterView(final String afterView)
     {
         this.afterView = afterView;
     }
-
+    
     public void setBeforeView(final String beforeView)
     {
         this.beforeView = beforeView;
     }
-
-    public void setId(final Long id)
-    {
-        this.id = id;
-    }
-
-    public void setItemType(final String itemType)
-    {
-        this.itemType = itemType;
-    }
-
+    
     public void setKey(final String key)
     {
         this.key = key;
     }
-
+    
     public void setName(final String name)
     {
         this.name = name;
     }
-
+    
     public void setOriginalValue(final String originalValue)
     {
         this.originalValue = originalValue;
     }
-
+    
     public void setPosition(final String position)
     {
         this.position = position;
     }
-
-    public void setRole(final String role)
-    {
-        this.role = role;
-    }
-
+    
     public void setType(final String type)
     {
         this.type = type;
     }
-
+    
     public void setValue(final String value)
     {
         this.value = value;
     }
-
+    
     @Override
     public String toString()
     {
