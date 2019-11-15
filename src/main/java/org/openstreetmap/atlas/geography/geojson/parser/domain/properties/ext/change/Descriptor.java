0803dfc225399500ab8e966c9fb0c5b0b20c96db
Yazad Khambata <ykhambata@apple.com>
2019-11-15 09:51:38 -0800

    geojson one word

--- src/main/java/org/openstreetmap/atlas/geography/geojson/parser/domain/properties/ext/change/Descriptor.java
+++ src/main/java/org/openstreetmap/atlas/geography/geojson/parser/domain/properties/ext/change/Descriptor.java
@@ -21,103 +21,103 @@ public class Descriptor implements Serializable
     private String position;
     private String beforeView;
     private String afterView;
-    
+
     public Descriptor()
     {
     }
-    
+
     @Override
     public boolean equals(final Object that)
     {
         return EqualsBuilder.reflectionEquals(this, that);
     }
-    
+
     public String getAfterView()
     {
         return this.afterView;
     }
-    
+
     public String getBeforeView()
     {
         return this.beforeView;
     }
-    
+
     public String getKey()
     {
         return this.key;
     }
-    
+
     public String getName()
     {
         return this.name;
     }
-    
+
     public String getOriginalValue()
     {
         return this.originalValue;
     }
-    
+
     public String getPosition()
     {
         return this.position;
     }
-    
+
     public String getType()
     {
         return this.type;
     }
-    
+
     public String getValue()
     {
         return this.value;
     }
-    
+
     @Override
     public int hashCode()
     {
         return HashCodeBuilder.reflectionHashCode(this);
     }
-    
+
     public void setAfterView(final String afterView)
     {
         this.afterView = afterView;
     }
-    
+
     public void setBeforeView(final String beforeView)
     {
         this.beforeView = beforeView;
     }
-    
+
     public void setKey(final String key)
     {
         this.key = key;
     }
-    
+
     public void setName(final String name)
     {
         this.name = name;
     }
-    
+
     public void setOriginalValue(final String originalValue)
     {
         this.originalValue = originalValue;
     }
-    
+
     public void setPosition(final String position)
     {
         this.position = position;
     }
-    
+
     public void setType(final String type)
     {
         this.type = type;
     }
-    
+
     public void setValue(final String value)
     {
         this.value = value;
     }
-    
+
     @Override
     public String toString()
     {
