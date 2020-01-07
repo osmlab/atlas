4d3d38a92be419b14ca98e087733162f8580659c
lucaspcram <lucaspcram@gmail.com>
2020-01-07 15:57:18 -0800

    Working on fix for null name issue

--- src/main/java/org/openstreetmap/atlas/streaming/resource/FileSuffix.java
+++ src/main/java/org/openstreetmap/atlas/streaming/resource/FileSuffix.java
@@ -31,7 +31,7 @@ public enum FileSuffix
     WKT(".wkt"),
     WKB(".wkb");
 
-    private String suffix;
+    private final String suffix;
 
     public static FileSuffix getEnum(final String value)
     {
@@ -42,19 +42,13 @@ public enum FileSuffix
     public static Predicate<Path> pathFilter(final FileSuffix... listOfSuffixes)
     {
         final String suffix = Joiner.on("").join(listOfSuffixes);
-        return path ->
-        {
-            return path.getFileName().toString().toLowerCase().endsWith(suffix);
-        };
+        return path -> path.getFileName().toString().toLowerCase().endsWith(suffix);
     }
 
     public static Predicate<Resource> resourceFilter(final FileSuffix... listOfSuffixes)
     {
         final String suffix = Joiner.on("").join(listOfSuffixes);
-        return resource ->
-        {
-            return resource.getName() == null || resource.getName().endsWith(suffix);
-        };
+        return resource -> resource.getName() == null || resource.getName().endsWith(suffix);
     }
 
     public static Optional<FileSuffix> suffixFor(final String value)
