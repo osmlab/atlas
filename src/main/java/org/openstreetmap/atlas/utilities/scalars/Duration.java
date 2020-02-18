9e103aa1c2f38e7440b040cad1a9b054c96c7f1a
matthieun <matthieu.nahoum@gmail.com>
2020-02-17 16:45:43 -0800

    ordering

--- src/main/java/org/openstreetmap/atlas/utilities/scalars/Duration.java
+++ src/main/java/org/openstreetmap/atlas/utilities/scalars/Duration.java
@@ -19,9 +19,9 @@ public final class Duration implements Serializable, Comparable<Duration>
     public static final Duration ONE_SECOND = seconds(1);
     public static final Duration ZERO = milliseconds(0);
     public static final Duration MAXIMUM = milliseconds(Long.MAX_VALUE);
+
     private static final long NANOSECONDS_PER_MILLISECONDS = 1_000_000;
     private static final long MILLISECONDS_PER_SECOND = 1000;
-
     private static final long SECONDS_PER_MINUTE = 60;
     private static final long MINUTES_PER_HOUR = 60;
 
