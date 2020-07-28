0578eb423975ca666ad595af66e0d8f056dedd94
sayana-saithu <saithusayana@gmail.com>
2020-07-28 10:05:23 -0700

    Update unit tests

--- src/test/java/org/openstreetmap/atlas/tags/annotations/extraction/LengthExtractorTest.java
+++ src/test/java/org/openstreetmap/atlas/tags/annotations/extraction/LengthExtractorTest.java
@@ -75,6 +75,13 @@ public class LengthExtractorTest
     }
 
     @Test
+    public void validMilesWithCommaDecimalSeparatorTest()
+    {
+        Assert.assertEquals(Optional.of(Distance.miles(20.54)),
+                LengthExtractor.validateAndExtract("20,54 mi"));
+    }
+
+    @Test
     public void validNauticalMilesMixedCapsTest()
     {
         Assert.assertEquals(Optional.of(Distance.nauticalMiles(20.543)),
@@ -96,7 +103,7 @@ public class LengthExtractorTest
     }
 
     @Test
-    public void validNumberWithCommaDescimalSeparatorTest()
+    public void validNumberWithCommaDecimalSeparatorTest()
     {
         Assert.assertEquals(Optional.of(Distance.meters(20.5)),
                 LengthExtractor.validateAndExtract("20,5"));
