47d4b9c2b0a823b458164e63507b9d1ef82c71ac
lucaspcram <lucaspcram@gmail.com>
2020-10-01 16:04:58 -0700

    Refactored PbfToAtlasCommand test

--- src/test/java/org/openstreetmap/atlas/utilities/command/subcommands/PbfToAtlasCommandTest.java
+++ src/test/java/org/openstreetmap/atlas/utilities/command/subcommands/PbfToAtlasCommandTest.java
@@ -1,16 +1,24 @@
 package org.openstreetmap.atlas.utilities.command.subcommands;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileSystem;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.AtlasResourceLoader;
 import org.openstreetmap.atlas.streaming.resource.File;
 import org.openstreetmap.atlas.streaming.resource.InputStreamResource;
 import org.openstreetmap.atlas.streaming.resource.Resource;
 import org.openstreetmap.atlas.streaming.resource.StringResource;
-import org.openstreetmap.atlas.utilities.collections.StringList;
 import org.openstreetmap.atlas.utilities.testing.OsmFileParser;
 import org.openstreetmap.atlas.utilities.testing.OsmFileToPbf;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 
 /**
  * @author matthieun
@@ -18,41 +26,49 @@ import org.openstreetmap.atlas.utilities.testing.OsmFileToPbf;
 public class PbfToAtlasCommandTest
 {
     @Test
-    public void testCommand()
+    public void test()
     {
-        final File folder = File.temporaryFolder();
-        try
+        try (FileSystem filesystem = Jimfs.newFileSystem(Configuration.osX()))
         {
-            final Resource resource = new InputStreamResource(() -> PbfToAtlasCommandTest.class
-                    .getResourceAsStream("testPbf2Atlas.josm.osm"));
-            final File pbfFile = folder.child("PbfToAtlasCommandTest.pbf");
-            final StringResource osmFile = new StringResource();
-            new OsmFileParser().update(resource, osmFile);
-            new OsmFileToPbf().update(osmFile, pbfFile);
+            setupFilesystem1(filesystem);
+            final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+            final PbfToAtlasCommand command = new PbfToAtlasCommand();
+            command.setNewFileSystem(filesystem);
+            command.setNewOutStream(new PrintStream(outContent));
+            command.setNewErrStream(new PrintStream(errContent));
 
-            final StringList arguments = new StringList();
-            arguments.add(pbfFile.getAbsolutePathString());
-            arguments.add("--output=" + folder.getAbsolutePathString());
-            arguments.add("--countryName=FRA");
+            command.runSubcommand("--verbose", "--output=/Users/foo", "--countryName=FRA",
+                    "/Users/foo/PbfToAtlasCommandTest.pbf");
 
-            final String[] args = new String[arguments.size()];
-            for (int index = 0; index < arguments.size(); index++)
-            {
-                args[index] = arguments.get(index);
-            }
-            new PbfToAtlasCommand().runSubcommand(args);
-            final Atlas atlas = new AtlasResourceLoader()
-                    .load(folder.child("FRA_PbfToAtlasCommandTest.atlas"));
-            System.out.println("Atlas result: " + atlas);
-            Assert.assertEquals(5, atlas.numberOfNodes());
-            Assert.assertEquals(4, atlas.numberOfEdges());
-            Assert.assertEquals(1, atlas.numberOfAreas());
-            Assert.assertEquals(1, atlas.numberOfLines());
-            Assert.assertEquals(1, atlas.numberOfRelations());
+            Assert.assertEquals("/Users/foo/FRA_PbfToAtlasCommandTest.atlas\n",
+                    outContent.toString());
+            Assert.assertEquals("pbf2atlas: loading /Users/foo/PbfToAtlasCommandTest.pbf\n",
+                    errContent.toString());
+            final File outputAtlasFile = new File("/Users/foo/FRA_PbfToAtlasCommandTest.atlas",
+                    filesystem);
+            final Atlas outputAtlas = new AtlasResourceLoader()
+                    .load(new InputStreamResource(outputAtlasFile::read)
+                            .withName(outputAtlasFile.getAbsolutePathString()));
+            Assert.assertEquals(5, outputAtlas.numberOfNodes());
+            Assert.assertEquals(4, outputAtlas.numberOfEdges());
+            Assert.assertEquals(1, outputAtlas.numberOfAreas());
+            Assert.assertEquals(1, outputAtlas.numberOfLines());
+            Assert.assertEquals(1, outputAtlas.numberOfRelations());
         }
-        finally
+        catch (final IOException exception)
         {
-            folder.deleteRecursively();
+            throw new CoreException("FileSystem operation failed", exception);
         }
+    }
+
+    private void setupFilesystem1(final FileSystem filesystem) throws IOException
+    {
+        final File pbfFile = new File("/Users/foo/PbfToAtlasCommandTest.pbf", filesystem);
+        final Resource resource = new InputStreamResource(
+                () -> PbfToAtlasCommandTest.class.getResourceAsStream("testPbf2Atlas.josm.osm"));
+        final StringResource osmFile = new StringResource();
+        new OsmFileParser().update(resource, osmFile);
+        new OsmFileToPbf().update(osmFile, pbfFile);
     }
 }
