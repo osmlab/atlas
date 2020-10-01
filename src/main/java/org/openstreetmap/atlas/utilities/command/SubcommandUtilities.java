689bae60941d5b28e417e4a058dc7222c24fb6b5
lucaspcram <lucaspcram@gmail.com>
2020-10-01 15:26:02 -0700

    WIP subcommand utilities

---
+++ src/main/java/org/openstreetmap/atlas/utilities/command/SubcommandUtilities.java
@@ -0,0 +1,14 @@
+package org.openstreetmap.atlas.utilities.command;
+
+/**
+ * Some static utilities that may be useful for various subcommands.
+ * 
+ * @author lcram
+ */
+public final class SubcommandUtilities
+{
+    private SubcommandUtilities()
+    {
+
+    }
+}
