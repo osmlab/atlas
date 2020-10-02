10d9239da0ffa4fa9db98bc5f9c381acda914dee
lucaspcram <lucaspcram@gmail.com>
2020-10-02 11:47:50 -0700

    Added read times

--- src/main/java/org/openstreetmap/atlas/ZipTestingCommand.java
+++ src/main/java/org/openstreetmap/atlas/ZipTestingCommand.java
@@ -1,6 +1,7 @@
 package org.openstreetmap.atlas;
 
 import org.openstreetmap.atlas.exception.CoreException;
+import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.AtlasResourceLoader;
 import org.openstreetmap.atlas.geography.atlas.items.Point;
 import org.openstreetmap.atlas.streaming.resource.File;
@@ -8,6 +9,8 @@ import org.openstreetmap.atlas.streaming.resource.FileSuffix;
 import org.openstreetmap.atlas.utilities.command.abstractcommand.AbstractAtlasShellToolsCommand;
 import org.openstreetmap.atlas.utilities.command.parsing.ArgumentArity;
 import org.openstreetmap.atlas.utilities.command.parsing.ArgumentOptionality;
+import org.openstreetmap.atlas.utilities.scalars.Duration;
+import org.openstreetmap.atlas.utilities.time.Time;
 
 /**
  * @author lcram
@@ -31,17 +34,28 @@ public class ZipTestingCommand extends AbstractAtlasShellToolsCommand
             if (FileSuffix.pathFilter(FileSuffix.ATLAS).test(file.toAbsolutePath()) || FileSuffix
                     .pathFilter(FileSuffix.ATLAS, FileSuffix.TEXT).test(file.toAbsolutePath()))
             {
-                final Point point = new AtlasResourceLoader().load(file).point(1L);
+                final Atlas atlas = new AtlasResourceLoader().load(file);
+                final Time now = Time.now();
+                final Point point = atlas.point(1L);
+                final Duration timeToRead = now.elapsedSince();
                 this.getCommandOutputDelegate()
                         .printlnCommandMessage("was an atlas, Point 1L is " + point);
+                this.getCommandOutputDelegate().printlnCommandMessage(
+                        "took " + timeToRead.asMilliseconds() + "ms to read point data");
             }
             else
             {
+                final Time now = Time.now();
+                final String contents = file.readAndClose();
+                final Duration timeToRead = now.elapsedSince();
                 this.getCommandOutputDelegate()
-                        .printlnCommandMessage("was a file, contents:\n" + file.readAndClose());
+                        .printlnCommandMessage("was a file, contents:\n" + contents);
+                this.getCommandOutputDelegate().printlnCommandMessage(
+                        "took " + timeToRead.asMilliseconds() + "ms to read file data");
             }
 
             // sleep for 3 seconds to simulate a slow operation
+            this.getCommandOutputDelegate().printlnCommandMessage("sleeping for 3 sec...");
             try
             {
                 Thread.sleep(3000);
