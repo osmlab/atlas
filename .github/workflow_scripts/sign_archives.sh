73925da1f3519ce986ae47aa550a1684e4d41fbd
matthieun <matthieu.nahoum@gmail.com>
2021-01-11 14:48:31 -0800

    gpg tweak

--- .github/workflow_scripts/sign_archives.sh
+++ .github/workflow_scripts/sign_archives.sh
@@ -6,9 +6,14 @@ echo "GPG Key location: $GPG_KEY_LOCATION"
 echo "GPG Key id: $GPG_KEY_ID"
 echo "GPG passphrase: $GPG_PASSPHRASE"
 
+echo ""
 echo "Key size:"
 wc -c $GPG_KEY_LOCATION
+echo ""
 echo "GPG Version:"
 gpg --version
+echo ""
+echo "Export GPG TTY:"
+export GPG_TTY=$(tty)
 
 ./gradlew -s sign -x check
