6c50c0052c01cd8b44d9c4f2f38a862cc47636d3
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2021-01-12 09:11:25 -0800

    spotless apply

--- src/test/java/org/openstreetmap/atlas/tags/HighwayTagTestCase.java
+++ src/test/java/org/openstreetmap/atlas/tags/HighwayTagTestCase.java
@@ -19,16 +19,19 @@ public class HighwayTagTestCase
         final Taggable emergencyBayTaggable = Taggable.with(HighwayTag.KEY, "emergency_bay");
         Assert.assertTrue(Validators.hasValuesFor(emergencyBayTaggable, HighwayTag.class));
 
-        final TestTaggable falseNodeOnlyTag = new TestTaggable(HighwayTag.KEY, HighwayTag.TRUNK.toString());
+        final TestTaggable falseNodeOnlyTag = new TestTaggable(HighwayTag.KEY,
+                HighwayTag.TRUNK.toString());
         Assert.assertFalse(HighwayTag.isNodeOnlyTag(falseNodeOnlyTag));
 
-        final TestTaggable falseWayOnlyTag = new TestTaggable(HighwayTag.KEY, HighwayTag.BUS_STOP.toString());
+        final TestTaggable falseWayOnlyTag = new TestTaggable(HighwayTag.KEY,
+                HighwayTag.BUS_STOP.toString());
         Assert.assertFalse(HighwayTag.isWayOnlyTag(falseWayOnlyTag));
 
         final TestTaggable trueNodeOnlyTag = new TestTaggable(HighwayTag.KEY, "traffic_mirror");
         Assert.assertTrue(HighwayTag.isNodeOnlyTag(trueNodeOnlyTag));
 
-        final TestTaggable trueWayOnlyTag = new TestTaggable(HighwayTag.KEY, HighwayTag.PRIMARY.toString());
+        final TestTaggable trueWayOnlyTag = new TestTaggable(HighwayTag.KEY,
+                HighwayTag.PRIMARY.toString());
         Assert.assertTrue(HighwayTag.isWayOnlyTag(trueWayOnlyTag));
 
         final Taggable trafficMirrorTaggable = Taggable.with(HighwayTag.KEY, "traffic_mirror");
