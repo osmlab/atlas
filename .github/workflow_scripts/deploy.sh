2256efcc1df38795c3fb643a4e694db3e63639ed
matthieun <matthieu.nahoum@gmail.com>
2021-01-12 15:21:34 -0800

    fix indentation

--- .github/workflow_scripts/deploy.sh
+++ .github/workflow_scripts/deploy.sh
@@ -9,15 +9,15 @@ echo "Manual Release Triggered (Should be true): $MANUAL_RELEASE_TRIGGERED"
 if [ "$CURRENT_BRANCH" = "main" ] && [ -z "$GITHUB_HEAD_REF" ];
 then
     echo "On branch main, not in a Pull Request"
-	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
-	then
-		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
+    if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
+    then
+        echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
         ./gradlew uploadArchives publishToNexusAndClose
-		#python -m pip install --user --upgrade twine
-		#twine upload ./pyatlas/dist/*
+        #python -m pip install --user --upgrade twine
+        #twine upload ./pyatlas/dist/*
     else
         echo "Not publishing artifacts, since MANUAL_RELEASE_TRIGGERED=$MANUAL_RELEASE_TRIGGERED"
-	fi
+    fi
 else
     echo "Not publishing artifacts, since not on branch main, or in a Pull Request"
 fi
