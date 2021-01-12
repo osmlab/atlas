19fa8638f6605ac2428692542352ec911669f287
matthieun <matthieu.nahoum@gmail.com>
2021-01-12 15:20:16 -0800

    fix TODO

--- .github/workflow_scripts/deploy.sh
+++ .github/workflow_scripts/deploy.sh
@@ -12,8 +12,7 @@ then
 	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
 	then
 		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
-        # TODO Re-enable below
-        # ./gradlew uploadArchives publishToNexusAndClose
+        ./gradlew uploadArchives publishToNexusAndClose
 		#python -m pip install --user --upgrade twine
 		#twine upload ./pyatlas/dist/*
     else
