5d30d6e450e50286c733a1c3e077f055414ff002
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2021-01-12 08:28:25 -0800

    aggregating tests into one function.

--- src/test/java/org/openstreetmap/atlas/tags/HighwayTagTestCase.java
+++ src/test/java/org/openstreetmap/atlas/tags/HighwayTagTestCase.java
@@ -14,55 +14,27 @@ public class HighwayTagTestCase
 {
 
     @Test
-    public void emergencyBayIsHighwayTestCase()
+    public void HighwayTagTests()
     {
-        final Taggable taggable = Taggable.with("highway", "emergency_bay");
-        Assert.assertTrue(Validators.hasValuesFor(taggable, HighwayTag.class));
-    }
+        final Taggable emergencyBayTaggable = Taggable.with("highway", "emergency_bay");
+        Assert.assertTrue(Validators.hasValuesFor(emergencyBayTaggable, HighwayTag.class));
 
-    @Test
-    public void testFalseNodeOnlyHighwayTag()
-    {
         final TestTaggable falseNodeOnlyTag = new TestTaggable(HighwayTag.KEY, "trunk");
-
         Assert.assertFalse(HighwayTag.isNodeOnlyTag(falseNodeOnlyTag));
-    }
 
-    @Test
-    public void testFalseWayOnlyHighwayTag()
-    {
         final TestTaggable falseWayOnlyTag = new TestTaggable(HighwayTag.KEY, "bus_stop");
-
         Assert.assertFalse(HighwayTag.isWayOnlyTag(falseWayOnlyTag));
-    }
 
-    @Test
-    public void testTrueNodeOnlyHighwayTag()
-    {
         final TestTaggable trueNodeOnlyTag = new TestTaggable(HighwayTag.KEY, "traffic_mirror");
-
         Assert.assertTrue(HighwayTag.isNodeOnlyTag(trueNodeOnlyTag));
-    }
 
-    @Test
-    public void testTrueWayOnlyHighwayTag()
-    {
         final TestTaggable trueWayOnlyTag = new TestTaggable(HighwayTag.KEY, "primary");
-
         Assert.assertTrue(HighwayTag.isWayOnlyTag(trueWayOnlyTag));
-    }
 
-    @Test
-    public void trafficMirrorIsHighwayTestCase()
-    {
-        final Taggable taggable = Taggable.with("highway", "traffic_mirror");
-        Assert.assertTrue(Validators.hasValuesFor(taggable, HighwayTag.class));
-    }
+        final Taggable trafficMirrorTaggable = Taggable.with("highway", "traffic_mirror");
+        Assert.assertTrue(Validators.hasValuesFor(trafficMirrorTaggable, HighwayTag.class));
 
-    @Test
-    public void trailheadIsHighwayTestCase()
-    {
-        final Taggable taggable = Taggable.with("highway", "trailhead");
-        Assert.assertTrue(Validators.hasValuesFor(taggable, HighwayTag.class));
+        final Taggable trailheadTaggable = Taggable.with("highway", "trailhead");
+        Assert.assertTrue(Validators.hasValuesFor(trailheadTaggable, HighwayTag.class));
     }
 }
