fe8e223401b15609cb3c3e11008f4e3f98f5f59a
lucaspcram <lucaspcram@gmail.com>
2021-01-21 11:44:50 -0800

    EntityIdentifierGenerator now has some config options (#728)
    * EntityIdentifierGenerator now has some config options
    
    * Test updates
    
    * More testing and better errors
    
    * More comments
    
    Co-authored-by: jwpgage <me@jamesgage.com>

--- src/main/java/org/openstreetmap/atlas/utilities/identifiers/EntityIdentifierGenerator.java
+++ src/main/java/org/openstreetmap/atlas/utilities/identifiers/EntityIdentifierGenerator.java
@@ -26,8 +26,137 @@ import org.openstreetmap.atlas.utilities.collections.StringList;
  */
 public class EntityIdentifierGenerator
 {
+    /**
+     * A config class for {@link EntityIdentifierGenerator} to set various configuration parameters.
+     * 
+     * @author lcram
+     */
+    public static class Configuration
+    {
+        private boolean useGeometry;
+        private boolean useTags;
+        private boolean useRelationMembers;
+
+        public Configuration()
+        {
+            this.useGeometry = false;
+            this.useTags = false;
+            this.useRelationMembers = false;
+        }
+
+        public Configuration excludeGeometry()
+        {
+            this.useGeometry = false;
+            return this;
+        }
+
+        public Configuration excludeRelationMembers()
+        {
+            this.useRelationMembers = false;
+            return this;
+        }
+
+        public Configuration excludeTags()
+        {
+            this.useTags = false;
+            return this;
+        }
+
+        /**
+         * Get an {@link EntityIdentifierGenerator} built with this {@link Configuration}.
+         * 
+         * @return a configured {@link EntityIdentifierGenerator}
+         */
+        public EntityIdentifierGenerator getGenerator()
+        {
+            return new EntityIdentifierGenerator(this);
+        }
+
+        /**
+         * This {@link Configuration} is empty if all fields are false. Empty {@link Configuration}s
+         * are generally not valid.
+         *
+         * @return if this {@link Configuration} is empty
+         */
+        public boolean isEmpty()
+        {
+            return !this.useGeometry && !this.useTags && !this.useRelationMembers;
+        }
+
+        /**
+         * Check if this {@link Configuration} is non-relation invariant. A non-relation invariant
+         * {@link Configuration} is one that will generate the same ID for any non-relation type
+         * entity.
+         *
+         * @return if this {@link Configuration} is non-relation invariant.
+         */
+        public boolean isNonRelationInvariant()
+        {
+            return !this.useGeometry && !this.useTags;
+        }
+
+        public boolean isUsingGeometry()
+        {
+            return this.useGeometry;
+        }
+
+        public boolean isUsingRelationMembers()
+        {
+            return this.useRelationMembers;
+        }
+
+        public boolean isUsingTags()
+        {
+            return this.useTags;
+        }
+
+        /**
+         * Set all fields to true. This imitates the behaviour of the default
+         * {@link EntityIdentifierGenerator} constructor without any configuration.
+         *
+         * @return a {@link Configuration} set with the defaults
+         */
+        public Configuration useDefaults()
+        {
+            this.useGeometry = true;
+            this.useTags = true;
+            this.useRelationMembers = true;
+            return this;
+        }
+
+        public Configuration useGeometry()
+        {
+            this.useGeometry = true;
+            return this;
+        }
+
+        public Configuration useRelationMembers()
+        {
+            this.useRelationMembers = true;
+            return this;
+        }
+
+        public Configuration useTags()
+        {
+            this.useTags = true;
+            return this;
+        }
+    }
+
     private static final long HIGHEST_ATLAS_ID = 9999999999999999L;
     private static final long LOWEST_ATLAS_ID = -9999999999999999L;
+
+    private final Configuration configuration;
+
+    public EntityIdentifierGenerator()
+    {
+        this(new Configuration().useDefaults());
+    }
+
+    public EntityIdentifierGenerator(final Configuration configuration)
+    {
+        this.configuration = configuration;
+    }
 
     /**
      * Generate a 64 bit hash for a given non-{@link Edge} {@link CompleteEntity}. The entity must
@@ -39,10 +168,10 @@ public class EntityIdentifierGenerator
      */
     public long generateIdentifier(final CompleteEntity<?> entity)
     {
-        if (entity instanceof CompleteEdge)
+        if (entity.getType() == ItemType.EDGE)
         {
             throw new IllegalArgumentException(
-                    "For CompleteEdge, please use generatePositiveIdentifierForEdge");
+                    "For type EDGE, please use generatePositiveIdentifierForEdge");
         }
         return generate(entity, true);
     }
@@ -71,23 +200,33 @@ public class EntityIdentifierGenerator
      */
     String getBasicPropertyString(final CompleteEntity<?> entity)
     {
-        final String wkt = entity.toWkt();
-        if (wkt == null && !(entity instanceof CompleteRelation))
+        final StringBuilder builder = new StringBuilder();
+
+        if (this.configuration.isUsingGeometry())
         {
-            throw new CoreException("Geometry must be set for entity {}", entity.prettify());
+            final String wkt = entity.toWkt();
+            if (wkt == null && !(entity instanceof CompleteRelation))
+            {
+                throw new CoreException("Geometry must be set for entity {}", entity.prettify());
+            }
+            builder.append(wkt);
+        }
+        if (this.configuration.isUsingTags())
+        {
+            final Map<String, String> tags = entity.getTags();
+            if (tags == null)
+            {
+                throw new CoreException("Tags must be set for entity {}", entity.prettify());
+            }
+            final SortedSet<String> sortedTags = tags.entrySet().stream()
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.toCollection(TreeSet::new));
+            final String tagString = new StringList(sortedTags).join(",");
+            builder.append(";");
+            builder.append(tagString);
         }
 
-        final Map<String, String> tags = entity.getTags();
-        if (tags == null)
-        {
-            throw new CoreException("Tags must be set for entity {}", entity.prettify());
-        }
-        final SortedSet<String> sortedTags = tags.entrySet().stream()
-                .map(entry -> entry.getKey() + "=" + entry.getValue())
-                .collect(Collectors.toCollection(TreeSet::new));
-        final String tagString = new StringList(sortedTags).join(",");
-
-        return wkt + ";" + tagString;
+        return builder.toString();
     }
 
     /**
@@ -100,32 +239,41 @@ public class EntityIdentifierGenerator
      */
     String getTypeSpecificPropertyString(final CompleteEntity<?> entity)
     {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(";");
-        if (entity.getType() == ItemType.RELATION)
+        if (entity.getType() != ItemType.RELATION)
         {
+            return "";
+        }
+
+        if (this.configuration.isUsingRelationMembers())
+        {
+            final StringBuilder builder = new StringBuilder();
+            builder.append(";");
+
             final CompleteRelation relation = (CompleteRelation) entity;
-            if (relation.members() != null)
+
+            if (relation.members() == null)
             {
-                final RelationBean bean = relation.members().asBean();
-                builder.append("RelationBean[");
-                // Here use sorted list to ensure determinism
-                for (final RelationBean.RelationBeanItem beanItem : bean.asSortedList())
-                {
-                    builder.append("(");
-                    builder.append(beanItem.getType());
-                    builder.append(",");
-                    builder.append(beanItem.getIdentifier());
-                    builder.append(",");
-                    builder.append(beanItem.getRole());
-                    builder.append(")");
-                }
-                builder.append("]");
+                throw new CoreException("Relation members must be set for entity {}",
+                        entity.prettify());
             }
+
+            final RelationBean bean = relation.members().asBean();
+            builder.append("RelationBean[");
+            // Here use sorted list to ensure determinism
+            for (final RelationBean.RelationBeanItem beanItem : bean.asSortedList())
+            {
+                builder.append("(");
+                builder.append(beanItem.getType());
+                builder.append(",");
+                builder.append(beanItem.getIdentifier());
+                builder.append(",");
+                builder.append(beanItem.getRole());
+                builder.append(")");
+            }
+            builder.append("]");
             return builder.toString();
         }
 
-        // Otherwise no extra data
         return "";
     }
 
@@ -142,6 +290,18 @@ public class EntityIdentifierGenerator
      */
     private long generate(final CompleteEntity<?> entity, final boolean allowNegativeIdentifiers)
     {
+        if (this.configuration.isEmpty())
+        {
+            throw new CoreException(
+                    "EntityIdentifierGenerator.Configuration was empty! Please set at least one of geometry, tags, or relation members.");
+        }
+
+        if (entity.getType() != ItemType.RELATION && this.configuration.isNonRelationInvariant())
+        {
+            throw new CoreException(
+                    "EntityIdentifierGenerator.Configuration was non-relation invariant! Please set at least one of geometry or tags to generate IDs for non-relation type entities.");
+        }
+
         int iterations = 0;
         final int maximumIterations = 1000;
 
@@ -157,9 +317,16 @@ public class EntityIdentifierGenerator
 
             if (iterations > maximumIterations)
             {
+                /*
+                 * If this happens, we have a problem. It means that, within 1000 tries, we could
+                 * not generate an ID that was both safe to use (outside of OSM ID boundary) and
+                 * within the sign constraints (positive vs. negative allowed IDs). Realistically,
+                 * this should never happen. If for some reason it did, a possible solution would be
+                 * to simply bump up the iteration threshold.
+                 */
                 throw new CoreException(
-                        "Exceeded maximum iterations ({}) when attempting to generate hash",
-                        maximumIterations);
+                        "Exceeded maximum iterations ({}) when attempting to generate hash for {}",
+                        maximumIterations, entity.prettify());
             }
             iterations++;
         }
--- src/test/java/org/openstreetmap/atlas/utilities/identifiers/EntityIdentifierGeneratorTest.java
+++ src/test/java/org/openstreetmap/atlas/utilities/identifiers/EntityIdentifierGeneratorTest.java
@@ -1,7 +1,10 @@
 package org.openstreetmap.atlas.utilities.identifiers;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.openstreetmap.atlas.exception.CoreException;
 import org.openstreetmap.atlas.geography.Location;
 import org.openstreetmap.atlas.geography.PolyLine;
 import org.openstreetmap.atlas.geography.Rectangle;
@@ -18,16 +21,61 @@ import org.openstreetmap.atlas.utilities.collections.Sets;
  */
 public class EntityIdentifierGeneratorTest
 {
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testEmptyConfigException()
+    {
+        final CompletePoint point = new CompletePoint(1L, Location.CENTER,
+                Maps.hashMap("a", "b", "c", "d"), Sets.hashSet());
+
+        Assert.assertTrue((new EntityIdentifierGenerator.Configuration().getGenerator()
+                .getBasicPropertyString(point)
+                + new EntityIdentifierGenerator.Configuration().getGenerator()
+                        .getTypeSpecificPropertyString(point)).isEmpty());
+
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage(
+                "EntityIdentifierGenerator.Configuration was empty! Please set at least one of geometry, tags, or relation members.");
+        new EntityIdentifierGenerator.Configuration().getGenerator().generateIdentifier(point);
+    }
+
+    @Test
+    public void testForcePositiveIDForEdge()
+    {
+        final CompleteEdge edge = new CompleteEdge(1L, PolyLine.SIMPLE_POLYLINE,
+                Maps.hashMap("a", "b", "c", "d"), 2L, 3L, Sets.hashSet());
+
+        this.expectedException.expect(IllegalArgumentException.class);
+        this.expectedException
+                .expectMessage("For type EDGE, please use generatePositiveIdentifierForEdge");
+        new EntityIdentifierGenerator.Configuration().useDefaults().getGenerator()
+                .generateIdentifier(edge);
+    }
+
     @Test
     public void testGetFullPropertyString()
     {
         final CompleteEdge edge = new CompleteEdge(1L, PolyLine.SIMPLE_POLYLINE,
                 Maps.hashMap("a", "b", "c", "d"), 2L, 3L, Sets.hashSet());
 
-        final String goldenPropertyString = "LINESTRING (1 1, 2 2);a=b,c=d";
+        final String goldenPropertyStringAll = "LINESTRING (1 1, 2 2);a=b,c=d";
+        Assert.assertEquals(goldenPropertyStringAll,
+                new EntityIdentifierGenerator.Configuration().useDefaults().getGenerator()
+                        .getBasicPropertyString(edge)
+                        + new EntityIdentifierGenerator().getTypeSpecificPropertyString(edge));
 
-        Assert.assertEquals(goldenPropertyString,
-                new EntityIdentifierGenerator().getBasicPropertyString(edge)
+        final String goldenPropertyStringGeometryOnly = "LINESTRING (1 1, 2 2)";
+        Assert.assertEquals(goldenPropertyStringGeometryOnly,
+                new EntityIdentifierGenerator.Configuration().useGeometry().getGenerator()
+                        .getBasicPropertyString(edge)
+                        + new EntityIdentifierGenerator().getTypeSpecificPropertyString(edge));
+
+        final String goldenPropertyStringTagsOnly = ";a=b,c=d";
+        Assert.assertEquals(goldenPropertyStringTagsOnly,
+                new EntityIdentifierGenerator.Configuration().useTags().getGenerator()
+                        .getBasicPropertyString(edge)
                         + new EntityIdentifierGenerator().getTypeSpecificPropertyString(edge));
     }
 
@@ -65,16 +113,124 @@ public class EntityIdentifierGeneratorTest
                 new EntityIdentifierGenerator().getTypeSpecificPropertyString(relation1));
         Assert.assertEquals(goldenPropertyString,
                 new EntityIdentifierGenerator().getTypeSpecificPropertyString(relation2));
+        Assert.assertTrue(
+                new EntityIdentifierGenerator.Configuration().useDefaults().excludeRelationMembers()
+                        .getGenerator().getTypeSpecificPropertyString(relation1).isEmpty());
+        Assert.assertEquals(goldenPropertyString, new EntityIdentifierGenerator.Configuration()
+                .useRelationMembers().getGenerator().getTypeSpecificPropertyString(relation2));
     }
 
     @Test
-    public void testHash()
+    public void testHashes()
     {
         final CompleteEdge edge = new CompleteEdge(1L, PolyLine.SIMPLE_POLYLINE,
                 Maps.hashMap("a", "b", "c", "d"), 2L, 3L, Sets.hashSet());
 
-        final long goldenHash = 6463671242943641314L;
-        Assert.assertEquals(goldenHash,
+        final long goldenHash1 = 6463671242943641314L;
+        Assert.assertEquals(goldenHash1,
                 new EntityIdentifierGenerator().generatePositiveIdentifierForEdge(edge));
+
+        final CompletePoint point = new CompletePoint(1L, Location.CENTER,
+                Maps.hashMap("a", "b", "c", "d"), Sets.hashSet());
+
+        final long goldenHash2 = 4334702026426103264L;
+        Assert.assertEquals(goldenHash2, new EntityIdentifierGenerator().generateIdentifier(point));
+        Assert.assertEquals(goldenHash2, new EntityIdentifierGenerator.Configuration().useDefaults()
+                .getGenerator().generateIdentifier(point));
+
+        final long goldenHash3 = -2934213421148195810L;
+        Assert.assertEquals(goldenHash3, new EntityIdentifierGenerator.Configuration().useGeometry()
+                .getGenerator().generateIdentifier(point));
+        Assert.assertEquals(goldenHash3, new EntityIdentifierGenerator.Configuration().useDefaults()
+                .excludeTags().excludeRelationMembers().getGenerator().generateIdentifier(point));
+
+        final long goldenHash4 = 3739460904040018219L;
+        Assert.assertEquals(goldenHash4, new EntityIdentifierGenerator.Configuration().useTags()
+                .getGenerator().generateIdentifier(point));
+        Assert.assertEquals(goldenHash4,
+                new EntityIdentifierGenerator.Configuration().useDefaults().excludeGeometry()
+                        .excludeRelationMembers().getGenerator().generateIdentifier(point));
+    }
+
+    @Test
+    public void testNonRelationInvariantConfigException()
+    {
+        final EntityIdentifierGenerator nonRelationInvariantGenerator = new EntityIdentifierGenerator.Configuration()
+                .useRelationMembers().getGenerator();
+
+        final RelationBean bean1 = new RelationBean();
+        bean1.addItem(1L, "role", ItemType.POINT);
+        bean1.addItem(10L, "role", ItemType.AREA);
+        final CompleteRelation relation1 = new CompleteRelation(1L,
+                Maps.hashMap("a", "b", "c", "d"), Rectangle.MINIMUM, bean1, null, null, null,
+                Sets.hashSet());
+        Assert.assertEquals(6707509058043000459L,
+                nonRelationInvariantGenerator.generateIdentifier(relation1));
+
+        final CompletePoint point = new CompletePoint(1L, Location.CENTER,
+                Maps.hashMap("a", "b", "c", "d"), Sets.hashSet());
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage(
+                "EntityIdentifierGenerator.Configuration was non-relation invariant! Please set at least one of geometry or tags to generate IDs for non-relation type entities.");
+        nonRelationInvariantGenerator.generateIdentifier(point);
+    }
+
+    @Test
+    public void testUnsetGeometryException()
+    {
+        final CompletePoint pointNoGeometry = new CompletePoint(1L, null,
+                Maps.hashMap("a", "b", "c", "d"), Sets.hashSet());
+        final CompletePoint pointNoTags = new CompletePoint(1L, Location.CENTER, null,
+                Sets.hashSet());
+
+        new EntityIdentifierGenerator.Configuration().useTags().getGenerator()
+                .generateIdentifier(pointNoGeometry);
+        new EntityIdentifierGenerator.Configuration().useGeometry().getGenerator()
+                .generateIdentifier(pointNoTags);
+
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Geometry must be set for entity");
+        new EntityIdentifierGenerator().generateIdentifier(pointNoGeometry);
+    }
+
+    @Test
+    public void testUnsetRelationMembersException()
+    {
+        final RelationBean bean1 = new RelationBean();
+        bean1.addItem(1L, "role", ItemType.POINT);
+        bean1.addItem(10L, "role", ItemType.AREA);
+        final CompleteRelation relationWithMembers = new CompleteRelation(1L,
+                Maps.hashMap("a", "b", "c", "d"), Rectangle.MINIMUM, bean1, null, null, null,
+                Sets.hashSet());
+        final CompleteRelation relationNoMembers = new CompleteRelation(1L,
+                Maps.hashMap("a", "b", "c", "d"), Rectangle.MINIMUM, null, null, null, null,
+                Sets.hashSet());
+
+        new EntityIdentifierGenerator.Configuration().useDefaults().getGenerator()
+                .generateIdentifier(relationWithMembers);
+        new EntityIdentifierGenerator.Configuration().useDefaults().excludeRelationMembers()
+                .getGenerator().generateIdentifier(relationNoMembers);
+
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Relation members must be set for entity");
+        new EntityIdentifierGenerator().generateIdentifier(relationNoMembers);
+    }
+
+    @Test
+    public void testUnsetTagsException()
+    {
+        final CompletePoint pointNoGeometry = new CompletePoint(1L, null,
+                Maps.hashMap("a", "b", "c", "d"), Sets.hashSet());
+        final CompletePoint pointNoTags = new CompletePoint(1L, Location.CENTER, null,
+                Sets.hashSet());
+
+        new EntityIdentifierGenerator.Configuration().useTags().getGenerator()
+                .generateIdentifier(pointNoGeometry);
+        new EntityIdentifierGenerator.Configuration().useGeometry().getGenerator()
+                .generateIdentifier(pointNoTags);
+
+        this.expectedException.expect(CoreException.class);
+        this.expectedException.expectMessage("Tags must be set for entity");
+        new EntityIdentifierGenerator().generateIdentifier(pointNoTags);
     }
 }
