a6c20b666621786ead7aab846e05441bafc0d0cb
matthieun <matthieu.nahoum@gmail.com>
2021-02-18 15:49:48 -0800

    Added some test

--- src/test/java/org/openstreetmap/atlas/geography/atlas/dynamic/DynamicAtlasAggressiveRelationsTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/dynamic/DynamicAtlasAggressiveRelationsTest.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 import org.openstreetmap.atlas.geography.Rectangle;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
 import org.openstreetmap.atlas.geography.atlas.dynamic.policy.DynamicAtlasPolicy;
+import org.openstreetmap.atlas.geography.atlas.dynamic.rules.DynamicAtlasAggressiveRelationsTestRule;
 import org.openstreetmap.atlas.geography.atlas.dynamic.rules.DynamicAtlasTestRule;
+import org.openstreetmap.atlas.geography.atlas.items.Relation;
 import org.openstreetmap.atlas.geography.sharding.Shard;
 import org.openstreetmap.atlas.geography.sharding.SlippyTile;
 import org.openstreetmap.atlas.geography.sharding.SlippyTileSharding;
@@ -20,10 +22,13 @@ import org.openstreetmap.atlas.geography.sharding.SlippyTileSharding;
 /**
  * @author matthieun
  */
-public class DynamicAtlasAgressiveRelationsTest
+public class DynamicAtlasAggressiveRelationsTest
 {
     @Rule
     public DynamicAtlasTestRule rule = new DynamicAtlasTestRule();
+
+    @Rule
+    public DynamicAtlasAggressiveRelationsTestRule rule2 = new DynamicAtlasAggressiveRelationsTestRule();
 
     private Map<Shard, Atlas> store;
     private final Supplier<DynamicAtlasPolicy> policySupplier = () -> new DynamicAtlasPolicy(
@@ -39,6 +44,19 @@ public class DynamicAtlasAgressiveRelationsTest
                 }
             }, new SlippyTileSharding(12), new SlippyTile(1350, 1870, 12), Rectangle.MAXIMUM);
 
+    private final Supplier<DynamicAtlasPolicy> policySupplier2 = () -> new DynamicAtlasPolicy(
+            shard ->
+            {
+                if (this.store.containsKey(shard))
+                {
+                    return Optional.of(this.store.get(shard));
+                }
+                else
+                {
+                    return Optional.empty();
+                }
+            }, new SlippyTileSharding(11), new SlippyTile(998, 708, 11), Rectangle.MAXIMUM);
+
     @Before
     public void prepare()
     {
@@ -47,6 +65,8 @@ public class DynamicAtlasAgressiveRelationsTest
         this.store.put(new SlippyTile(1350, 1869, 12), this.rule.getAtlasz12x1350y1869());
         this.store.put(new SlippyTile(1349, 1869, 12), this.rule.getAtlasz12x1349y1869());
         this.store.put(new SlippyTile(1349, 1870, 12), this.rule.getAtlasz12x1349y1870());
+        this.store.put(new SlippyTile(998, 708, 11), this.rule2.getAtlasZ11X998Y708());
+        this.store.put(new SlippyTile(999, 708, 11), this.rule2.getAtlasZ11X999Y708());
     }
 
     @Test
@@ -62,5 +82,20 @@ public class DynamicAtlasAgressiveRelationsTest
         Assert.assertNotNull(dynamicAtlas.relation(2));
         Assert.assertNotNull(dynamicAtlas.relation(3));
         Assert.assertEquals(9, dynamicAtlas.numberOfEdges());
+    }
+
+    @Test
+    public void testRelationsAggressivelyWithExpantionIndefinite()
+    {
+        final DynamicAtlas dynamicAtlas = new DynamicAtlas(this.policySupplier2.get()
+                .withAggressivelyExploreRelations(true).withExtendIndefinitely(true)
+                .withDeferredLoading(true)
+                .withAtlasEntitiesToConsiderForExpansion(entity -> entity instanceof Relation));
+
+        // Prompts load of 11-999-708
+        dynamicAtlas.preemptiveLoad();
+        final Relation relation = dynamicAtlas.relation(99756000000L);
+        Assert.assertNotNull(relation);
+        Assert.assertEquals(2, relation.members().size());
     }
 }
