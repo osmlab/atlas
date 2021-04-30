0b15c794282949ed8d34e33834ba1040af4523eb
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2021-04-30 15:27:36 -0700

    added fixme tag with key (#755)
    * added fixme tag with key
    
    * adjusted validation to non empty string
    
    * return optional and make key static/final
    
    * making interface
    
    * fixme sonar lint ignore
    
    * removing function
    
    * optimize import
    
    Co-authored-by: Samuel Gass <adahn6@gmail.com>

---
+++ src/main/java/org/openstreetmap/atlas/tags/FixMeTag.java
@@ -0,0 +1,16 @@
+package org.openstreetmap.atlas.tags;
+
+import org.openstreetmap.atlas.tags.annotations.Tag;
+import org.openstreetmap.atlas.tags.annotations.TagKey;
+
+/**
+ * OSM fix me tag. Check OSM Wiki below for more information.
+ *
+ * @author v-garei
+ */
+@Tag(value = Tag.Validation.NON_EMPTY_STRING, taginfo = "https://taginfo.openstreetmap.org/keys/fixme#values", osm = "https://wiki.openstreetmap.org/wiki/Key:fixme")
+public interface FixMeTag
+{
+    @TagKey
+    String KEY = "fixme";
+}
