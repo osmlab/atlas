8735c523f4ca9fc8fdf1e73ca68b41c6cf5d1132
Sam Gass <adahn6@gmail.com>
2021-08-26 13:37:55 -0700

    remove aql

--- src/main/groovy/org/openstreetmap/atlas/geography/atlas/dsl/query/commit/Commitable.groovy
+++
@@ -1,9 +0,0 @@
-package org.openstreetmap.atlas.geography.atlas.dsl.query.commit
-
-/**
- * Statements that support committing.
- *
- * @author Yazad Khambata
- */
-interface Commitable {
-}
--- src/main/groovy/org/openstreetmap/atlas/geography/atlas/dsl/selection/package-info.groovy
+++
@@ -1,6 +0,0 @@
-/**
- * Deals with selection (σ) in terms of relational algebra and SQL.
- *
- * See <a href="https://en.wikipedia.org/wiki/Relational_algebra#Selection_(%CF%83)">Selection on Wikipedia</a>
- */
-package org.openstreetmap.atlas.geography.atlas.dsl.selection
